ba498163f59fb39d5c0e318372f7f04fb1a9f441
uboness <uboness@gmail.com>
2014-07-21 04:37:22 +0200

    Added more unit tests, re-implemented & added tests for ESUsersTool
    - Added CliTool infrastructure (should eventually be moved to core and removed from this repo)
    
    Original commit: elastic/x-pack@758dd75d27d8069ebc00bea5425b398190d8d032

---
+++ src/main/java/org/elasticsearch/shield/support/cli/HelpPrinter.java
@@ -0,0 +1,39 @@
+package org.elasticsearch.shield.support.cli;
+
+import com.google.common.base.Charsets;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ */
+public class HelpPrinter {
+
+    public void print(CliToolConfig config, Terminal terminal) {
+        URL url = config.toolType().getResource(config.name() + ".help");
+        print(url, terminal);
+    }
+
+    public void print(CliToolConfig.Cmd cmd, Terminal terminal) {
+        URL url = cmd.cmdType().getResource(cmd.name() + ".help");
+        print(url, terminal);
+    }
+
+    private static void print(URL url, Terminal terminal) {
+        terminal.println();
+        try {
+            Path helpFile = Paths.get(url.toURI());
+            for (String line : Files.readAllLines(helpFile, Charsets.UTF_8)) {
+                terminal.println(line);
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace(terminal.writer());
+        }
+        terminal.println();
+    }
+}
