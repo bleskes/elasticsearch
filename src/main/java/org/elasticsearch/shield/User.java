e2d1b3116be994cdfcf09b2ebe6de1d3254a2cf6
uboness <uboness@gmail.com>
2014-10-17 19:03:35 -0700

    Changed the cached hashing algorithm for cached realms
    Now the passwords are hashed in-memory using SHA2 by default (instead of original bcrypt). Also, it's now possible to configure the in-memory hashing algorithm.
    
    Original commit: elastic/x-pack@cb7737e70a52ea5d47f64b73afbdf1d994225d6f

--- src/main/java/org/elasticsearch/shield/User.java
+++ src/main/java/org/elasticsearch/shield/User.java
@@ -19,6 +19,8 @@ package org.elasticsearch.shield;
 
 import org.elasticsearch.shield.authz.SystemRole;
 
+import java.util.Arrays;
+
 /**
  * An authenticated user
  */
@@ -60,6 +62,26 @@ public abstract class User {
         public String[] roles() {
             return roles;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Simple simple = (Simple) o;
+
+            if (!Arrays.equals(roles, simple.roles)) return false;
+            if (!username.equals(simple.username)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = username.hashCode();
+            result = 31 * result + Arrays.hashCode(roles);
+            return result;
+        }
     }
 
     private static class System extends User {
@@ -79,6 +101,7 @@ public abstract class User {
         public String[] roles() {
             return ROLES;
         }
+
     }
 
 }
