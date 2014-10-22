29751a866df082cbc798ae9776df527740497554
Igor Motov <igor@motovs.org>
2014-10-21 22:35:11 -0400

    Packaging system refactoring (WIP)
    Original commit: elastic/x-pack@203de93b9ae838a1a2c9ef37d7b2551da12f4447

--- src/main/java/org/elasticsearch/license/plugin/core/LicensesService.java
+++ src/main/java/org/elasticsearch/license/plugin/core/LicensesService.java
@@ -77,10 +77,10 @@ public class LicensesService extends AbstractLifecycleComponent<LicensesService>
     private volatile ScheduledFuture notificationScheduler;
 
     @Inject
-    public LicensesService(Settings settings, ClusterService clusterService, ThreadPool threadPool) {
+    public LicensesService(Settings settings, ClusterService clusterService, ThreadPool threadPool, ESLicenseManager esLicenseManager) {
         super(settings);
         this.clusterService = clusterService;
-        this.esLicenseManager = new ESLicenseManager();
+        this.esLicenseManager = esLicenseManager;
         this.threadPool = threadPool;
     }
 
