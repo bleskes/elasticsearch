a73c6b60f8152fef7c85b7cc3314c9ae8f78aa4b
Brian Murphy <brian.p.murphy@gmail.com>
2014-10-24 12:24:04 +0100

    AlertActionManager: Add alert action job queue
    This change adds the AlertActionManager and AlertActionEntry. The old AlertActionManager has become the AlertActionRegistry.
    This means that now the results of Alerts are queued up in a job queue and executed in separate threads.
    The AlertActionManager is a composite member of the AlertManager.
    Change the BasicTest to just run on a single node to fix the action registration if the action happens on a different node.
    Threads are not directly constructed but now the threadpool is used.
    The ClusterStateListener in AlertManager is responsible now for starting the job queue.
    
    Original commit: elastic/x-pack@aaaf5116b852f5b7411bd58537b1672358ed473b

--- src/main/java/org/elasticsearch/alerts/AlertingModule.java
+++ src/main/java/org/elasticsearch/alerts/AlertingModule.java
@@ -1,6 +1,6 @@
 package org.elasticsearch.alerts;
 
-import org.elasticsearch.alerts.actions.AlertActionManager;
+import org.elasticsearch.alerts.actions.AlertActionRegistry;
 import org.elasticsearch.alerts.rest.AlertRestHandler;
 import org.elasticsearch.alerts.scheduler.AlertScheduler;
 import org.elasticsearch.alerts.triggers.TriggerManager;
@@ -13,7 +13,7 @@ public class AlertingModule extends AbstractModule {
         bind(AlertManager.class).asEagerSingleton();
         bind(TriggerManager.class).asEagerSingleton();
         bind(AlertScheduler.class).asEagerSingleton();
-        bind(AlertActionManager.class).asEagerSingleton();
+        bind(AlertActionRegistry.class).asEagerSingleton();
         bind(AlertRestHandler.class).asEagerSingleton();
     }
 
