f7dc1b7c9c98a9f80945992dbc9c1dbd111f4308
Areek Zillur <areek.zillur@elasticsearch.com>
2014-10-27 12:49:18 -0400

    nuked LicenseSpec; merge to ESLicense
    Original commit: elastic/x-pack@5324562eb23d1510662c458fef58d956510fac42

--- src/main/java/org/elasticsearch/license/core/ESLicense.java
+++ src/main/java/org/elasticsearch/license/core/ESLicense.java
@@ -269,7 +269,7 @@ public class ESLicense implements Comparable<ESLicense> {
             return this;
         }
 
-        public Builder fromLicense(ESLicense license) {
+        public Builder fromLicenseSpec(ESLicense license, String signature) {
             return uid(license.uid())
                     .issuedTo(license.issuedTo())
                     .issueDate(license.issueDate())
@@ -279,7 +279,7 @@ public class ESLicense implements Comparable<ESLicense> {
                     .maxNodes(license.maxNodes())
                     .expiryDate(license.expiryDate())
                     .issuer(license.issuer())
-                    .signature(license.signature());
+                    .signature(signature);
         }
 
         public ESLicense verifyAndBuild() {
