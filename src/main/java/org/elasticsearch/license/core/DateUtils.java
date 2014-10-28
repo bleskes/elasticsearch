80a3f01ab1a80e6d29439629e7411f72f5c619ef
Areek Zillur <areek.zillur@elasticsearch.com>
2014-10-27 23:28:48 -0400

    explicit UTC date parsing
    Original commit: elastic/x-pack@77aababd4d94fd13992f3d050d106815c88c0991

--- src/main/java/org/elasticsearch/license/core/DateUtils.java
+++ src/main/java/org/elasticsearch/license/core/DateUtils.java
@@ -28,7 +28,7 @@ public class DateUtils {
 
     private final static FormatDateTimeFormatter formatDateTimeFormatter = Joda.forPattern("yyyy-MM-dd");
 
-    private final static DateTimeFormatter dateTimeFormatter = formatDateTimeFormatter.parser();
+    private final static DateTimeFormatter dateTimeFormatter = formatDateTimeFormatter.parser().withZoneUTC();
 
     public static long endOfTheDay(String date) {
         MutableDateTime dateTime = dateTimeFormatter.parseMutableDateTime(date);
