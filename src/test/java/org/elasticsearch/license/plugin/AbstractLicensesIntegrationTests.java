4c838f18d4f54f8414aeb1ac68801a98e60866d1
Igor Motov <igor@motovs.org>
2014-10-28 16:45:27 -0400

    Fix license metadata serialization
    Original commit: elastic/x-pack@5d5ec18c2065defa954a099bf04ca1bf8a99909d

---
+++ src/test/java/org/elasticsearch/license/plugin/AbstractLicensesIntegrationTests.java
@@ -0,0 +1,74 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.license.plugin;
+
+import org.elasticsearch.cluster.ClusterService;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ProcessedClusterStateUpdateTask;
+import org.elasticsearch.cluster.metadata.MetaData;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.license.plugin.core.LicensesMetaData;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ */
+public abstract class AbstractLicensesIntegrationTests extends ElasticsearchIntegrationTest {
+
+    @Override
+    protected Settings nodeSettings(int nodeOrdinal) {
+        return ImmutableSettings.settingsBuilder()
+                .put("plugins.load_classpath_plugins", false)
+                .put("plugin.types", LicensePlugin.class.getName())
+                .build();
+    }
+
+    @Override
+    protected Settings transportClientSettings() {
+        // Plugin should be loaded on the transport client as well
+        return nodeSettings(0);
+    }
+
+    protected void wipeAllLicenses() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        ClusterService clusterService = internalCluster().getInstance(ClusterService.class, internalCluster().getMasterName());
+        clusterService.submitStateUpdateTask("delete licensing metadata", new ProcessedClusterStateUpdateTask() {
+            @Override
+            public void clusterStateProcessed(String source, ClusterState oldState, ClusterState newState) {
+                latch.countDown();
+            }
+
+            @Override
+            public ClusterState execute(ClusterState currentState) throws Exception {
+                MetaData.Builder mdBuilder = MetaData.builder(currentState.metaData());
+                mdBuilder.putCustom(LicensesMetaData.TYPE, null);
+                return ClusterState.builder(currentState).metaData(mdBuilder).build();
+            }
+
+            @Override
+            public void onFailure(String source, @Nullable Throwable t) {
+                logger.error("error on metaData cleanup after test", t);
+            }
+        });
+        latch.await();
+    }
+
+}
