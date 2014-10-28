a1c136d3bb8e6acb0809aa0f39b0951d71ac68dc
Areek Zillur <areek.zillur@elasticsearch.com>
2014-10-27 21:37:16 -0400

    licensesService cleanup
    Original commit: elastic/x-pack@0f2008cf40560a44900435507e1629caf809eb71

--- src/test/java/org/elasticsearch/license/plugin/LicensesServiceTests.java
+++ src/test/java/org/elasticsearch/license/plugin/LicensesServiceTests.java
@@ -160,7 +160,7 @@ public class LicensesServiceTests extends ElasticsearchIntegrationTest {
         List<ESLicense> licenses = ESLicenses.fromSource(licenseOutput);
 
         LicensesManagerService licensesManagerService = masterLicensesManagerService();
-        ESLicenseManager esLicenseManager = ((LicensesService) licensesManagerService).getEsLicenseManager();
+        ESLicenseManager esLicenseManager = masterLicenseManager();
         final CountDownLatch latch1 = new CountDownLatch(1);
         // todo: fix with awaitBusy
         licensesManagerService.registerLicenses(new LicensesService.PutLicenseRequestHolder(new PutLicenseRequest().licenses(licenses), "test"), new ActionListener<ClusterStateUpdateResponse>() {
@@ -356,6 +356,11 @@ public class LicensesServiceTests extends ElasticsearchIntegrationTest {
     private LicensesManagerService masterLicensesManagerService() {
         final InternalTestCluster clients = internalCluster();
         return clients.getInstance(LicensesManagerService.class, clients.getMasterName());
+    }
+
+    private ESLicenseManager masterLicenseManager() {
+        final InternalTestCluster clients = internalCluster();
+        return clients.getInstance(ESLicenseManager.class, clients.getMasterName());
     }
 
     private LicensesManagerService licensesManagerService() {
