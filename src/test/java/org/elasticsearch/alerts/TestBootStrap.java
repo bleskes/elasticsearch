db497a6b519b8eedbc1e77a39991fe3ae9acd771
Brian Murphy <brian.p.murphy@gmail.com>
2014-11-18 11:17:00 +0000

    TESTS : Add stats test and bootstrap test
    This commit adds tests for the stats API along with a bootstrap test.
    The bootstrap test is currently failing outside of a debugger for me so I'm digging into it.
    
    Original commit: elastic/x-pack@71b65ef04745af611808cde256ee4510b3ec6b7c

---
+++ src/test/java/org/elasticsearch/alerts/TestBootStrap.java
@@ -0,0 +1,89 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+
+package org.elasticsearch.alerts;
+
+import org.apache.lucene.util.LuceneTestCase;
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.alerts.transport.actions.stats.AlertsStatsRequest;
+import org.elasticsearch.alerts.transport.actions.stats.AlertsStatsResponse;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.search.builder.SearchSourceBuilder.searchSource;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ */
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.SUITE, numClientNodes = 0, transportClientRatio = 0, numDataNodes = 3)
+public class TestBootStrap extends AbstractAlertingTests {
+
+    @Test
+    @LuceneTestCase.AwaitsFix(bugUrl = "This test fails but shouldn't")
+    public void testBootStrapAlerts() throws Exception {
+        ensureGreen();
+
+        SearchRequest searchRequest = createTriggerSearchRequest("my-index").source(searchSource().query(termQuery("field", "value")));
+        BytesReference alertSource = createAlertSource("0 0/5 * * * ? *", searchRequest, "hits.total == 1");
+        alertClient().prepareIndexAlert("my-first-alert")
+                .setAlertSource(alertSource)
+                .get();
+
+        AlertsStatsRequest alertsStatsRequest = alertClient().prepareAlertsStats().request();
+        AlertsStatsResponse response = alertClient().alertsStats(alertsStatsRequest).actionGet();
+
+        assertTrue(response.isAlertActionManagerStarted());
+        assertTrue(response.isAlertManagerStarted());
+        assertThat(response.getNumberOfRegisteredAlerts(), equalTo(1L));
+
+
+        String oldMaster = internalTestCluster().getMasterName();
+
+        try {
+            internalTestCluster().stopCurrentMasterNode();
+        } catch (IOException ioe) {
+            throw new ElasticsearchException("Failed to stop current master", ioe);
+        }
+
+        //Wait for alerts to start
+        TimeValue maxTime = new TimeValue(30, TimeUnit.SECONDS);
+        Thread.sleep(maxTime.getMillis());
+
+        String newMaster = internalTestCluster().getMasterName();
+
+        assertFalse(newMaster.equals(oldMaster));
+        logger.info("Switched master from [{}] to [{}]",oldMaster,newMaster);
+
+        alertsStatsRequest = alertClient().prepareAlertsStats().request();
+        response = alertClient().alertsStats(alertsStatsRequest).actionGet();
+
+        assertTrue(response.isAlertActionManagerStarted());
+        assertTrue(response.isAlertManagerStarted());
+
+        assertThat(response.getNumberOfRegisteredAlerts(), equalTo(1L));
+
+    }
+
+}
---
+++ src/test/java/org/elasticsearch/alerts/actions/TestAlertStats.java
@@ -0,0 +1,72 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.alerts.actions;
+
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.alerts.AbstractAlertingTests;
+import org.elasticsearch.alerts.client.AlertsClient;
+import org.elasticsearch.alerts.transport.actions.stats.AlertsStatsRequest;
+import org.elasticsearch.alerts.transport.actions.stats.AlertsStatsResponse;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.junit.Test;
+
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.elasticsearch.search.builder.SearchSourceBuilder.searchSource;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+/**
+ */
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.SUITE, numClientNodes = 0, transportClientRatio = 0)
+public class TestAlertStats extends AbstractAlertingTests {
+
+    @Test
+    public void testStartedStats() throws Exception {
+        AlertsStatsRequest alertsStatsRequest = alertClient().prepareAlertsStats().request();
+        AlertsStatsResponse response = alertClient().alertsStats(alertsStatsRequest).actionGet();
+
+        assertTrue(response.isAlertActionManagerStarted());
+        assertTrue(response.isAlertManagerStarted());
+        assertThat(response.getAlertActionManagerQueueSize(), equalTo(0L));
+        assertThat(response.getNumberOfRegisteredAlerts(), equalTo(0L));
+    }
+
+    @Test
+    public void testAlertCountStats() throws Exception {
+        AlertsClient alertsClient = alertClient();
+
+        AlertsStatsRequest alertsStatsRequest = alertsClient.prepareAlertsStats().request();
+        AlertsStatsResponse response = alertsClient.alertsStats(alertsStatsRequest).actionGet();
+
+        assertTrue(response.isAlertActionManagerStarted());
+        assertTrue(response.isAlertManagerStarted());
+
+        SearchRequest searchRequest = createTriggerSearchRequest("my-index").source(searchSource().query(termQuery("field", "value")));
+        BytesReference alertSource = createAlertSource("0/5 * * * * ? *", searchRequest, "hits.total == 1");
+        alertClient().prepareIndexAlert("testAlert")
+                .setAlertSource(alertSource)
+                .get();
+
+        response = alertClient().alertsStats(alertsStatsRequest).actionGet();
+        assertTrue(response.isAlertActionManagerStarted());
+        assertTrue(response.isAlertManagerStarted());
+        assertThat(response.getNumberOfRegisteredAlerts(), equalTo(1L));
+
+    }
+}
