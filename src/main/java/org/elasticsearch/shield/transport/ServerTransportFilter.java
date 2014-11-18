545ff241366e32dd7d6aacc857b438a946de560d
Alexander Reelsen <alexander@reelsen.net>
2014-11-18 14:26:35 +0100

    Refactoring: Make TransportFilter work for inbound connections only
    As netty uses different bootstraps for sending/receiving requests, having a
    single interface for filtering incoming/outgoing messages does not make a
    lot of sense.
    
    This commit changes from a TransportFilter interface to a InboundTransportFilter
    interface, which is only able to filter incoming messages - which is all we do
    anyway right now.
    
    Original commit: elastic/x-pack@954ea735264cfd79958653fe3f05974a20123a0e

---
+++ src/main/java/org/elasticsearch/shield/transport/ServerTransportFilter.java
@@ -0,0 +1,36 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.shield.transport;
+
+import org.elasticsearch.transport.TransportRequest;
+
+public interface ServerTransportFilter {
+
+    static final ServerTransportFilter NOOP = new ServerTransportFilter() {
+        @Override
+        public void inbound(String action, TransportRequest request) {}
+    };
+
+    /**
+     * Called just after the given request was received by the transport. Any exception
+     * thrown by this method will stop the request from being handled and the error will
+     * be sent back to the sender.
+     */
+    void inbound(String action, TransportRequest request);
+
+}
