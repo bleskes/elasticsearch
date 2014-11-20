e406bbcda77848e61671af241de78b38b7b3d041
Dimitrios Athanasiou <dimitris@prelert.com>
2014-11-20 15:18:04 +0000

    Extract data streaming into its own class
    Former-commit-id: 21b7bcd812469c4a86f7230d9499ffe486202afc
    
    Original commit: elastic/x-pack@7d1c8d3bfd96645d055e87bea63b691a17c4d86e

--- java/apps/engineApi/engine-api/src/main/java/com/prelert/rs/resources/Data.java
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/rs/resources/Data.java
@@ -2,13 +2,6 @@ package com.prelert.rs.resources;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.ZipException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -21,7 +14,6 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.prelert.job.JobInUseException;
 import com.prelert.job.TooManyJobsException;
 import com.prelert.job.UnknownJobException;
@@ -30,135 +22,61 @@ import com.prelert.job.process.MissingFieldException;
 import com.prelert.job.process.NativeProcessRunException;
 import com.prelert.job.status.HighProportionOfBadTimestampsException;
 import com.prelert.job.status.OutOfOrderRecordsException;
-import com.prelert.rs.data.ErrorCode;
-import com.prelert.rs.provider.RestApiException;
-import com.prelert.rs.streaminginterceptor.StreamingInterceptor;
+import com.prelert.rs.resources.data.DataStreamer;
 
 
 /**
  * Streaming data endpoint
- * 
+ *
  * <pre>curl -X POST 'http://localhost:8080/api/data/<jobid>/' --data @<filename></pre>
  * <br/>
- * Binary gzipped files must be POSTed with the --data-binary option 
+ * Binary gzipped files must be POSTed with the --data-binary option
  * <pre>curl -X POST 'http://localhost:8080/api/data/<jobid>/' --data-binary @<filename.gz></pre>
  *
  */
 @Path("/data")
 public class Data extends ResourceWithJobManager
-{   
-	static final private Logger s_Logger = Logger.getLogger(Data.class);
-	
-	/**
-	 * Persisted data files are named with this date format
-	 * e.g. Tue_22_Apr_2014_091033
-	 */
-	static final private SimpleDateFormat s_PersistedFileNameDateFormat = 
-			new SimpleDateFormat("EEE_d_MMM_yyyy_HHmmss");
-	
-	/**
-	 * The name of this endpoint
-	 */
+{
+    static final private Logger s_Logger = Logger.getLogger(Data.class);
+
+    /**
+     * The name of this endpoint
+     */
 	static final public String ENDPOINT = "data";
-	
-	private boolean m_isPersistData;
-	private String m_BaseDirectory;  
-	
-	public Data()
-	{
-		// should we save uploaded data and where
-		m_BaseDirectory = System.getProperty("persistbasedir");
-		m_isPersistData = m_BaseDirectory != null;	
-	}
 
-
-	/**
-	 * Data upload endpoint.
-	 * 
-	 * @param headers
-	 * @param jobId
-	 * @param input
-	 * @return
-	 * @throws IOException
-	 * @throws UnknownJobException
-	 * @throws NativeProcessRunException
-	 * @throws MissingFieldException
-     * @throws JobInUseException if the data cannot be written to because 
-	 * the job is already handling data
-	 * @throws HighProportionOfBadTimestampsException 
-	 * @throws OutOfOrderRecordsException 
-	 * @throws TooManyJobsException If the license is violated
-	 */
+    /**
+     * Data upload endpoint.
+     *
+     * @param headers
+     * @param jobId
+     * @param input
+     * @return
+     * @throws IOException
+     * @throws UnknownJobException
+     * @throws NativeProcessRunException
+     * @throws MissingFieldException
+     * @throws JobInUseException if the data cannot be written to because
+     * the job is already handling data
+     * @throws HighProportionOfBadTimestampsException
+     * @throws OutOfOrderRecordsException
+     * @throws TooManyJobsException If the license is violated
+     */
     @POST
     @Path("/{jobId}")
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON,
-    	MediaType.APPLICATION_OCTET_STREAM})
+        MediaType.APPLICATION_OCTET_STREAM})
     public Response streamData(@Context HttpHeaders headers,
-    		@PathParam("jobId") String jobId, InputStream input)  
+            @PathParam("jobId") String jobId, InputStream input)
     throws IOException, UnknownJobException, NativeProcessRunException,
-    	MissingFieldException, JobInUseException, HighProportionOfBadTimestampsException,
-    	OutOfOrderRecordsException, TooManyJobsException
-    {   	   	
-    	s_Logger.debug("Handle Post data to job = " + jobId);
-    	
-    	String contentEncoding = headers.getHeaderString(HttpHeaders.CONTENT_ENCODING);
-    	if (contentEncoding!= null && contentEncoding.equals("gzip"))
-    	{
-    		s_Logger.info("Decompressing post data in job = " + jobId);
-    		try
-    		{
-    			input = new GZIPInputStream(input);
-    		}
-    		catch (ZipException ze)
-    		{
-    			throw new RestApiException("Content-Encoding = gzip "
-    					+ "but the data is not in gzip format",
-    					ErrorCode.UNCOMPRESSED_DATA,
-    					Response.Status.BAD_REQUEST);
-    		}
-    	}
-    	
-    	
-    	if (m_isPersistData)
-    	{   	
-    		try
-    		{
-    			Files.createDirectory(FileSystems.getDefault().getPath(
-    					m_BaseDirectory, jobId));
-    		}
-    		catch (FileAlreadyExistsException e)
-    		{
-    			// continue
-    		}
-    		
-    		java.nio.file.Path filePath = FileSystems.getDefault().getPath(
-    				m_BaseDirectory, jobId, s_PersistedFileNameDateFormat.format(new Date()) + ".gz"); 
-    		
-    		s_Logger.info("Data will be persisted to: " + filePath);
-    		
-    		// Create the interceptor for writing data to disk 
-    		// and start running in a new thread.
-    		final StreamingInterceptor si = new StreamingInterceptor(filePath);
-    		final InputStream uploadStream = input;
-    		
-    		input = si.createStream();
-    		
-    		new Thread() {
-    			@Override
-    			public void run()
-    			{
-    				si.pump(uploadStream);
-    			}
-    		}.start();
-    	}
-    	
-   		handleStream(jobId, input);    
-    	
-    	s_Logger.debug("File uploaded to job " + jobId);
-    	return Response.accepted().build();
+            MissingFieldException, JobInUseException, HighProportionOfBadTimestampsException,
+            OutOfOrderRecordsException, TooManyJobsException
+    {
+        DataStreamer dataStreamer = new DataStreamer(jobManager());
+        dataStreamer.streamData(headers, jobId, input);
+        return Response.accepted().build();
     }
-     
-    
+
+
     /**
      * Calling this endpoint indicates that data transfer is complete.
      * The job is retired and cleaned up after this
@@ -166,47 +84,19 @@ public class Data extends ResourceWithJobManager
      * @return
      * @throws UnknownJobException
      * @throws NativeProcessRunException
-     * @throws JobInUseException 
+     * @throws JobInUseException
      */
     @Path("/{jobId}/close")
     @POST
-    public Response commitUpload(@PathParam("jobId") String jobId) 
+    public Response commitUpload(@PathParam("jobId") String jobId)
     throws UnknownJobException, NativeProcessRunException, JobInUseException
-    {   	
-    	s_Logger.debug("Post to close data upload for job " + jobId);
-
-    	JobManager manager = jobManager();
-    	manager.finishJob(jobId);
-    	
-   		s_Logger.debug("Process finished successfully, Job Id = '" + jobId + "'");    		
-   		return Response.accepted().build();
-    }
-    
-    /**
-     * Pass the data stream to the native process. 
-     *    
-     * @param jobId
-     * @param input
-     * @return 
-     * 
-	 * @throws NativeProcessRunException If there is an error starting the native 
-	 * process
-	 * @throws UnknownJobException If the jobId is not recognised
-	 * @throws MissingFieldException If a configured field is missing from 
-	 * the CSV header
-     * @throws JsonParseException 
-     * @throws JobInUseException if the data cannot be written to because 
-	 * the job is already handling data
-     * @throws HighProportionOfBadTimestampsException 
-     * @throws OutOfOrderRecordsException 
-	 * @throws TooManyJobsException If the license is violated
-	 */
-    private boolean handleStream(String jobId, InputStream input)
-    throws NativeProcessRunException, UnknownJobException, MissingFieldException, 
-    JsonParseException, JobInUseException, HighProportionOfBadTimestampsException,
-    OutOfOrderRecordsException, TooManyJobsException
     {
-    	JobManager manager = jobManager();
-		return manager.dataToJob(jobId, input);
+        s_Logger.debug("Post to close data upload for job " + jobId);
+
+        JobManager manager = jobManager();
+        manager.finishJob(jobId);
+
+        s_Logger.debug("Process finished successfully, Job Id = '" + jobId + "'");
+        return Response.accepted().build();
     }
 }
