5109b4bea6121258071217de01495d3027f67e0e
Brian Murphy <brian.p.murphy@gmail.com>
2014-11-26 14:07:33 +0000

    TEST: This test got forgotten when I squash the commits.
    Original commit: elastic/x-pack@d41b3cd8b19725f5021d7ae7a2f7b2bfcd18a747

---
+++ src/test/java/org/elasticsearch/alerts/ActionHistoryIndexNameTest.java
@@ -0,0 +1,40 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.alerts;
+
+import org.elasticsearch.alerts.actions.AlertActionManager;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.DateTimeZone;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ */
+public class ActionHistoryIndexNameTest extends ElasticsearchTestCase {
+
+    @Test
+    public void testActionHistoryNameTest() {
+        assertThat(AlertActionManager.getAlertHistoryIndexNameForTime(new DateTime(0, DateTimeZone.UTC)), equalTo(".alert_history_1970-01-01"));
+        assertThat(AlertActionManager.getAlertHistoryIndexNameForTime(new DateTime(100000000000L, DateTimeZone.UTC)), equalTo(".alert_history_1973-03-03"));
+        assertThat(AlertActionManager.getAlertHistoryIndexNameForTime(new DateTime(1416582852000L, DateTimeZone.UTC)), equalTo(".alert_history_2014-11-21"));
+        assertThat(AlertActionManager.getAlertHistoryIndexNameForTime(new DateTime(2833165811000L, DateTimeZone.UTC)), equalTo(".alert_history_2059-10-12"));
+    }
+
+}
