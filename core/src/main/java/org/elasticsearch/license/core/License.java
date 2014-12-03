d606beeb148d4da7620bcf6dc4560b266267322c
Areek Zillur <areek.zillur@elasticsearch.com>
2014-12-03 17:28:21 -0500

    Add LicenseVersion to track version
    closes elastic/x-pack#23
    
    Original commit: elastic/x-pack@7750c02f12b9e7b506fad84ff4f299b31b0d76fc

--- core/src/main/java/org/elasticsearch/license/core/License.java
+++ core/src/main/java/org/elasticsearch/license/core/License.java
@@ -17,6 +17,7 @@
  */
 package org.elasticsearch.license.core;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
@@ -32,6 +33,8 @@ import java.io.IOException;
  * Provides serialization/deserialization & validation methods for license object
  */
 public class License implements ToXContent {
+    public final static int VERSION_START = 1;
+    public final static int VERSION_CURRENT = VERSION_START;
 
     private final String uid;
     private final String issuer;
@@ -154,7 +157,10 @@ public class License implements ToXContent {
     }
 
     static License readLicense(StreamInput in) throws IOException {
-        in.readVInt(); // Version for future extensibility
+        int version = in.readVInt(); // Version for future extensibility
+        if (version > VERSION_CURRENT) {
+            throw new ElasticsearchException("Unknown license version found, please upgrade all nodes to the latest elasticsearch-license plugin");
+        }
         Builder builder = builder();
         builder.uid(in.readString());
         builder.type(in.readString());
@@ -170,7 +176,7 @@ public class License implements ToXContent {
     }
 
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeVInt(VERSION);
+        out.writeVInt(VERSION_CURRENT);
         out.writeString(uid);
         out.writeString(type);
         out.writeString(subscriptionType);
@@ -218,7 +224,6 @@ public class License implements ToXContent {
         return builder;
     }
 
-    private final static int VERSION = 1;
 
     final static class Fields {
         static final String STATUS = "status";
