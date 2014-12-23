7a131bc4d882af49bfb60e005804d2bd6d3e094f
Martijn van Groningen <martijn.v.groningen@gmail.com>
2014-12-23 16:41:16 +0100

    slightly improved logging.
    Original commit: elastic/x-pack@5c920d47065efc0ce40008e12cf125c8c0093fbb

--- src/main/java/org/elasticsearch/alerts/AlertsStore.java
+++ src/main/java/org/elasticsearch/alerts/AlertsStore.java
@@ -170,7 +170,7 @@ public class AlertsStore extends AbstractComponent {
                 try {
                     loadAlerts();
                 } catch (Exception e) {
-                    logger.warn("Failed to load alerts", e);
+                    logger.warn("Failed to load previously stored alerts. Schedule to retry alert loading...", e);
                     alertMap.clear();
                     return false;
                 }
@@ -178,7 +178,7 @@ public class AlertsStore extends AbstractComponent {
                 started.set(true);
                 return true;
             } else {
-                logger.info("Not all primary shards of the .alerts index are started");
+                logger.warn("Not all primary shards of the .alerts index are started. Schedule to retry alert loading...");
                 return false;
             }
         } else {
