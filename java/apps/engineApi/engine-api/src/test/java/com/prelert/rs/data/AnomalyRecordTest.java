d6ea321696e784df823d40f032f3cc12ca3b7428
Dimitrios Athanasiou <dimitris@prelert.com>
2014-12-31 15:28:04 +0000

    Add a few unit tests for AnomalyRecord
    Former-commit-id: 7844051e3d04ee7b235a40fe901faef5fba31535
    
    Original commit: elastic/x-pack@cf4443c907c2bf6b3d8ec64df033ed59f40bc0fc

---
+++ java/apps/engineApi/engine-api/src/test/java/com/prelert/rs/data/AnomalyRecordTest.java
@@ -0,0 +1,71 @@
+/************************************************************
+ *                                                          *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2014     *
+ *                                                          *
+ *----------------------------------------------------------*
+ *----------------------------------------------------------*
+ * WARNING:                                                 *
+ * THIS FILE CONTAINS UNPUBLISHED PROPRIETARY               *
+ * SOURCE CODE WHICH IS THE PROPERTY OF PRELERT LTD AND     *
+ * PARENT OR SUBSIDIARY COMPANIES.                          *
+ * PLEASE READ THE FOLLOWING AND TAKE CAREFUL NOTE:         *
+ *                                                          *
+ * This source code is confidential and any person who      *
+ * receives a copy of it, or believes that they are viewing *
+ * it without permission is asked to notify Prelert Ltd     *
+ * on +44 (0)20 3567 1249 or email to legal@prelert.com.    *
+ * All intellectual property rights in this source code     *
+ * are owned by Prelert Ltd.  No part of this source code   *
+ * may be reproduced, adapted or transmitted in any form or *
+ * by any means, electronic, mechanical, photocopying,      *
+ * recording or otherwise.                                  *
+ *                                                          *
+ *----------------------------------------------------------*
+ *                                                          *
+ *                                                          *
+ ************************************************************/
+
+package com.prelert.rs.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class AnomalyRecordTest
+{
+
+    @Test
+    public void testGetId_GivenIdIsZero()
+    {
+        AnomalyRecord anomalyRecord = new AnomalyRecord();
+        anomalyRecord.setId("0");
+
+        assertNull(anomalyRecord.getId());
+    }
+
+    @Test
+    public void testSetId_GivenNoPartitionField()
+    {
+        AnomalyRecord anomalyRecord = new AnomalyRecord();
+
+        anomalyRecord.setId("1403701200individual metric/1");
+
+        assertEquals("1403701200", anomalyRecord.getParent());
+        assertEquals("1403701200individual metric/1", anomalyRecord.getId());
+    }
+
+    @Test
+    public void testSetId_GivenPartitionField()
+    {
+        AnomalyRecord anomalyRecord = new AnomalyRecord();
+        anomalyRecord.setFieldName("responsetime");
+        anomalyRecord.setPartitionFieldName("airline");
+
+        anomalyRecord.setId("1403701200individual metric/0/0/responsetime/airline/1");
+
+        assertEquals("1403701200", anomalyRecord.getParent());
+        assertEquals("1403701200individual metric/0/0/responsetime/airline/1",
+                anomalyRecord.getId());
+    }
+}
