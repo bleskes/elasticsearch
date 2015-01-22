e7fc03d29a23f9a01dc1a3f71b4e1ae6ba5594c8
David Roberts <dave@prelert.com>
2015-01-22 12:01:44 +0000

    Bug 763 - Don't be silent if an unexpected exception occurs
    Former-commit-id: 609fba60993e5b365951fb8401468cf9d6c557a4
    
    Original commit: elastic/x-pack@0313a8e5fd6c0ede7df8e2cdb9deabdace7efbc5

--- java/apps/engineApi/apiServer/src/main/java/com/prelert/jetty/ApiErrorHandler.java
+++ java/apps/engineApi/apiServer/src/main/java/com/prelert/jetty/ApiErrorHandler.java
@@ -1,6 +1,6 @@
 /************************************************************
  *                                                          *
- * Contents of file Copyright (c) Prelert Ltd 2006-2014     *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2015     *
  *                                                          *
  *----------------------------------------------------------*
  *----------------------------------------------------------*
@@ -26,25 +26,84 @@
  ************************************************************/
 package com.prelert.jetty;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.handler.ErrorHandler;
 
-/**
- * A Servlet error handler that performs no extra processing.
- * The main purpose of this class is to override the default
- * Jetty implementation which returns Jetty branded HTML. 
- */
-public class ApiErrorHandler extends ErrorHandler 
-{
-	@Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request, 
-			HttpServletResponse response)
-	{
-		// do nothing to the response object
-	}
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
 
+/**
+ * A Servlet error handler that overrides the default Jetty
+ * handler which returns Jetty branded HTML.  Instead of this
+ * HTML we return some JSON in the same format as our
+ * documented error responses.
+ *
+ * This handler can get called in two situations:
+ * 1) A bug in the Jetty code
+ * 2) An unchecked exception being thrown by our code
+ *
+ * Testing shows that in case (2) the reason is not particularly
+ * helpful and the cause is sometimes null, so this is not
+ * great.  However, it's better than being completely silent
+ * when an unexpected error occurs in our code.  Also, in case
+ * (1) the output will be more informative.
+ */
+public class ApiErrorHandler extends ErrorHandler
+{
+    private static final Logger LOGGER = Logger.getLogger(ApiErrorHandler.class);
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request,
+            HttpServletResponse response)
+    throws IOException
+    {
+        JsonStringEncoder encoder = JsonStringEncoder.getInstance();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append('{');
+
+        if (response instanceof Response)
+        {
+            Response serverResponse = (Response)response;
+            String reason = serverResponse.getReason();
+            if (reason != null && !reason.isEmpty())
+            {
+                LOGGER.error("Unexpected server failure: " + reason);
+
+                char [] message = encoder.quoteAsString(reason);
+                builder.append("\n  \"message\" : \"").append(message).append("\",");
+            }
+        }
+
+        // We don't have a clue what caused the underlying error in this case,
+        // so just set the error code to 0
+        builder.append("\n  \"errorCode\" : 0");
+
+        Throwable th = (Throwable)request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        if (th != null)
+        {
+            LOGGER.error("Unexpected server exception was thrown", th);
+
+            char [] cause = encoder.quoteAsString(th.toString());
+            builder.append(",\n  \"cause\" : \"").append(cause).append('"');
+        }
+
+        builder.append("\n}\n");
+
+        String errorText = builder.toString();
+
+        response.setContentLength(errorText.length());
+        Writer writer = response.getWriter();
+        writer.write(errorText);
+        writer.flush();
+    }
 
 }
