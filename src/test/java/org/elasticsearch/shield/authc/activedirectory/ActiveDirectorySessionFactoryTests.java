31b79d51ac26af8a5042f25d8d5e992993e0c4a7
jaymode <jay.modi@elasticsearch.com>
2015-02-18 20:02:59 -0500

    [Test] AwaitsFix for tcp timeout tests. See elastic/x-pack#499
    Original commit: elastic/x-pack@598b36c0a790156be7ca21a7de7081fc947cc198

--- src/test/java/org/elasticsearch/shield/authc/activedirectory/ActiveDirectorySessionFactoryTests.java
+++ src/test/java/org/elasticsearch/shield/authc/activedirectory/ActiveDirectorySessionFactoryTests.java
@@ -17,6 +17,7 @@
 
 package org.elasticsearch.shield.authc.activedirectory;
 
+import org.apache.lucene.util.LuceneTestCase.AwaitsFix;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.shield.authc.RealmConfig;
@@ -86,6 +87,7 @@ public class ActiveDirectorySessionFactoryTests extends ElasticsearchTestCase {
     }
 
     @Test
+    @AwaitsFix(bugUrl = "https://github.com/elasticsearch/elasticsearch-shield/issues/499")
     public void testTcpReadTimeout() {
         Settings settings = ImmutableSettings.builder()
                 .put(buildAdSettings(AD_LDAP_URL, AD_DOMAIN, false))
