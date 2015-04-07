5baf1885d62f6477ffd91b4f81033e41e17ac140
Dimitrios Athanasiou <dimitris@prelert.com>
2015-04-07 11:35:16 +0100

    Extract skeleton of Data and DataLoad endpoints into its own class
    Former-commit-id: 7e6ed9cddef812b392dfbca1b234d711fc0e5f40
    
    Original commit: elastic/x-pack@87dc9da73719225f6f3c28bea720e6aab06bcac6

--- java/apps/engineApi/engine-api/src/main/java/com/prelert/rs/resources/Data.java
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/rs/resources/Data.java
@@ -1,6 +1,6 @@
 /************************************************************
  *                                                          *
- * Contents of file Copyright (c) Prelert Ltd 2006-2014     *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2015     *
  *                                                          *
  *----------------------------------------------------------*
  *----------------------------------------------------------*
@@ -26,34 +26,7 @@
  ************************************************************/
 package com.prelert.rs.resources;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.apache.log4j.Logger;
-
-import com.prelert.job.DataCounts;
-import com.prelert.job.exceptions.JobInUseException;
-import com.prelert.job.exceptions.TooManyJobsException;
-import com.prelert.job.exceptions.UnknownJobException;
-import com.prelert.job.process.exceptions.MalformedJsonException;
-import com.prelert.job.process.exceptions.MissingFieldException;
-import com.prelert.job.process.exceptions.NativeProcessRunException;
-import com.prelert.job.status.HighProportionOfBadTimestampsException;
-import com.prelert.job.status.OutOfOrderRecordsException;
-import com.prelert.rs.data.Acknowledgement;
-import com.prelert.rs.resources.data.AbstractDataStreamer;
-import com.prelert.rs.resources.data.DataStreamer;
 
 
 /**
@@ -66,99 +39,16 @@ import com.prelert.rs.resources.data.DataStreamer;
  *
  */
 @Path("/data")
-public class Data extends ResourceWithJobManager
+public class Data extends AbstractDataLoad
 {
-
-    private static final Logger LOGGER = Logger.getLogger(Data.class);
-
     /**
      * The name of this endpoint
      */
     public static final String ENDPOINT = "data";
 
-    /**
-     * Parameter to control whether interim results are calculated on a flush
-     */
-    public static final String CALC_INTERIM_PARAM = "calcInterim";
-
-    /**
-     * Data upload endpoint.
-     *
-     * @param headers
-     * @param jobId
-     * @param input
-     * @return
-     * @throws IOException
-     * @throws UnknownJobException
-     * @throws NativeProcessRunException
-     * @throws MissingFieldException
-     * @throws JobInUseException if the data cannot be written to because
-     * the job is already handling data
-     * @throws HighProportionOfBadTimestampsException
-     * @throws OutOfOrderRecordsException
-     * @throws TooManyJobsException If the license is violated
-     * @throws MalformedJsonException If JSON data is malformed and we cannot recover
-     */
-    @POST
-    @Path("/{jobId}")
-    @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON,
-        MediaType.APPLICATION_OCTET_STREAM})
-    public Response streamData(@Context HttpHeaders headers,
-            @PathParam("jobId") String jobId, InputStream input)
-    throws IOException, UnknownJobException, NativeProcessRunException,
-            MissingFieldException, JobInUseException, HighProportionOfBadTimestampsException,
-            OutOfOrderRecordsException, TooManyJobsException, MalformedJsonException
+    @Override
+    protected boolean shouldPersist()
     {
-        AbstractDataStreamer dataStreamer = new DataStreamer(jobManager());
-        String contentEncoding = headers.getHeaderString(HttpHeaders.CONTENT_ENCODING);
-        DataCounts stats = dataStreamer.streamData(contentEncoding, jobId, input);
-        return Response.accepted().entity(stats).build();
-    }
-
-
-    /**
-     * Calling this endpoint ensures that the native process has received all
-     * data that has been previously uploaded, and that none is being buffered.
-     * At present a flush is not permitted while a data upload in another
-     * thread is part way through.
-     * @param jobId
-     * @param calcInterim Should interim results be calculated based on the data
-     * up to the point of the flush?
-     * @return
-     * @throws UnknownJobException
-     * @throws NativeProcessRunException
-     * @throws JobInUseException if a data upload is part way through
-     */
-    @Path("/{jobId}/flush")
-    @POST
-    public Response flushUpload(@PathParam("jobId") String jobId,
-            @DefaultValue("false") @QueryParam(CALC_INTERIM_PARAM) boolean calcInterim)
-    throws UnknownJobException, NativeProcessRunException, JobInUseException
-    {
-        LOGGER.debug("Post to flush data upload for job " + jobId +
-                     " with " + CALC_INTERIM_PARAM + '=' + calcInterim);
-        jobManager().flushJob(jobId, calcInterim);
-        return Response.ok().entity(new Acknowledgement()).build();
-    }
-
-
-    /**
-     * Calling this endpoint indicates that data transfer is complete.
-     * The job is retired and cleaned up after this
-     * @param jobId
-     * @return
-     * @throws UnknownJobException
-     * @throws NativeProcessRunException
-     * @throws JobInUseException
-     */
-    @Path("/{jobId}/close")
-    @POST
-    public Response commitUpload(@PathParam("jobId") String jobId)
-    throws UnknownJobException, NativeProcessRunException, JobInUseException
-    {
-        LOGGER.debug("Post to close data upload for job " + jobId);
-        jobManager().finishJob(jobId);
-        LOGGER.debug("Process finished successfully, Job Id = '" + jobId + "'");
-        return Response.accepted().entity(new Acknowledgement()).build();
+        return false;
     }
 }
