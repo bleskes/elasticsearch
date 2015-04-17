bc237d1beb3d87f7b582748504475622fba72e6b
Brian Murphy <brian.p.murphy@gmail.com>
2015-04-17 09:45:16 -0400

    Support for Sanitized HTML in emails.
    This change adds a shaded dependency on owasp (https://code.google.com/p/owasp-java-html-sanitizer/) to add support for HTML Sanitization.
    Only images that reference an attachment are supported. This Sanitization may be customized for each email profile.
    Other dangerous behavior is suppressed.
    
    See elastic/x-pack#163
    Fixes elastic/x-pack#163
    
    Original commit: elastic/x-pack@701dc8d830f52809bb1006120ec8ac5399ade1ea

---
+++ src/test/java/org/elasticsearch/watcher/actions/email/service/HtmlSanitizeTests.java
@@ -0,0 +1,82 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.watcher.actions.email.service;
+
+import org.elasticsearch.common.collect.ImmutableMap;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ */
+public class HtmlSanitizeTests extends ElasticsearchTestCase {
+
+    @Test
+    public void test_HtmlSanitizer_onclick() {
+        String badHtml = "<button type=\"button\"" +
+                "onclick=\"document.getElementById('demo').innerHTML = Date()\">" +
+                "Click me to display Date and Time.</button>";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), badHtml);
+        assertThat(sanitizedHtml, equalTo("Click me to display Date and Time."));
+    }
+
+    @Test
+    public void test_HtmlSanitizer_Nonattachment_img() {
+        String badHtml = "<img src=\"http://test.com/nastyimage.jpg\"/>This is a bad image";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), badHtml);
+        assertThat(sanitizedHtml, equalTo("This is a bad image"));
+    }
+
+    @Test
+    public void test_HtmlSanitizer_Goodattachment_img() {
+        String goodHtml = "<img src=\"cid:foo\" />This is a good image";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), goodHtml);
+        assertThat(sanitizedHtml, equalTo(goodHtml));
+    }
+
+    @Test
+    public void test_HtmlSanitizer_table() {
+        String goodHtml = "<table><tr><td>cell1</td><td>cell2</td></tr></table>";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), goodHtml);
+        assertThat(sanitizedHtml, equalTo(goodHtml));
+
+    }
+
+    @Test
+    public void test_HtmlSanitizer_Badattachment_img() {
+        String goodHtml = "<img src=\"cid:bad\" />This is a bad image";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), goodHtml);
+        assertThat(sanitizedHtml, equalTo("This is a bad image"));
+    }
+
+    @Test
+    public void test_HtmlSanitizer_Script() {
+        String badHtml = "<script>doSomethingNefarious()</script>This was a dangerous script";
+        byte[] bytes = new byte[0];
+        String sanitizedHtml = Profile.sanitizeHtml(ImmutableMap.of("foo", (Attachment) new Attachment.Bytes("foo", bytes, "")), badHtml);
+        assertThat(sanitizedHtml, equalTo("This was a dangerous script"));
+    }
+
+
+}
