c5dde855d2f001d2bf672916385c3a6354749248
uboness <uboness@gmail.com>
2015-04-20 20:56:56 +0200

    Added the watch metadata to the template/script model
    The watch metadata is now accessible to the templates and scripts (under `ctx.metadata`).
    
    Also, changed the default email attachment to include the ctx model as a whole (not just the payload). This provides more context to the watch exectuion (e.g. watch id, execution time, metadata, etc...)
    
    Original commit: elastic/x-pack@0dc2cff6e2a3bb15508bbff159380f322e624e60

---
+++ src/test/java/org/elasticsearch/watcher/support/VariablesTests.java
@@ -0,0 +1,69 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.watcher.support;
+
+import org.elasticsearch.common.collect.ImmutableMap;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.elasticsearch.watcher.execution.WatchExecutionContext;
+import org.elasticsearch.watcher.test.WatcherTestUtils;
+import org.elasticsearch.watcher.trigger.TriggerEvent;
+import org.elasticsearch.watcher.trigger.schedule.ScheduleTriggerEvent;
+import org.elasticsearch.watcher.watch.Payload;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.elasticsearch.common.joda.time.DateTimeZone.UTC;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ */
+public class VariablesTests extends ElasticsearchTestCase {
+
+    @Test
+    public void testCreateCtxModel() throws Exception {
+        DateTime scheduledTime = DateTime.now(UTC);
+        DateTime triggeredTime = scheduledTime.plusMillis(50);
+        DateTime executionTime = triggeredTime.plusMillis(50);
+        Payload payload = new Payload.Simple(ImmutableMap.<String, Object>builder().put("payload_key", "payload_value").build());
+        Map<String, Object> metatdata = ImmutableMap.<String, Object>builder().put("metadata_key", "metadata_value").build();
+        TriggerEvent event = new ScheduleTriggerEvent(triggeredTime, scheduledTime);
+        WatchExecutionContext wec = WatcherTestUtils.mockExecutionContextBuilder("_watch_id")
+                .executionTime(executionTime)
+                .triggerEvent(event)
+                .payload(payload)
+                .metadata(metatdata)
+                .buildMock();
+
+        Map<String, Object> model = Variables.createCtxModel(wec, payload);
+        assertThat(model, notNullValue());
+        assertThat(model, hasKey(Variables.CTX));
+        assertThat(model.get(Variables.CTX), instanceOf(Map.class));
+        assertThat(model.size(), is(1));
+
+        Map<String, Object> ctx = (Map<String, Object>) model.get(Variables.CTX);
+        assertThat(ctx, hasEntry(Variables.WATCH_ID, (Object) "_watch_id"));
+        assertThat(ctx, hasEntry(Variables.EXECUTION_TIME, (Object) executionTime));
+        assertThat(ctx, hasEntry(Variables.TRIGGER, (Object) event.data()));
+        assertThat(ctx, hasEntry(Variables.PAYLOAD, (Object) payload.data()));
+        assertThat(ctx, hasEntry(Variables.METADATA, (Object) metatdata));
+        assertThat(ctx.size(), is(5));
+    }
+}
