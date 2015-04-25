3e9332df946d0785e1118642ef656cd3a4f4cef9
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-04-25 17:42:48 +0200

    test: unset 'shieldEnabled' and 'scheduleEngine' after test class is completed.
    Original commit: elastic/x-pack@232f24bea10e1ff6e8e2e393b282a5cf130ee029

--- src/test/java/org/elasticsearch/watcher/test/AbstractWatcherIntegrationTests.java
+++ src/test/java/org/elasticsearch/watcher/test/AbstractWatcherIntegrationTests.java
@@ -72,6 +72,7 @@ import org.elasticsearch.watcher.trigger.schedule.Schedules;
 import org.elasticsearch.watcher.watch.Watch;
 import org.elasticsearch.watcher.WatcherService;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 
 import java.io.File;
@@ -103,13 +104,6 @@ public abstract class AbstractWatcherIntegrationTests extends ElasticsearchInteg
 
     @Override
     protected Settings nodeSettings(int nodeOrdinal) {
-        if (scheduleEngine == null) {
-            scheduleEngine = randomFrom(ScheduleModule.Engine.values());
-        }
-        if (shieldEnabled == null) {
-            shieldEnabled = enableShield();
-        }
-
         String scheduleImplName = scheduleEngine().name().toLowerCase(Locale.ROOT);
         logger.info("using schedule engine [" + scheduleImplName + "]");
         return ImmutableSettings.builder()
@@ -170,6 +164,12 @@ public abstract class AbstractWatcherIntegrationTests extends ElasticsearchInteg
         // Clear all internal watcher state for the next test method:
         logger.info("[{}#{}]: clearing watcher state", getTestClass().getSimpleName(), getTestName());
         stopWatcher();
+    }
+
+    @AfterClass
+    public static void _cleanupClass() {
+        shieldEnabled = null;
+        scheduleEngine = null;
     }
 
     @Override
@@ -515,6 +515,12 @@ public abstract class AbstractWatcherIntegrationTests extends ElasticsearchInteg
 
         @Override
         public void beforeTest(Random random, double transportClientRatio) throws IOException {
+            if (scheduleEngine == null) {
+                scheduleEngine = randomFrom(ScheduleModule.Engine.values());
+            }
+            if (shieldEnabled == null) {
+                shieldEnabled = enableShield();
+            }
             testCluster.beforeTest(random, transportClientRatio);
         }
 
