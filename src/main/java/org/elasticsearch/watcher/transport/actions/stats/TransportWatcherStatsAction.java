03c52668e0c5a7e02bd8059bb35efaf7baf54cfa
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-05-13 10:44:15 +0200

    Use ThreadPool.Names.SAME executor in stats and service apis.
    Original commit: elastic/x-pack@bd936002c7e3dde94d8a5a1cb42f7aaaac04a770

--- src/main/java/org/elasticsearch/watcher/transport/actions/stats/TransportWatcherStatsAction.java
+++ src/main/java/org/elasticsearch/watcher/transport/actions/stats/TransportWatcherStatsAction.java
@@ -54,7 +54,8 @@ public class TransportWatcherStatsAction extends WatcherTransportAction<WatcherS
 
     @Override
     protected String executor() {
-        return ThreadPool.Names.MANAGEMENT;
+        // cheap operation, no need to fork into another thread
+        return ThreadPool.Names.SAME;
     }
 
     @Override
