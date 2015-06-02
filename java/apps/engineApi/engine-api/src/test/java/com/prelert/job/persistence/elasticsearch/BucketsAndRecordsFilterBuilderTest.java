ca599ee23c65d08e0532e22bbc763c9380aa3ed9
Dimitrios Athanasiou <dimitris@prelert.com>
2015-06-02 14:30:07 +0100

    Extract buckets and records filter builder into its own class
    Former-commit-id: 47490de4d42e67da6335e9fa28b80d97057b362b
    
    Original commit: elastic/x-pack@e107524c701255f10b9dfa22e2b082d4b0f69514

---
+++ java/apps/engineApi/engine-api/src/test/java/com/prelert/job/persistence/elasticsearch/BucketsAndRecordsFilterBuilderTest.java
@@ -0,0 +1,203 @@
+/************************************************************
+ *                                                          *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2015     *
+ *                                                          *
+ *----------------------------------------------------------*
+ *----------------------------------------------------------*
+ * WARNING:                                                 *
+ * THIS FILE CONTAINS UNPUBLISHED PROPRIETARY               *
+ * SOURCE CODE WHICH IS THE PROPERTY OF PRELERT LTD AND     *
+ * PARENT OR SUBSIDIARY COMPANIES.                          *
+ * PLEASE READ THE FOLLOWING AND TAKE CAREFUL NOTE:         *
+ *                                                          *
+ * This source code is confidential and any person who      *
+ * receives a copy of it, or believes that they are viewing *
+ * it without permission is asked to notify Prelert Ltd     *
+ * on +44 (0)20 3567 1249 or email to legal@prelert.com.    *
+ * All intellectual property rights in this source code     *
+ * are owned by Prelert Ltd.  No part of this source code   *
+ * may be reproduced, adapted or transmitted in any form or *
+ * by any means, electronic, mechanical, photocopying,      *
+ * recording or otherwise.                                  *
+ *                                                          *
+ *----------------------------------------------------------*
+ *                                                          *
+ *                                                          *
+ ************************************************************/
+
+package com.prelert.job.persistence.elasticsearch;
+
+import static org.junit.Assert.assertEquals;
+
+import org.elasticsearch.index.query.FilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.junit.Test;
+
+public class BucketsAndRecordsFilterBuilderTest
+{
+    @Test
+    public void testBuild_GivenNoFilters()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder().build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenFilterInCtorButNoAdditionalFilters()
+    {
+        FilterBuilder originalFilter = FilterBuilders.limitFilter(42);
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder(originalFilter).build();
+
+        assertEquals(originalFilter.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenZeroStartAndEndTime()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .timeRange(0, 0)
+                .build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenOnlyStartTime()
+    {
+        FilterBuilder expected = FilterBuilders
+                .rangeFilter(ElasticsearchMappings.ES_TIMESTAMP)
+                .gte(1000);
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .timeRange(1000, 0)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenOnlyEndTime()
+    {
+        FilterBuilder expected = FilterBuilders
+                .rangeFilter(ElasticsearchMappings.ES_TIMESTAMP)
+                .lt(2000);
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .timeRange(0, 2000)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenStartAndEndTime()
+    {
+        FilterBuilder expected = FilterBuilders
+                .rangeFilter(ElasticsearchMappings.ES_TIMESTAMP)
+                .gte(40)
+                .lt(50);
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .timeRange(40, 50)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenNegativeStartAndEndTime()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .timeRange(-10, -5)
+                .build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenZeroScore()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .score("someField", 0.0)
+                .build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenNegativeScore()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .score("someField", -10.0)
+                .build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenPositiveScore()
+    {
+        FilterBuilder expected = FilterBuilders
+                .rangeFilter("someField")
+                .gte(40.3);
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .score("someField", 40.3)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenInterimTrue()
+    {
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .interim("isInterim", true)
+                .build();
+
+        assertEquals(FilterBuilders.matchAllFilter().toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenInterimFalse()
+    {
+        FilterBuilder expected = FilterBuilders.notFilter(FilterBuilders.termFilter("isInterim",
+                Boolean.TRUE.toString()));
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder()
+                .interim("isInterim", false)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+
+    @Test
+    public void testBuild_GivenCombination()
+    {
+        FilterBuilder originalFilter = FilterBuilders.limitFilter(42);
+        FilterBuilder timeFilter = FilterBuilders
+                .rangeFilter(ElasticsearchMappings.ES_TIMESTAMP)
+                .gte(1000)
+                .lt(2000);
+        FilterBuilder score1Filter = new BucketsAndRecordsFilterBuilder()
+                .score("score1", 50.0)
+                .build();
+        FilterBuilder score2Filter = new BucketsAndRecordsFilterBuilder()
+                .score("score2", 80.0)
+                .build();
+        FilterBuilder interimFilter = FilterBuilders.notFilter(FilterBuilders.termFilter(
+                "isInterim", Boolean.TRUE.toString()));
+        FilterBuilder expected = FilterBuilders.andFilter(originalFilter, timeFilter, score1Filter,
+                score2Filter, interimFilter);
+
+        FilterBuilder fb = new BucketsAndRecordsFilterBuilder(originalFilter)
+                .timeRange(1000, 2000)
+                .score("score1", 50.0)
+                .score("score2", 80.0)
+                .interim("isInterim", false)
+                .build();
+
+        assertEquals(expected.toString(), fb.toString());
+    }
+}
