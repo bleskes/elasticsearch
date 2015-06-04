ef0872884b426498d3d6a2df5a3c44f8db2522e8
Dimitrios Athanasiou <dimitris@prelert.com>
2015-06-04 18:28:58 +0100

    Bug 1079 - Add lowercase and uppercase transforms
    Former-commit-id: f5cf1fdabe1188fc84e0e56f6bfdc06610948b30
    
    Original commit: elastic/x-pack@fe3d01e9dfef6c21488a71d1ce4df425300bf850

---
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/transforms/CaseTransform.java
@@ -0,0 +1,71 @@
+/************************************************************
+ *                                                          *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2015     *
+ *                                                          *
+ *----------------------------------------------------------*
+ *----------------------------------------------------------*
+ * WARNING:                                                 *
+ * THIS FILE CONTAINS UNPUBLISHED PROPRIETARY               *
+ * SOURCE CODE WHICH IS THE PROPERTY OF PRELERT LTD AND     *
+ * PARENT OR SUBSIDIARY COMPANIES.                          *
+ * PLEASE READ THE FOLLOWING AND TAKE CAREFUL NOTE:         *
+ *                                                          *
+ * This source code is confidential and any person who      *
+ * receives a copy of it, or believes that they are viewing *
+ * it without permission is asked to notify Prelert Ltd     *
+ * on +44 (0)20 3567 1249 or email to legal@prelert.com.    *
+ * All intellectual property rights in this source code     *
+ * are owned by Prelert Ltd.  No part of this source code   *
+ * may be reproduced, adapted or transmitted in any form or *
+ * by any means, electronic, mechanical, photocopying,      *
+ * recording or otherwise.                                  *
+ *                                                          *
+ *----------------------------------------------------------*
+ *                                                          *
+ *                                                          *
+ ************************************************************/
+
+package com.prelert.transforms;
+
+import java.util.List;
+import java.util.function.Function;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.base.Preconditions;
+
+public class CaseTransform extends Transform
+{
+    private final Function<String, String> m_ConvertFunction;
+
+    public static CaseTransform createLowerCase(List<TransformIndex> readIndicies,
+            List<TransformIndex> writeIndicies, Logger logger)
+    {
+        return new CaseTransform(s -> s.toLowerCase(), readIndicies, writeIndicies, logger);
+    }
+
+    public static CaseTransform createUpperCase(List<TransformIndex> readIndicies,
+            List<TransformIndex> writeIndicies, Logger logger)
+    {
+        return new CaseTransform(s -> s.toUpperCase(), readIndicies, writeIndicies, logger);
+    }
+
+    private CaseTransform(Function<String, String> convertFunction,
+            List<TransformIndex> readIndicies, List<TransformIndex> writeIndicies, Logger logger)
+    {
+        super(readIndicies, writeIndicies, logger);
+        m_ConvertFunction = convertFunction;
+        Preconditions.checkArgument(readIndicies.size() == 1);
+        Preconditions.checkArgument(writeIndicies.size() == 1);
+    }
+
+    @Override
+    public TransformResult transform(String[][] readWriteArea) throws TransformException
+    {
+        TransformIndex readIndex = m_ReadIndicies.get(0);
+        TransformIndex writeIndex = m_WriteIndicies.get(0);
+        String input = readWriteArea[readIndex.array][readIndex.index];
+        readWriteArea[writeIndex.array][writeIndex.index] = m_ConvertFunction.apply(input);
+        return TransformResult.OK;
+    }
+}
