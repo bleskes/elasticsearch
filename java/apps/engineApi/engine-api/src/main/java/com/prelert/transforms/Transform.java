9656b0d293b42c4e024a788b73df6c68716e92b7
Dimitrios Athanasiou <dimitris@prelert.com>
2015-06-11 09:53:22 +0100

    Use Objects#hashCode in TransformIndex
    Former-commit-id: fc76928e3b2fd88edaedf50221d8f6d5b41ce2e1
    
    Original commit: elastic/x-pack@e73ce0ab18f5b19fd231e77ead1387473f4a5217

--- java/apps/engineApi/engine-api/src/main/java/com/prelert/transforms/Transform.java
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/transforms/Transform.java
@@ -28,6 +28,7 @@
 package com.prelert.transforms;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.log4j.Logger;
 
@@ -67,11 +68,7 @@ public abstract class Transform
         @Override
         public int hashCode()
         {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + array;
-            result = prime * result + index;
-            return result;
+            return Objects.hash(array, index);
         }
 
         @Override
