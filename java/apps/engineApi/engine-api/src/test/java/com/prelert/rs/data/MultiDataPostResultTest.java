cff8b02d69142e0fe1b0ef5011a4a839758ffd2f
David Kyle <David.Kyle@prelert.com>
2015-06-11 12:16:29 +0100

    Unit test for MultiDataPostResult
    Former-commit-id: a18714bb6cef05367616fdb4ccae00234794a532
    
    Original commit: elastic/x-pack@fd327ba99e6f5cdd0fc868bc62c4fa7d61803c30

---
+++ java/apps/engineApi/engine-api/src/test/java/com/prelert/rs/data/MultiDataPostResultTest.java
@@ -0,0 +1,84 @@
+/************************************************************
+ *                                                          *
+ * Contents of file Copyright (c) Prelert Ltd 2006-2015     *
+ *                                                          *
+ *----------------------------------------------------------*
+ *----------------------------------------------------------*
+ * WARNING:                                                 *
+ * THIS FILE CONTAINS UNPUBLISHED PROPRIETARY               *
+ * SOURCE CODE WHICH IS THE PROPERTY OF PRELERT LTD AND     *
+ * PARENT OR SUBSIDIARY COMPANIES.                          *
+ * PLEASE READ THE FOLLOWING AND TAKE CAREFUL NOTE:         *
+ *                                                          *
+ * This source code is confidential and any person who      *
+ * receives a copy of it, or believes that they are viewing *
+ * it without permission is asked to notify Prelert Ltd     *
+ * on +44 (0)20 3567 1249 or email to legal@prelert.com.    *
+ * All intellectual property rights in this source code     *
+ * are owned by Prelert Ltd.  No part of this source code   *
+ * may be reproduced, adapted or transmitted in any form or *
+ * by any means, electronic, mechanical, photocopying,      *
+ * recording or otherwise.                                  *
+ *                                                          *
+ *----------------------------------------------------------*
+ *                                                          *
+ *                                                          *
+ ************************************************************/
+package com.prelert.rs.data;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.prelert.job.DataCounts;
+import com.prelert.job.errorcodes.ErrorCodes;
+
+public class MultiDataPostResultTest {
+
+    @Test
+    public void testEqualsAndHashCode()
+    {
+        DataCounts dc = createCounts(20, 30, 40, 10, 5, 15, 25, 35, 45, 50);
+        DataPostResult dp = new DataPostResult("foo", dc);
+
+        ApiError error = new ApiError(ErrorCodes.UNCOMPRESSED_DATA);
+        DataPostResult errorDp = new DataPostResult("foo", error);
+
+        MultiDataPostResult a = new MultiDataPostResult();
+        a.addResult(dp);
+        a.addResult(errorDp);
+
+        MultiDataPostResult b = new MultiDataPostResult();
+        assertFalse(a.equals(b));
+        assertFalse(a.hashCode() == b.hashCode());
+
+        b.setResults(Arrays.asList(dp, errorDp));
+
+        assertEquals(a, b);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+
+
+    private static DataCounts createCounts(long bucketCount,
+            long processedRecordCount, long processedFieldCount,
+            long inputBytes, long inputFieldCount,
+            long invalidDateCount, long missingFieldCount,
+            long outOfOrderTimeStampCount, long failedTransformCount, long latestRecordTime)
+    {
+        DataCounts counts = new DataCounts();
+        counts.setBucketCount(bucketCount);
+        counts.setProcessedRecordCount(processedRecordCount);
+        counts.setProcessedFieldCount(processedFieldCount);
+        counts.setInputBytes(inputBytes);
+        counts.setInputFieldCount(inputFieldCount);
+        counts.setInvalidDateCount(invalidDateCount);
+        counts.setMissingFieldCount(missingFieldCount);
+        counts.setOutOfOrderTimeStampCount(outOfOrderTimeStampCount);
+        counts.setFailedTransformCount(failedTransformCount);
+        counts.setLatestRecordTime(latestRecordTime);
+        return counts;
+    }
+}
