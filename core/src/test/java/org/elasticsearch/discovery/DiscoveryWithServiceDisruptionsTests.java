7742ab71c2f755841c2cdcedc4b186254e0fe957
Boaz Leskes <b.leskes@gmail.com>
2015-06-14 11:49:13 +0200

    enable back shard index operation count check

--- core/src/test/java/org/elasticsearch/discovery/DiscoveryWithServiceDisruptionsTests.java
+++ core/src/test/java/org/elasticsearch/discovery/DiscoveryWithServiceDisruptionsTests.java
@@ -82,21 +82,20 @@ public class DiscoveryWithServiceDisruptionsTests extends ElasticsearchIntegrati
 
     @Override
     protected void beforeIndexDeletion() {
-        // nocommit: chase the problems this assertion signals..
-//        try {
-//            // some tests may leave inflight ops. We should wait.
-//            assertBusy(new Runnable() {
-//                @Override
-//                public void run() {
-//                    DiscoveryWithServiceDisruptionsTests.super.beforeIndexDeletion();
-//                }
-//            });
-//        } catch (Exception e) {
-//            if (e instanceof RuntimeException) {
-//                throw (RuntimeException) e;
-//            }
-//            throw new RuntimeException("unexpeted error", e);
-//        }
+        try {
+            // some tests may leave inflight ops. We should wait.
+            assertBusy(new Runnable() {
+                @Override
+                public void run() {
+                    DiscoveryWithServiceDisruptionsTests.super.beforeIndexDeletion();
+                }
+            });
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                throw (RuntimeException) e;
+            }
+            throw new RuntimeException("unexpeted error", e);
+        }
     }
 
     @Override
