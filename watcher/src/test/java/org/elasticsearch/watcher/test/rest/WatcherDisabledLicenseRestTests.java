61b1bf0142adaf88a701a7c255b4fb7f5d216775
Martijn van Groningen <martijn.v.groningen@gmail.com>
2015-08-07 14:26:22 +0200

    Fixed rest runners that check watcher+shield and an expired license.
    * The watcher+shield test failed, but the error was ignored due to a buggy if statement that existed for the hijack rest test.
    * Blacklisted the hijack test.
    * Also ade sure that we run the watcher+rest test with an user that doesn't have credentials.
    
    Original commit: elastic/x-pack@3c1df3ab323ce710a9f93472b5948916abd7ac78

--- watcher/src/test/java/org/elasticsearch/watcher/test/rest/WatcherDisabledLicenseRestTests.java
+++ watcher/src/test/java/org/elasticsearch/watcher/test/rest/WatcherDisabledLicenseRestTests.java
@@ -43,16 +43,14 @@ public class WatcherDisabledLicenseRestTests extends WatcherRestTests {
 
     @Test
     public void test() throws IOException {
-        disableLicensing();
         try {
+            disableLicensing();
             super.test();
             fail();
-        } catch(AssertionError ae) {
-            if (ae.getMessage() == null || ae.getMessage().contains("not supported")){
-                //This was a test testing the "hijacked" methods
-                return;
-            }
-            assertThat(ae.getMessage(), containsString("license expired for feature [watcher]"));
+        } catch(AssertionError e) {
+            assertThat(e.getMessage(), containsString("license expired for feature [watcher]"));
+        } finally {
+            enableLicensing();
         }
     }
 
@@ -60,5 +58,16 @@ public class WatcherDisabledLicenseRestTests extends WatcherRestTests {
         for (MockLicenseService service : internalCluster().getInstances(MockLicenseService.class)) {
             service.disable();
         }
+    }
+
+    public static void enableLicensing() {
+        for (MockLicenseService service : internalCluster().getInstances(MockLicenseService.class)) {
+            service.enable();
+        }
+    }
+
+    @Override
+    protected boolean enableShield() {
+        return false;
     }
 }
