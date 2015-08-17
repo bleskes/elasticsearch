31b1a6be50ea20985292c4d2289d821b0a06572a
David Roberts <dave@prelert.com>
2015-08-17 10:02:43 +0100

    Correct typo
    Former-commit-id: 80a7a26002d2437df90667c1b7e8fe1cbbf59766
    
    Original commit: elastic/x-pack@08e4a845911559ddd92850b50f8a675b869fff4c

--- java/apps/engineApi/apiClient/src/test/java/com/prelert/rs/client/integrationtests/PreviewIntegrationTest.java
+++ java/apps/engineApi/apiClient/src/test/java/com/prelert/rs/client/integrationtests/PreviewIntegrationTest.java
@@ -31,7 +31,7 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 
