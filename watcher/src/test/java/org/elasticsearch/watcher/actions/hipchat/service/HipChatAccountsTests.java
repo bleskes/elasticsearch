9d9ee1354231a06c312e50e2de7c21e345486bb9
uboness <uboness@gmail.com>
2015-08-18 13:13:42 +0200

    Introducing HipChat Action
    An action capable of sending notifications to rooms and users on hipchat. This actions support three types of HipChat APIs:
    
    - `v1` - The (now deprecated) legacy API where a token can be registered at the group level, and the `v1` version of the API can be used. This API only supports room notification (users cannot be notified). multi-room notification is supported.
    
    - `integration` - The basic integration that one can create in HipChat (it is using the `v2` API version), where notifications can be sent to a single room. User notification is unsupported by this API
    
    - `user` - this API uses an API token of a specific user. An admin user can create an API token and configure it to have access to room notification and user private messaging. This API supports multi-room and multi-user notifications.
    
    The settings for `hipchat` are very similar to the `email` infrastructure in nature. It is possible to configure multiple/different hipchat account, each is associated with the api type (a.k.a profile) - can be `v1`, `integration` or `user`, and the respective `auth_token`. When configuring the action in the watch, one can specify what hipchat account they would like to use (when not specifying an account, the `default_account` will be used). Each account can also specify its own unique `host`/`port` for the hipchat server - for full flexibility.
    
    Closes elastic/x-pack#462
    
    Original commit: elastic/x-pack@fea31a39d0bcab0917e72e6a28d241bcc123ce26

---
+++ watcher/src/test/java/org/elasticsearch/watcher/actions/hipchat/service/HipChatAccountsTests.java
@@ -0,0 +1,141 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.watcher.actions.hipchat.service;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsException;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.watcher.support.http.HttpClient;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ *
+ */
+public class HipChatAccountsTests extends ESTestCase {
+
+    private HttpClient httpClient;
+
+    @Before
+    public void init() throws Exception {
+        httpClient = mock(HttpClient.class);
+    }
+
+    @Test
+    public void testSingleAccount() throws Exception {
+        Settings.Builder builder = Settings.builder()
+                .put("default_account", "account1");
+        addAccountSettings("account1", builder);
+
+        HipChatAccounts accounts = new HipChatAccounts(builder.build(), httpClient, logger);
+        HipChatAccount account = accounts.account("account1");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+        account = accounts.account(null); // falling back on the default
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+    }
+
+    @Test
+    public void testSingleAccount_NoExplicitDefault() throws Exception {
+        Settings.Builder builder = Settings.builder();
+        addAccountSettings("account1", builder);
+
+        HipChatAccounts accounts = new HipChatAccounts(builder.build(), httpClient, logger);
+        HipChatAccount account = accounts.account("account1");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+        account = accounts.account(null); // falling back on the default
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+    }
+
+    @Test
+    public void testMultipleAccounts() throws Exception {
+        Settings.Builder builder = Settings.builder()
+                .put("default_account", "account1");
+        addAccountSettings("account1", builder);
+        addAccountSettings("account2", builder);
+
+        HipChatAccounts accounts = new HipChatAccounts(builder.build(), httpClient, logger);
+        HipChatAccount account = accounts.account("account1");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+        account = accounts.account("account2");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account2"));
+        account = accounts.account(null); // falling back on the default
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+    }
+
+    @Test
+    public void testMultipleAccounts_NoExplicitDefault() throws Exception {
+        Settings.Builder builder = Settings.builder()
+                .put("default_account", "account1");
+        addAccountSettings("account1", builder);
+        addAccountSettings("account2", builder);
+
+        HipChatAccounts accounts = new HipChatAccounts(builder.build(), httpClient, logger);
+        HipChatAccount account = accounts.account("account1");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account1"));
+        account = accounts.account("account2");
+        assertThat(account, notNullValue());
+        assertThat(account.name, equalTo("account2"));
+        account = accounts.account(null);
+        assertThat(account, notNullValue());
+        assertThat(account.name, isOneOf("account1", "account2"));
+    }
+
+    @Test(expected = SettingsException.class)
+    public void testMultipleAccounts_UnknownDefault() throws Exception {
+        Settings.Builder builder = Settings.builder()
+                .put("default_account", "unknown");
+        addAccountSettings("account1", builder);
+        addAccountSettings("account2", builder);
+        new HipChatAccounts(builder.build(), httpClient, logger);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNoAccount() throws Exception {
+        Settings.Builder builder = Settings.builder();
+        HipChatAccounts accounts = new HipChatAccounts(builder.build(), httpClient, logger);
+        accounts.account(null);
+        fail("no accounts are configured so trying to get the default account should throw an IllegalStateException");
+    }
+
+    @Test(expected = SettingsException.class)
+    public void testNoAccount_WithDefaultAccount() throws Exception {
+        Settings.Builder builder = Settings.builder()
+                .put("default_account", "unknown");
+        new HipChatAccounts(builder.build(), httpClient, logger);
+    }
+
+    private void addAccountSettings(String name, Settings.Builder builder) {
+        HipChatAccount.Profile profile = randomFrom(HipChatAccount.Profile.values());
+        builder.put("account." + name + ".profile", profile.value());
+        builder.put("account." + name + ".auth_token", randomAsciiOfLength(50));
+        if (profile == HipChatAccount.Profile.INTEGRATION) {
+            builder.put("account." + name + ".room", randomAsciiOfLength(10));
+        }
+    }
+}
