d16f9dc1dfb36f711fc37a4b4c059871b796ebdd
Areek Zillur <areek.zillur@elasticsearch.com>
2015-08-21 14:04:30 -0400

    stub out acknowledge callbacks for commercial plugins
    Original commit: elastic/x-pack@1e0f57c8ca2d0f516c2ae492b8f8dec932853519

--- shield/src/main/java/org/elasticsearch/shield/license/LicenseService.java
+++ shield/src/main/java/org/elasticsearch/shield/license/LicenseService.java
@@ -30,6 +30,8 @@ import org.elasticsearch.license.plugin.core.LicensesClientService;
 import org.elasticsearch.shield.ShieldPlugin;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -47,6 +49,7 @@ public class LicenseService extends AbstractLifecycleComponent<LicenseService> {
     private final LicensesClientService licensesClientService;
     private final LicenseEventsNotifier notifier;
     private final Collection<LicensesClientService.ExpirationCallback> expirationLoggers;
+    private final LicensesClientService.AcknowledgementCallback acknowledgementCallback;
 
     private boolean enabled = false;
 
@@ -90,6 +93,15 @@ public class LicenseService extends AbstractLifecycleComponent<LicenseService> {
                     }
                 }
         );
+        this.acknowledgementCallback = new LicensesClientService.AcknowledgementCallback() {
+            @Override
+            public List<String> acknowledge(License currentLicense, License newLicense) {
+                // TODO: add messages to be acknowledged when installing newLicense from currentLicense
+                // NOTE: currentLicense can be null, as a license registration can happen before
+                // a trial license could be generated
+                return Collections.emptyList();
+            }
+        };
     }
 
     public synchronized boolean enabled() {
@@ -99,7 +111,7 @@ public class LicenseService extends AbstractLifecycleComponent<LicenseService> {
     @Override
     protected void doStart() throws ElasticsearchException {
         if (settings.getGroups("tribe", true).isEmpty()) {
-            licensesClientService.register(FEATURE_NAME, TRIAL_LICENSE_OPTIONS, expirationLoggers, null, new InternalListener());
+            licensesClientService.register(FEATURE_NAME, TRIAL_LICENSE_OPTIONS, expirationLoggers, acknowledgementCallback, new InternalListener());
         } else {
             //TODO currently we disable licensing on tribe node. remove this once es core supports merging cluster
             new InternalListener().onEnabled(null);
