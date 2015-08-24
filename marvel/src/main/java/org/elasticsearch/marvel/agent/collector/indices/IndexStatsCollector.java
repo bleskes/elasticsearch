7ed95605ffa67001f47f371fb8664a1e5e76f1d1
Tanguy Leroux <tlrx.dev@gmail.com>
2015-08-24 19:57:20 +0200

    Marvel: Stop collecting data X days after license expiration
    Closes elastic/x-pack#370, elastic/x-pack#470
    
    Original commit: elastic/x-pack@7b523fa491f67a90d028358ae34f2773e1f2cf39

--- marvel/src/main/java/org/elasticsearch/marvel/agent/collector/indices/IndexStatsCollector.java
+++ marvel/src/main/java/org/elasticsearch/marvel/agent/collector/indices/IndexStatsCollector.java
@@ -28,6 +28,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.marvel.agent.collector.AbstractCollector;
 import org.elasticsearch.marvel.agent.exporter.MarvelDoc;
 import org.elasticsearch.marvel.agent.settings.MarvelSettings;
+import org.elasticsearch.marvel.license.LicenseService;
 
 import java.util.Collection;
 
@@ -45,14 +46,15 @@ public class IndexStatsCollector extends AbstractCollector<IndexStatsCollector> 
     private final Client client;
 
     @Inject
-    public IndexStatsCollector(Settings settings, ClusterService clusterService, MarvelSettings marvelSettings, Client client) {
-        super(settings, NAME, clusterService, marvelSettings);
+    public IndexStatsCollector(Settings settings, ClusterService clusterService, MarvelSettings marvelSettings,  LicenseService licenseService,
+                               Client client) {
+        super(settings, NAME, clusterService, marvelSettings, licenseService);
         this.client = client;
     }
 
     @Override
-    protected boolean masterOnly() {
-        return true;
+    protected boolean canCollect() {
+        return super.canCollect() && isLocalNodeMaster();
     }
 
     @Override
