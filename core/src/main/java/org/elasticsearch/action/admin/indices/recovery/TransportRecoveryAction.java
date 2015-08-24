77485af09317ca924b8dc0c578f4cfeb22783f54
Jason Tedor <jason@tedor.me>
2015-08-23 21:21:38 -0400

    Move and rename RecoveryNodeResponse

--- core/src/main/java/org/elasticsearch/action/admin/indices/recovery/TransportRecoveryAction.java
+++ core/src/main/java/org/elasticsearch/action/admin/indices/recovery/TransportRecoveryAction.java
@@ -25,6 +25,7 @@ import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.DefaultShardOperationFailedException;
 import org.elasticsearch.action.support.broadcast.BroadcastShardOperationFailedException;
 import org.elasticsearch.action.support.indices.BaseNodeBroadcastRequest;
+import org.elasticsearch.action.support.indices.BaseNodeBroadcastResponse;
 import org.elasticsearch.action.support.indices.TransportNodeBroadcastAction;
 import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.cluster.ClusterState;
@@ -33,8 +34,13 @@ import org.elasticsearch.cluster.block.ClusterBlockLevel;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.routing.GroupShardsIterator;
 import org.elasticsearch.cluster.routing.ShardRouting;
+import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.indices.IndicesService;
@@ -42,6 +48,7 @@ import org.elasticsearch.indices.recovery.RecoveryState;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -49,7 +56,7 @@ import java.util.Map;
  * Transport action for shard recovery operation. This transport action does not actually
  * perform shard recovery, it only reports on recoveries (both active and complete).
  */
-public class TransportRecoveryAction extends TransportNodeBroadcastAction<RecoveryRequest, RecoveryResponse, TransportRecoveryAction.ShardRecoveryRequest, RecoveryNodeResponse, RecoveryState> {
+public class TransportRecoveryAction extends TransportNodeBroadcastAction<RecoveryRequest, RecoveryResponse, TransportRecoveryAction.ShardRecoveryRequest, TransportRecoveryAction.NodeRecoveryResponse, RecoveryState> {
 
     private final IndicesService indicesService;
 
@@ -63,9 +70,9 @@ public class TransportRecoveryAction extends TransportNodeBroadcastAction<Recove
     }
 
     @Override
-    protected RecoveryResponse newResponse(RecoveryRequest request, int totalShards, int successfulShards, int failedShards, List<RecoveryNodeResponse> responses, List<DefaultShardOperationFailedException> shardFailures) {
+    protected RecoveryResponse newResponse(RecoveryRequest request, int totalShards, int successfulShards, int failedShards, List<NodeRecoveryResponse> responses, List<DefaultShardOperationFailedException> shardFailures) {
         Map<String, List<RecoveryState>> shardResponses = Maps.newHashMap();
-        for (RecoveryNodeResponse response : responses) {
+        for (NodeRecoveryResponse response : responses) {
             for (RecoveryState recoveryState : response.recoveryStates) {
                 if (recoveryState == null) {
                     continue;
@@ -92,13 +99,13 @@ public class TransportRecoveryAction extends TransportNodeBroadcastAction<Recove
     }
 
     @Override
-    protected RecoveryNodeResponse newNodeResponse() {
-        return new RecoveryNodeResponse();
+    protected NodeRecoveryResponse newNodeResponse() {
+        return new NodeRecoveryResponse();
     }
 
     @Override
-    protected RecoveryNodeResponse newNodeResponse(String nodeId, int totalShards, int successfulShards, List<RecoveryState> results, List<BroadcastShardOperationFailedException> exceptions) {
-        RecoveryNodeResponse response = new RecoveryNodeResponse(nodeId, totalShards, successfulShards, exceptions);
+    protected NodeRecoveryResponse newNodeResponse(String nodeId, int totalShards, int successfulShards, List<RecoveryState> results, List<BroadcastShardOperationFailedException> exceptions) {
+        NodeRecoveryResponse response = new NodeRecoveryResponse(nodeId, totalShards, successfulShards, exceptions);
         response.setRecoveryStates(results);
         return response;
     }
@@ -137,6 +144,88 @@ public class TransportRecoveryAction extends TransportNodeBroadcastAction<Recove
         @Override
         protected RecoveryRequest newRequest() {
             return new RecoveryRequest();
+        }
+    }
+
+    /**
+     * Information regarding the recovery state of a shard.
+     */
+    public static class NodeRecoveryResponse extends BaseNodeBroadcastResponse implements ToXContent {
+
+        List<RecoveryState> recoveryStates;
+
+        public NodeRecoveryResponse() { }
+
+        /**
+         * Constructs shard recovery in formation for the given index and shard id.
+         * @param nodeId Id of the node
+         * @param totalShards The total number of shards for which the operation was performed
+         * @param successfulShards The number of shards for which the operation was successful
+         * @param exceptions The exceptions from the failed shards
+         */
+        public NodeRecoveryResponse(String nodeId, int totalShards, int successfulShards, List<BroadcastShardOperationFailedException> exceptions) {
+            super(nodeId, totalShards, successfulShards, exceptions);
+        }
+
+        /**
+         * Sets the recovery state information for the shard.
+         *
+         * @param recoveryStates Recovery states
+         */
+        public void setRecoveryStates(List<RecoveryState> recoveryStates) {
+            this.recoveryStates = recoveryStates;
+        }
+
+        /**
+         * Gets the recovery state information for the shard. Null if shard wasn't recovered / recovery didn't start yet.
+         *
+         * @return  Recovery state
+         */
+        @Nullable
+        public List<RecoveryState> recoveryStates() {
+            return recoveryStates;
+        }
+
+        @Override
+        public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+            builder.startArray("recovery_states");
+            for (RecoveryState recoveryState : recoveryStates) {
+                recoveryState.toXContent(builder, params);
+            }
+            builder.endArray();
+            return builder;
+        }
+
+        @Override
+        public void writeTo(StreamOutput out) throws IOException {
+            super.writeTo(out);
+            out.writeVInt(recoveryStates.size());
+            for (int i = 0; i < recoveryStates.size(); i++) {
+                recoveryStates.get(i).writeTo(out);
+            }
+        }
+
+        @Override
+        public void readFrom(StreamInput in) throws IOException {
+            super.readFrom(in);
+            int size = in.readVInt();
+            recoveryStates = Lists.newArrayListWithCapacity(size);
+            for (int i = 0; i < size; i++) {
+                recoveryStates.add(RecoveryState.readRecoveryState(in));
+            }
+        }
+
+        /**
+         * Builds a new NodeRecoveryResponse from the give input stream.
+         *
+         * @param in    Input stream
+         * @return      A new NodeRecoveryResponse
+         * @throws IOException
+         */
+        public static NodeRecoveryResponse readShardRecoveryResponse(StreamInput in) throws IOException {
+            NodeRecoveryResponse response = new NodeRecoveryResponse();
+            response.readFrom(in);
+            return response;
         }
     }
 }
