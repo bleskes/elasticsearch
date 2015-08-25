9ecc69c17cd8a1ae6962c86b47d7ccfbf907d546
uboness <uboness@gmail.com>
2015-08-25 13:02:39 +0200

    Introducing Slack Action
    - This action enables sending notifications to slack channels/users
    - Utilizes the incoming webhook API of slack
    - Similar to the `email` and `hipchat` actions, multiple slack accounts can be configured, each with its own URL and message defaults
    - Slack actions are associated with an account, or if not, they'll be sent via the default account.
    - The message itself is very flexible and enables defining simple messages to one or more users and/or one or more channels. For complex message structures, one can use `attachments` as described by the slack API.
    
     Closes elastic/x-pack#491
    
    Original commit: elastic/x-pack@129c825023cbfe1f91cacd5afa211ace90064a55

---
+++ watcher/src/main/java/org/elasticsearch/watcher/actions/slack/service/InternalSlackService.java
@@ -0,0 +1,89 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.watcher.actions.slack.service;
+
+import org.elasticsearch.common.component.AbstractLifecycleComponent;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.settings.NodeSettingsService;
+import org.elasticsearch.watcher.shield.WatcherSettingsFilter;
+import org.elasticsearch.watcher.support.http.HttpClient;
+
+/**
+ *
+ */
+public class InternalSlackService extends AbstractLifecycleComponent<InternalSlackService> implements SlackService {
+
+    private final HttpClient httpClient;
+    private volatile SlackAccounts accounts;
+
+    @Inject
+    public InternalSlackService(Settings settings, HttpClient httpClient, NodeSettingsService nodeSettingsService, WatcherSettingsFilter settingsFilter) {
+        super(settings);
+        this.httpClient = httpClient;
+        nodeSettingsService.addListener(new NodeSettingsService.Listener() {
+            @Override
+            public void onRefreshSettings(Settings settings) {
+                reset(settings);
+            }
+        });
+        settingsFilter.filterOut("watcher.actions.slack.service.account.*.url");
+    }
+
+    @Override
+    protected void doStart() {
+        reset(settings);
+    }
+
+    @Override
+    protected void doStop() {
+    }
+
+    @Override
+    protected void doClose() {
+    }
+
+    @Override
+    public SlackAccount getDefaultAccount() {
+        return accounts.account(null);
+    }
+
+    @Override
+    public SlackAccount getAccount(String name) {
+        return accounts.account(name);
+    }
+
+    void reset(Settings nodeSettings) {
+        Settings.Builder builder = Settings.builder();
+        String prefix = "watcher.actions.slack.service";
+        for (String setting : settings.getAsMap().keySet()) {
+            if (setting.startsWith(prefix)) {
+                builder.put(setting.substring(prefix.length()+1), settings.get(setting));
+            }
+        }
+        if (nodeSettings != settings) { // if it's the same settings, no point in re-applying it
+            for (String setting : nodeSettings.getAsMap().keySet()) {
+                if (setting.startsWith(prefix)) {
+                    builder.put(setting.substring(prefix.length() + 1), nodeSettings.get(setting));
+                }
+            }
+        }
+        accounts = new SlackAccounts(builder.build(), httpClient, logger);
+    }
+
+}
