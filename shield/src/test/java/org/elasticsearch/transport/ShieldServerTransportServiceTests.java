26473d0ddc66521655edf8ee1aa0328f95afbebb
Jason Tedor <jason@tedor.me>
2015-08-31 08:25:05 -0400

    Add test to ensure that ShieldServerTransportService wraps all handlers
    This commit adds a test to ensure that all request handlers are wrapped
    by ProfileSecuredRequestHandler.
    
    Original commit: elastic/x-pack@d89d8cde2bb9125a853609c8760a0836e1c27e8d

---
+++ shield/src/test/java/org/elasticsearch/transport/ShieldServerTransportServiceTests.java
@@ -0,0 +1,34 @@
+package org.elasticsearch.transport;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.shield.ShieldPlugin;
+import org.elasticsearch.shield.transport.ShieldServerTransportService;
+import org.elasticsearch.test.ShieldIntegTestCase;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.instanceOf;
+
+// this class sits in org.elasticsearch.transport so that TransportService.requestHandlers is visible
+public class ShieldServerTransportServiceTests extends ShieldIntegTestCase {
+    @Override
+    protected Settings transportClientSettings() {
+        return Settings.settingsBuilder()
+                .put(super.transportClientSettings())
+                .put(ShieldPlugin.ENABLED_SETTING_NAME, true)
+                .build();
+    }
+
+    public void testShieldServerTransportServiceWrapsAllHandlers() {
+        for (TransportService transportService : internalTestCluster().getInstances(TransportService.class)) {
+            assertThat(transportService, instanceOf(ShieldServerTransportService.class));
+            for (Map.Entry<String, RequestHandlerRegistry> entry : transportService.requestHandlers.entrySet()) {
+                assertThat(
+                        "handler not wrapped by " + ShieldServerTransportService.ProfileSecuredRequestHandler.class + "; do all the handler registration methods have overrides?",
+                        entry.getValue().getHandler(),
+                        instanceOf(ShieldServerTransportService.ProfileSecuredRequestHandler.class)
+                );
+            }
+        }
+    }
+}
