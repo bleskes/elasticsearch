7e9983df0ecde351ebd90214b4cf735d7a524490
jaymode <jay.modi@elasticsearch.com>
2015-09-02 11:32:57 -0400

    add shield integration to the ScriptServiceProxy
    This change adds support for to put the watcher user in the context that is passed to the
    ScriptService when Shield is installed and watcher integration with Shield is enabled.
    
    Original commit: elastic/x-pack@f5f942fd6ccb1441fb05bfbcaa17ab04cbb4e104

--- watcher/src/main/java/org/elasticsearch/watcher/support/init/proxy/ScriptServiceProxy.java
+++ watcher/src/main/java/org/elasticsearch/watcher/support/init/proxy/ScriptServiceProxy.java
@@ -17,8 +17,10 @@
 
 package org.elasticsearch.watcher.support.init.proxy;
 
+import org.elasticsearch.common.ContextAndHeaderHolder;
 import org.elasticsearch.common.inject.Injector;
 import org.elasticsearch.script.*;
+import org.elasticsearch.watcher.shield.ShieldIntegration;
 import org.elasticsearch.watcher.support.Script;
 import org.elasticsearch.watcher.support.init.InitializingService;
 
@@ -31,6 +33,7 @@ import java.util.Map;
 public class ScriptServiceProxy implements InitializingService.Initializable {
 
     private ScriptService service;
+    private ContextAndHeaderHolder contextAndHeaderHolder = new ContextAndHeaderHolder();
 
     /**
      * Creates a proxy to the given script service (can be used for testing)
@@ -44,6 +47,10 @@ public class ScriptServiceProxy implements InitializingService.Initializable {
     @Override
     public void init(Injector injector) {
         this.service = injector.getInstance(ScriptService.class);
+        ShieldIntegration shieldIntegration = injector.getInstance(ShieldIntegration.class);
+        if (shieldIntegration != null) {
+            shieldIntegration.putUserInContext(contextAndHeaderHolder);
+        }
     }
 
     public CompiledScript compile(Script script) {
@@ -51,7 +58,7 @@ public class ScriptServiceProxy implements InitializingService.Initializable {
     }
 
     public CompiledScript compile(org.elasticsearch.script.Script script) {
-        return service.compile(script, WatcherScriptContext.CTX);
+        return service.compile(script, WatcherScriptContext.CTX, contextAndHeaderHolder);
     }
 
     public ExecutableScript executable(CompiledScript compiledScript, Map<String, Object> vars) {
@@ -60,7 +67,7 @@ public class ScriptServiceProxy implements InitializingService.Initializable {
 
 
     public ExecutableScript executable(org.elasticsearch.script.Script script) {
-        return service.executable(script, WatcherScriptContext.CTX);
+        return service.executable(script, WatcherScriptContext.CTX, contextAndHeaderHolder);
     }
 
     public static final ScriptContext.Plugin INSTANCE = new ScriptContext.Plugin("elasticsearch-watcher", "watch");
