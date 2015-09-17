1ac9e22923c88c2fc924b3718aa1cd24678e6df0
jaymode <jay.modi@elasticsearch.com>
2015-09-17 08:02:45 -0400

    fix compilation errors caused by changes to wipe method
    This commit keeps the behavior the same and does not exclude the index audit trail template from
    being wiped, that will be done in a future commit.
    
    Original commit: elastic/x-pack@50f77366712976908afe852dfae771c8d1151afd

--- shield/src/test/java/org/elasticsearch/shield/audit/index/IndexAuditTrailTests.java
+++ shield/src/test/java/org/elasticsearch/shield/audit/index/IndexAuditTrailTests.java
@@ -61,6 +61,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -223,9 +224,9 @@ public class IndexAuditTrailTests extends ShieldIntegTestCase {
             auditor.close();
         }
 
-        cluster().wipe();
+        cluster().wipe(Collections.<String>emptySet());
         if (remoteIndexing && cluster2 != null) {
-            cluster2.wipe();
+            cluster2.wipe(Collections.emptySet());
             remoteClient.close();
             cluster2.close();
         }
--- shield/src/test/java/org/elasticsearch/shield/audit/index/RemoteIndexAuditTrailStartingTests.java
+++ shield/src/test/java/org/elasticsearch/shield/audit/index/RemoteIndexAuditTrailStartingTests.java
@@ -32,6 +32,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -117,7 +118,7 @@ public class RemoteIndexAuditTrailStartingTests extends ShieldIntegTestCase {
     public void stopRemoteCluster() throws Exception {
         if (remoteCluster != null) {
             try {
-                remoteCluster.wipe();
+                remoteCluster.wipe(Collections.<String>emptySet());
             } finally {
                 remoteCluster.afterTest();
             }
