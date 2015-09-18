6c6984986305b5f9880463c3b71dc08cc7f3e012
Jason Tedor <jason@tedor.me>
2015-09-17 23:36:38 -0400

    Replace Integers.checkedCast with built-in method
    Original commit: elastic/x-pack@a222e68bc86b6c5a9c2bd23e28c9059a319d3456

--- shield/src/test/java/org/elasticsearch/shield/authc/ldap/UserAttributeGroupsResolverTests.java
+++ shield/src/test/java/org/elasticsearch/shield/authc/ldap/UserAttributeGroupsResolverTests.java
@@ -22,7 +22,6 @@ import com.unboundid.ldap.sdk.LDAPConnectionOptions;
 import com.unboundid.ldap.sdk.LDAPURL;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.util.primitives.Integers;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.shield.authc.activedirectory.ActiveDirectorySessionFactoryTests;
 import org.elasticsearch.shield.authc.ldap.support.SessionFactory;
@@ -61,7 +60,7 @@ public class UserAttributeGroupsResolverTests extends ESTestCase {
         options.setFollowReferrals(true);
         options.setAutoReconnect(true);
         options.setAllowConcurrentSocketFactoryUse(true);
-        options.setConnectTimeoutMillis(Integers.checkedCast(SessionFactory.TIMEOUT_DEFAULT.millis()));
+        options.setConnectTimeoutMillis(Math.toIntExact(SessionFactory.TIMEOUT_DEFAULT.millis()));
         options.setResponseTimeoutMillis(SessionFactory.TIMEOUT_DEFAULT.millis());
         ldapConnection = new LDAPConnection(clientSSLService.sslSocketFactory(), options, ldapurl.getHost(), ldapurl.getPort(), BRUCE_BANNER_DN, ActiveDirectorySessionFactoryTests.PASSWORD);
     }
