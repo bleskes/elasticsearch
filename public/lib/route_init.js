0f50d69680b3aa7a50c0a95ded889788ee10031c
Chris Cowan <chris@chriscowan.us>
2015-09-23 08:56:56 -0700

    Clearify comment
    Original commit: elastic/x-pack@270d23423988afa8680ffc768b8c051cf0e1fa9b

--- public/lib/route_init.js
+++ public/lib/route_init.js
@@ -59,7 +59,7 @@ define(function (require) {
             return indexPattern;
           });
         })
-        // Finally return the Marvel object.
+        // Finally filter the cluster from the nav if it's light then return the Marvel object.
         .then(function () {
           var cluster = _.find(marvel.clusters, { cluster_uuid: globalState.cluster });
           var license = _.find(cluster.licenses, { feature: 'marvel' });
