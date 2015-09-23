6211eca00e20d96a12420cb12bba8038292d4107
Timothy Sullivan <tsullivan@elastic.co>
2015-09-23 09:56:00 -0700

    fix - welcome message toggle manages allowReport on $rootScope
    Original commit: elastic/x-pack@e5cc19437169082d66a5acfbd18a594d890cb5e4

--- public/directives/welcome_msg/index.js
+++ public/directives/welcome_msg/index.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const mod = require('ui/modules').get('marvel/directives', []);
 const template = require('plugins/marvel/directives/welcome_msg/index.html');
-mod.directive('marvelWelcomeMessage', function ($window, reportStats, features) {
+mod.directive('marvelWelcomeMessage', function ($window, $rootScope, reportStats, features) {
   return {
     restrict: 'E',
     scope: {
@@ -34,7 +34,7 @@ mod.directive('marvelWelcomeMessage', function ($window, reportStats, features) 
         scope.allowReport = features.isEnabled('report', true);
         scope.toggleAllowReport = function (data) {
           features.update('report', data.allowReport);
-          scope.allowReport = data.allowReport;
+          scope.$rootScope = scope.allowReport = data.allowReport;
         };
       }
 
