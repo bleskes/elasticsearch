7300cd4b7cad3c2e69acd8aa84e757f47269ffee
Chris Cowan <chris@chriscowan.us>
2015-10-13 15:20:23 -0700

    Executor Service
    - Adds a new executor service for non-courier repeating tasks
    - Refactor the cluster overview page to use the new executor service
    
    Original commit: elastic/x-pack@2aabd77c9c097640706198247c412ad52f1b6a0f

---
+++ public/services/executor.js
@@ -0,0 +1,84 @@
+const angular = require('angular');
+const _ = require('lodash');
+
+const mod = require('ui/modules').get('marvel/executor', []);
+mod.service('$executor', ($timeout, $rootScope, timefilter) => {
+
+  const queue = [];
+  let executionTimer;
+
+  /**
+   * Resets the timer to start again
+   * @returns {void}
+   */
+  function reset() {
+    cancel();
+    start();
+  }
+
+  /**
+   * Cancels the execution timer
+   * @returns {void}
+   */
+  function cancel() {
+    $timeout.cancel(executionTimer);
+  }
+
+  /**
+   * Registers a service with the executor
+   * @param {object} service The service to register
+   * @returns {void}
+   */
+  function register(service) {
+    queue.push(service);
+  }
+
+  /**
+   * Stops the executor and empties the service queue
+   * @returns {void}
+   */
+  function destroy() {
+    cancel();
+    queue.splice(0, queue.length);
+  }
+
+  /**
+   * Runs the queue (all at once)
+   * @returns {Promise} a promise of all the services
+   */
+  function run() {
+    return Promise.all(queue.map((service) => {
+      return service.execute()
+        .then(service.handleResponse || _.noop)
+        .catch(service.handleError || _.noop);
+    }))
+    .then(reset);
+  }
+
+  /**
+   * Starts the executor service if the timefilter is not paused
+   * @returns {void}
+   */
+  function start() {
+    if (!timefilter.refreshInterval.pause) {
+      executionTimer = $timeout(run, timefilter.refreshInterval.value);
+    }
+  }
+
+  /**
+   * Reset the executor when the timefilter updates
+   */
+  timefilter.on('update', reset);
+
+
+  /**
+   * Expose the methods
+   */
+  return {
+    register,
+    start,
+    destroy,
+    reset,
+    cancel
+  };
+});
--- public/views/home/home_controller.js
+++ public/views/home/home_controller.js
@@ -39,47 +39,36 @@ define(function (require) {
   })
   .otherwise({ redirectTo: '/no-data' });
 
-  module.controller('home', function ($route, $window, $scope, marvelClusters, timefilter, $timeout) {
+  module.controller('home', function ($route, $window, $scope, marvelClusters, timefilter, $timeout, $executor) {
 
+    // Set the key for as the cluster_uuid. This is mainly for
+    // react.js so we can use the key easily.
     function setKeyForClusters(cluster) {
       cluster.key = cluster.cluster_uuid;
       return cluster;
     }
 
+    // Set the inital value for the clusters (and map it with setKeyForClusters)
     $scope.clusters = $route.current.locals.clusters.map(setKeyForClusters);
 
+    // Enable the timefilter
     timefilter.enabled = true;
-    if (timefilter.refreshInterval.value === 0) {
-      timefilter.refreshInterval.value = 10000;
-      timefilter.refreshInterval.display = '10 Seconds';
-    }
 
-    var fetchTimer;
-    function startFetchInterval() {
-      if (!timefilter.refreshInterval.pause) {
-        fetchTimer = $timeout(fetch, timefilter.refreshInterval.value);
-      }
-    }
-    function cancelFetchInterval() {
-      $timeout.cancel(fetchTimer);
-    }
-
-    timefilter.on('update', (time) => {
-      cancelFetchInterval();
-      startFetchInterval();
-    });
-
-    function fetch() {
-      marvelClusters.fetch().then((clusters) => {
+    // Register the marvelClusters service.
+    $executor.register({
+      execute: function () {
+        return marvelClusters.fetch();
+      },
+      handleResponse: function (clusters) {
         $scope.clusters = clusters.map(setKeyForClusters);
-        startFetchInterval();
-      });
-    }
-
-    startFetchInterval();
-    $scope.$on('$destroy', () => {
-      cancelFetchInterval();
+      }
     });
+
+    // Start the executor
+    $executor.start();
+
+    // Destory the executor
+    $scope.$on('$destroy', $executor.destroy);
 
   });
 
