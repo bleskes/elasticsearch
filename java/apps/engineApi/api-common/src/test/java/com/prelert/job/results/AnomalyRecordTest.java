ac6ad240789b08a19ab9ef9e2ffceccbbd70d501
Dimitrios Athanasiou <dimitris@prelert.com>
2015-10-28 10:29:13 +0000

    Add some unit tests for AnomalyRecord
    Former-commit-id: 0153d41839ea8f12754ffd5a52e6430c116c16f1
    
    Original commit: elastic/x-pack@54d11ba62450f6c606b949fac249a9eb270c5bcd

--- java/apps/engineApi/api-common/src/test/java/com/prelert/job/results/AnomalyRecordTest.java
+++ java/apps/engineApi/api-common/src/test/java/com/prelert/job/results/AnomalyRecordTest.java
@@ -32,6 +32,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -63,12 +65,24 @@ public class AnomalyRecordTest
         AnomalyRecord anomalyRecord = new AnomalyRecord();
         anomalyRecord.setFieldName("responsetime");
         anomalyRecord.setPartitionFieldName("airline");
+        anomalyRecord.setPartitionFieldValue("AAL");
 
-        anomalyRecord.setId("1403701200individual metric/0/0/responsetime/airline/1");
+        anomalyRecord.setId("1403701200individual metric/0/0/responsetime/airline/AAL1");
 
         assertEquals("1403701200", anomalyRecord.getParent());
-        assertEquals("1403701200individual metric/0/0/responsetime/airline/1",
+        assertEquals("1403701200individual metric/0/0/responsetime/airline/AAL1",
                 anomalyRecord.getId());
+    }
+
+    @Test
+    public void testGenerateNewId()
+    {
+        AnomalyRecord anomalyRecord = new AnomalyRecord();
+        anomalyRecord.setId("1403701200individual metric/42");
+
+        anomalyRecord.generateNewId("1403704800", "population count/", 48);
+
+        assertEquals("1403704800population count/48", anomalyRecord.getId());
     }
 
     @Test
@@ -94,4 +108,66 @@ public class AnomalyRecordTest
         assertTrue(record.hadBigNormalisedUpdate());
     }
 
+    @Test
+    public void testEquals_GivenSameReference()
+    {
+        AnomalyRecord record = new AnomalyRecord();
+        assertTrue(record.equals(record));
+    }
+
+    @Test
+    public void testEquals_GivenDifferentClass()
+    {
+        AnomalyRecord record = new AnomalyRecord();
+        assertFalse(record.equals("a string"));
+    }
+
+    @Test
+    public void testEquals_GivenEqualRecords()
+    {
+        Influence influence = new Influence();
+
+        AnomalyRecord record1 = new AnomalyRecord();
+        record1.setActual(42.0);
+        record1.setAnomalyScore(99.0);
+        record1.setByFieldName("airline");
+        record1.setByFieldValue("AAL");
+        record1.setFieldName("responsetime");
+        record1.setFunction("metric");
+        record1.setFunctionDescription("Function blah blah");
+        record1.setId("1403701200individual metric/42");
+        record1.setInfluencers(Arrays.asList(influence));
+        record1.setInterim(false);
+        record1.setNormalizedProbability(86.4);
+        record1.setOverFieldName("airport");
+        record1.setOverFieldValue("SKG");
+        record1.setPartitionFieldName("planet");
+        record1.setPartitionFieldValue("earth");
+        record1.setProbability(0.00042);
+        record1.setTimestamp(new Date(0));
+        record1.setTypical(0.5);
+
+        AnomalyRecord record2 = new AnomalyRecord();
+        record2.setActual(42.0);
+        record2.setAnomalyScore(99.0);
+        record2.setByFieldName("airline");
+        record2.setByFieldValue("AAL");
+        record2.setFieldName("responsetime");
+        record2.setFunction("metric");
+        record2.setFunctionDescription("Function blah blah");
+        record2.setId("1403701200individual metric/42");
+        record2.setInfluencers(Arrays.asList(influence));
+        record2.setInterim(false);
+        record2.setNormalizedProbability(86.4);
+        record2.setOverFieldName("airport");
+        record2.setOverFieldValue("SKG");
+        record2.setPartitionFieldName("planet");
+        record2.setPartitionFieldValue("earth");
+        record2.setProbability(0.00042);
+        record2.setTimestamp(new Date(0));
+        record2.setTypical(0.5);
+
+        assertTrue(record1.equals(record2));
+        assertTrue(record2.equals(record1));
+    }
 }
