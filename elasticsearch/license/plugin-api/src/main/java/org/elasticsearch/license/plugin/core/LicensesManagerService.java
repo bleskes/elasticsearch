3ee8761312524e817bd1d10f865fd17405cd0ef3
jaymode <jay.modi@elasticsearch.com>
2015-11-25 10:18:08 -0500

    reorganize directory layout
    See elastic/x-pack#1022
    
    Original commit: elastic/x-pack@2f27f94fae20bc84590da8a10adf8d892ebfe536

---
+++ elasticsearch/license/plugin-api/src/main/java/org/elasticsearch/license/plugin/core/LicensesManagerService.java
@@ -0,0 +1,34 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+package org.elasticsearch.license.plugin.core;
+
+import org.elasticsearch.license.core.License;
+
+import java.util.List;
+
+public interface LicensesManagerService {
+
+    /**
+     * @return the id of registered licensees currently in <code>state</code>
+     */
+    List<String> licenseesWithState(LicenseState state);
+
+    /**
+     * @return the currently active license, or {@code null} if no license is currently installed
+     */
+    License getLicense();
+}
---
+++ elasticsearch/license/plugin/src/main/java/org/elasticsearch/license/plugin/rest/RestPutLicenseAction.java
@@ -0,0 +1,57 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+package org.elasticsearch.license.plugin.rest;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.license.plugin.action.put.PutLicenseAction;
+import org.elasticsearch.license.plugin.action.put.PutLicenseRequest;
+import org.elasticsearch.license.plugin.action.put.PutLicenseResponse;
+import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.action.support.RestBuilderListener;
+
+import static org.elasticsearch.rest.RestRequest.Method.POST;
+import static org.elasticsearch.rest.RestRequest.Method.PUT;
+
+public class RestPutLicenseAction extends BaseRestHandler {
+
+    @Inject
+    public RestPutLicenseAction(Settings settings, RestController controller, Client client) {
+        super(settings, controller, client);
+        controller.registerHandler(PUT, "/_license", this);
+        controller.registerHandler(POST, "/_license", this);
+    }
+
+    @Override
+    public void handleRequest(final RestRequest request, final RestChannel channel, final Client client) {
+        PutLicenseRequest putLicenseRequest = new PutLicenseRequest();
+        putLicenseRequest.license(request.content().toUtf8());
+        putLicenseRequest.acknowledge(request.paramAsBoolean("acknowledge", false));
+        client.admin().cluster().execute(PutLicenseAction.INSTANCE, putLicenseRequest, new RestBuilderListener<PutLicenseResponse>(channel) {
+            @Override
+            public RestResponse buildResponse(PutLicenseResponse response, XContentBuilder builder) throws Exception {
+                builder.startObject();
+                response.toXContent(builder, ToXContent.EMPTY_PARAMS);
+                builder.endObject();
+                return new BytesRestResponse(RestStatus.OK, builder);
+            }
+        });
+    }
+}
