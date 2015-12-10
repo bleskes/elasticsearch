4307bb163b6a49a05769c55e825fdc423ee65988
Robert Muir <rmuir@apache.org>
2015-12-10 08:47:55 -0500

    fix x-pack compile/tests to work with https://github.com/elastic/elasticsearch/pull/15328
    Squashed commit of the following:
    
    commit 54de841112778b01e817e465d2f59840970bb4f3
    Author: Robert Muir <rmuir@apache.org>
    Date:   Thu Dec 10 08:47:12 2015 -0500
    
        see exactly how far MessyTestPlugin can go (unit tests depend on both groovy and mustache!)
    
    commit 743b5a4e0cc9a05e307339dfcb4569feed31f337
    Author: Robert Muir <rmuir@apache.org>
    Date:   Thu Dec 10 08:35:03 2015 -0500
    
        fix x-pack compile/tests to work with https://github.com/elastic/elasticsearch/pull/15328
    
    Original commit: elastic/x-pack@8e698583ccfeac7d0eb98fdbf1b0f4cb1e53c3e7

---
+++ elasticsearch/qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/package-info.java
@@ -0,0 +1,43 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+/**
+ * This package contains tests that use mustache to test what looks
+ * to be unrelated functionality, or functionality that should be 
+ * tested with a mock instead. Instead of doing an epic battle
+ * with these tests, they are temporarily moved here to the mustache
+ * module's tests, but that is likely not where they belong. Please 
+ * help by cleaning them up and we can remove this package!
+ *
+ * <ul>
+ *   <li>If the test is testing templating integration with another core subsystem,
+ *       fix it to use a mock instead, so it can be in the core tests again</li>
+ *   <li>If the test is just being lazy, and does not really need templating to test
+ *       something, clean it up!</li>
+ * </ul>
+ */
+
+// renames that took place:
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/test/integration/BasicWatcherTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/BasicWatcherTests.java
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/actions/email/EmailActionIntegrationTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/EmailActionIntegrationTests.java
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/history/HistoryTemplateSearchInputMappingsTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/HistoryTemplateSearchInputMappingsTests.java
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/input/search/SearchInputTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/SearchInputTests.java
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/transform/search/SearchTransformTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/SearchTransformTests.java
+//  renamed:    x-pack/shield/src/test/java/org/elasticsearch/integration/ShieldCachePermissionTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/ShieldCachePermissionTests.java
+//  renamed:    x-pack/watcher/src/test/java/org/elasticsearch/watcher/actions/TimeThrottleIntegrationTests.java -> qa/messy-test-xpack-with-mustache/src/test/java/org/elasticsearch/messy/tests/TimeThrottleIntegrationTests.java
+
+package org.elasticsearch.messy.tests;
