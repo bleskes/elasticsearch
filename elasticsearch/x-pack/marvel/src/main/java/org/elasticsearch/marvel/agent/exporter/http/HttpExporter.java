8f6dd0c5303a882ac3920e7a95de0eae7420de65
Tanguy Leroux <tlrx.dev@gmail.com>
2016-01-18 13:48:16 +0100

    Marvel: Map renderers by object class rather than by name
    Original commit: elastic/x-pack@26276b9313d753f788e53988446e298d3a725cd1

--- elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/agent/exporter/http/HttpExporter.java
+++ elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/agent/exporter/http/HttpExporter.java
@@ -103,7 +103,7 @@ public class HttpExporter extends Exporter {
     final boolean hostnameVerification;
 
     final Environment env;
-    final RendererRegistry rendererRegistry;
+    final RendererRegistry renderers;
 
     final @Nullable TimeValue templateCheckTimeout;
 
@@ -121,7 +121,7 @@ public class HttpExporter extends Exporter {
 
         super(TYPE, config);
         this.env = env;
-        this.rendererRegistry = rendererRegistry;
+        this.renderers = rendererRegistry;
 
         hosts = config.settings().getAsArray(HOST_SETTING, Strings.EMPTY_ARRAY);
         if (hosts.length == 0) {
@@ -194,9 +194,12 @@ public class HttpExporter extends Exporter {
         final XContentType xContentType = XContentType.SMILE;
 
         // Get the appropriate renderer in order to render the MarvelDoc
-        Renderer renderer = rendererRegistry.renderer(marvelDoc.type());
+        Renderer renderer = renderers.getRenderer(marvelDoc);
+        assert renderer != null : "unable to render marvel document of type [" + marvelDoc.type() + "]. no renderer found in registry";
+
         if (renderer == null) {
-            logger.warn("unable to render marvel document of type [{}]. no renderer found in registry", marvelDoc.type());
+            logger.warn("http exporter [{}] - unable to render marvel document of type [{}]: no renderer found in registry",
+                    name(), marvelDoc.type());
             return;
         }
 
