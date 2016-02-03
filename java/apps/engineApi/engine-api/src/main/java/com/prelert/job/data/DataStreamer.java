0cc99839ad883500fa00006253b5e029eaacba4c
Dimitrios Athanasiou <dimitris@prelert.com>
2016-02-03 18:44:51 +0000

    Fix java compilation error
    Former-commit-id: 3926e204bb5709b09e71e8f10e53c43e9140c418
    
    Original commit: elastic/x-pack@5e70c995c51bbee2f5aef845fdb86b27cddba13c

--- java/apps/engineApi/engine-api/src/main/java/com/prelert/job/data/DataStreamer.java
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/job/data/DataStreamer.java
@@ -62,10 +62,10 @@ public class DataStreamer
 
     /**
      * Persisted data files are named with this date format
-     * e.g. Tue_22_Apr_2014_091033
+     * e.g. 2014_01_28_091033_748
      */
-    private static final SimpleDateFormat PERSISTED_FILE_NAME_DATE_FORMAT =
-            new SimpleDateFormat("yyyy_MM_dd_HHmmss_SSS");
+    private static final DateTimeFormatter PERSISTED_FILE_NAME_DATE_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy_MM_dd_HHmmss_SSS");
 
     private final boolean m_ShouldPersistDataToDisk;
     private final String m_BaseDirectory;
