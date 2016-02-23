40377cd18cc7fa1841d548f930af29a74d64e0d3
David Roberts <dave@prelert.com>
2016-02-23 15:40:34 +0000

    Fix typo
    Former-commit-id: fd9986bac9d9f1381e20ab42f1a8544ccf209f90
    
    Original commit: elastic/x-pack@6a45a8bbbdc24d205f0dcb6cd1985336c647d519

--- java/apps/engineApi/elasticsearch-persistence/src/main/java/com/prelert/job/persistence/elasticsearch/ElasticsearchMappings.java
+++ java/apps/engineApi/elasticsearch-persistence/src/main/java/com/prelert/job/persistence/elasticsearch/ElasticsearchMappings.java
@@ -409,7 +409,7 @@ public class ElasticsearchMappings
                             .field(TYPE, LONG)
                         .endObject()
                         .startObject(Bucket.BUCKET_INFLUENCERS)
-                            .startObject(TYPE, NESTED)
+                            .field(TYPE, NESTED)
                             .startObject(PROPERTIES)
                                 .startObject(BucketInfluencer.INFLUENCER_FIELD_NAME)
                                     .field(TYPE, STRING).field(INDEX, NOT_ANALYZED)
