9e0b58fcf1edb72913c30d05c6fc6031309d62bf
jaymode <jay.modi@elasticsearch.com>
2016-03-15 13:10:40 -0400

    security: file parsing only supports the new format
    This commit remove the pre-existing file parsing code and replaces it with the updated
    code in the RoleDescriptor class. This unifies the parsing for the files and API for roles.
    
    Closes elastic/x-pack#1596
    
    Original commit: elastic/x-pack@3cd6627e9f18f548e261f8ef5d09d493f5daf194

--- elasticsearch/x-pack/shield/src/test/java/org/elasticsearch/shield/authc/RunAsIntegTests.java
+++ elasticsearch/x-pack/shield/src/test/java/org/elasticsearch/shield/authc/RunAsIntegTests.java
@@ -48,9 +48,9 @@ public class RunAsIntegTests extends ShieldIntegTestCase {
     static final String TRANSPORT_CLIENT_USER = "transport_user";
     static final String ROLES =
             "transport_client:\n" +
-            "  cluster: cluster:monitor/nodes/liveness\n" +
+            "  cluster: [ 'cluster:monitor/nodes/liveness' ]\n" +
             "run_as_role:\n" +
-            "  run_as: " + ShieldSettingsSource.DEFAULT_USER_NAME + ",idontexist\n";
+            "  run_as: [ '" + ShieldSettingsSource.DEFAULT_USER_NAME + "', 'idontexist' ]\n";
 
     @Override
     public Settings nodeSettings(int nodeOrdinal) {
