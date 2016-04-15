2df6d5b27e5090b9b2fce65062a8e265a19a8838
Christoph Büscher <christoph@elastic.co>
2016-04-15 16:48:11 +0200

    Adapt to changes in core ES (elastic/x-pack#17417)
    Original commit: elastic/x-pack@25c248c588fed2250e4121079af183b1f9694432

--- elasticsearch/x-pack/watcher/src/main/java/org/elasticsearch/watcher/transform/search/SearchTransformFactory.java
+++ elasticsearch/x-pack/watcher/src/main/java/org/elasticsearch/watcher/transform/search/SearchTransformFactory.java
@@ -17,6 +17,9 @@
 
 package org.elasticsearch.watcher.transform.search;
 
+import java.io.IOException;
+
+import org.elasticsearch.common.ParseFieldMatcher;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.settings.Settings;
@@ -25,11 +28,9 @@ import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.query.QueryParseContext;
 import org.elasticsearch.indices.query.IndicesQueriesRegistry;
 import org.elasticsearch.search.aggregations.AggregatorParsers;
-import org.elasticsearch.watcher.support.init.proxy.WatcherClientProxy;
 import org.elasticsearch.search.suggest.Suggesters;
+import org.elasticsearch.watcher.support.init.proxy.WatcherClientProxy;
 import org.elasticsearch.watcher.transform.TransformFactory;
-
-import java.io.IOException;
 
 /**
  *
@@ -41,12 +42,14 @@ public class SearchTransformFactory extends TransformFactory<SearchTransform, Se
     private final IndicesQueriesRegistry queryRegistry;
     private final AggregatorParsers aggParsers;
     private final Suggesters suggesters;
+    private final ParseFieldMatcher parseFieldMatcher;
 
     @Inject
     public SearchTransformFactory(Settings settings, WatcherClientProxy client, IndicesQueriesRegistry queryRegistry,
                                   AggregatorParsers aggParsers, Suggesters suggesters) {
         super(Loggers.getLogger(ExecutableSearchTransform.class, settings));
         this.client = client;
+        this.parseFieldMatcher = new ParseFieldMatcher(settings);
         this.queryRegistry = queryRegistry;
         this.aggParsers = aggParsers;
         this.suggesters = suggesters;
@@ -60,8 +63,7 @@ public class SearchTransformFactory extends TransformFactory<SearchTransform, Se
 
     @Override
     public SearchTransform parseTransform(String watchId, XContentParser parser) throws IOException {
-        QueryParseContext context = new QueryParseContext(queryRegistry);
-        context.reset(parser);
+        QueryParseContext context = new QueryParseContext(queryRegistry, parser, parseFieldMatcher);
         return SearchTransform.parse(watchId, parser, context, aggParsers, suggesters);
     }
 
