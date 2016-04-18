24001927757136be53ddf029544c8e3b1392d80e
Ryan Ernst <ryan@iernst.net>
2016-04-18 14:37:17 -0700

    Remove XContentBuilderString
    This is the xplugins side of elastic/elasticsearchelastic/x-pack#17833
    
    Original commit: elastic/x-pack@8be3ed67d5b8a3e08aa1d82c861d0635ce7df175

--- elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/agent/resolver/cluster/DiscoveryNodeResolver.java
+++ elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/agent/resolver/cluster/DiscoveryNodeResolver.java
@@ -20,7 +20,6 @@ package org.elasticsearch.marvel.agent.resolver.cluster;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentBuilderString;
 import org.elasticsearch.marvel.agent.collector.cluster.DiscoveryNodeMonitoringDoc;
 import org.elasticsearch.marvel.agent.resolver.MonitoringIndexNameResolver;
 
@@ -67,10 +66,10 @@ public class DiscoveryNodeResolver extends MonitoringIndexNameResolver.Data<Disc
     }
 
     static final class Fields {
-        static final XContentBuilderString NODE = new XContentBuilderString(TYPE);
-        static final XContentBuilderString NAME = new XContentBuilderString("name");
-        static final XContentBuilderString TRANSPORT_ADDRESS = new XContentBuilderString("transport_address");
-        static final XContentBuilderString ATTRIBUTES = new XContentBuilderString("attributes");
-        static final XContentBuilderString ID = new XContentBuilderString("id");
+        static final String NODE = new String(TYPE);
+        static final String NAME = "name";
+        static final String TRANSPORT_ADDRESS = "transport_address";
+        static final String ATTRIBUTES = "attributes";
+        static final String ID = "id";
     }
 }
