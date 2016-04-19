63517099fb1c6b3ae4227bbfff4013feef42c729
David Roberts <dave@prelert.com>
2016-04-19 09:08:16 +0100

    Remove unused import
    Former-commit-id: 09d52908fe4a3a90e1116fe736f2adb5dc10a887
    
    Original commit: elastic/x-pack@84f5e12c8e04e148319613430ca99442dce9048c

--- java/apps/engineApi/engine-api/src/main/java/com/prelert/job/process/ProcessCtrl.java
+++ java/apps/engineApi/engine-api/src/main/java/com/prelert/job/process/ProcessCtrl.java
@@ -38,7 +38,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 
 import org.apache.commons.lang.SystemUtils;
