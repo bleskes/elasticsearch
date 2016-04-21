dd74db5dedff19d302cbbae100c4611afb659f93
Nik Everett <nik9000@gmail.com>
2016-04-21 07:58:51 -0400

    Remove readFrom from xpack
    Writeable#readFrom has become a method you just implement because
    the interface requires it but the prefered way to actually do the
    reading is a ctor that takes a StreamReader. readFrom just delegates
    to the ctor. This removes readFrom entirely because it is not needed
    anymore and is going away in core.
    
    Relates to https://github.com/elastic/elasticsearch/issues/17085
    
    Original commit: elastic/x-pack@c62038dd8b30f3d786e6776549235969ad756fd0

--- elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/action/MonitoringBulkDoc.java
+++ elasticsearch/x-pack/marvel/src/main/java/org/elasticsearch/marvel/action/MonitoringBulkDoc.java
@@ -36,12 +36,24 @@ public class MonitoringBulkDoc extends MonitoringDoc {
         super(monitoringId, monitoringVersion);
     }
 
+    /**
+     * Read from a stream.
+     */
     public MonitoringBulkDoc(StreamInput in) throws IOException {
         super(in);
         index = in.readOptionalString();
         type = in.readOptionalString();
         id = in.readOptionalString();
         source = in.readBytesReference();
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeOptionalString(index);
+        out.writeOptionalString(type);
+        out.writeOptionalString(id);
+        out.writeBytesReference(source);
     }
 
     public String getIndex() {
@@ -74,19 +86,5 @@ public class MonitoringBulkDoc extends MonitoringDoc {
 
     public void setSource(BytesReference source) {
         this.source = source;
-    }
-
-    @Override
-    public void writeTo(StreamOutput out) throws IOException {
-        super.writeTo(out);
-        out.writeOptionalString(index);
-        out.writeOptionalString(type);
-        out.writeOptionalString(id);
-        out.writeBytesReference(source);
-    }
-
-    @Override
-    public MonitoringBulkDoc readFrom(StreamInput in) throws IOException {
-        return new MonitoringBulkDoc(in);
     }
 }
