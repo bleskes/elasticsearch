dde98002e6ff18e5cc577f98342f700d358b502f
Henry Coggill <hcoggill@prelert.com>
2016-05-16 10:10:08 +0100

    Don’t declare runtime exception
    Former-commit-id: 64e28c88fad2104562f3ab6625fa2a9f15bc8d49
    
    Original commit: elastic/x-pack@913147e2c0de1a8c1148238b7412ca714899b003

--- java/apps/engineApi/apiServer/src/main/java/com/prelert/rs/provider/EngineStatusWriter.java
+++ java/apps/engineApi/apiServer/src/main/java/com/prelert/rs/provider/EngineStatusWriter.java
@@ -32,7 +32,6 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -78,7 +77,7 @@ public class EngineStatusWriter implements MessageBodyWriter<EngineStatus>
             Type genericType, Annotation[] annotations, MediaType mediaType,
             MultivaluedMap<String, Object> httpHeaders,
             OutputStream entityStream)
-    throws IOException, WebApplicationException
+    throws IOException
     {
         OBJECT_WRITER.writeValue(entityStream, bean);
     }
