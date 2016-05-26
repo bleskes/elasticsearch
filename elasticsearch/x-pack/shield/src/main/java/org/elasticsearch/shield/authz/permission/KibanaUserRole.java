00e129e342775053ea8250abb82c823bd3507c82
jaymode <jay.modi@elasticsearch.com>
2016-05-26 16:45:59 -0400

    security: add a built in kibana_user role
    This commit adds a `kibana_user` role that can be used to grant the minimum
    set of privileges necessary to access kibana.
    
    Closes elastic/x-pack#2166
    
    Original commit: elastic/x-pack@9e7a5e3739ff27493773726cff40341735446786

---
+++ elasticsearch/x-pack/shield/src/main/java/org/elasticsearch/shield/authz/permission/KibanaUserRole.java
@@ -0,0 +1,40 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.shield.authz.permission;
+
+import org.elasticsearch.shield.authz.RoleDescriptor;
+import org.elasticsearch.shield.authz.privilege.ClusterPrivilege;
+import org.elasticsearch.shield.authz.privilege.Privilege.Name;
+
+public class KibanaUserRole extends Role {
+
+    private static final String[] CLUSTER_PRIVILEGES = new String[] { "monitor" };
+    private static final RoleDescriptor.IndicesPrivileges[] INDICES_PRIVILEGES = new RoleDescriptor.IndicesPrivileges[] {
+            RoleDescriptor.IndicesPrivileges.builder().indices(".kibana*").privileges("manage", "read", "index").build() };
+
+    public static final String NAME = "kibana_user";
+    public static final RoleDescriptor DESCRIPTOR = new RoleDescriptor(NAME, CLUSTER_PRIVILEGES, INDICES_PRIVILEGES, null);
+    public static final KibanaUserRole INSTANCE = new KibanaUserRole();
+
+    private KibanaUserRole() {
+        super(DESCRIPTOR.getName(),
+                new ClusterPermission.Core(ClusterPrivilege.get(new Name(DESCRIPTOR.getClusterPrivileges()))),
+                new IndicesPermission.Core(Role.Builder.convertFromIndicesPrivileges(DESCRIPTOR.getIndicesPrivileges())),
+                RunAsPermission.Core.NONE);
+    }
+}
