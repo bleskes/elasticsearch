4555db634ce35b1aced0ae573ba1314f9604bb87
Ryan Ernst <ryan@iernst.net>
2016-07-01 13:45:16 -0700

    Uninject HtmlSanitizer
    This is just a utility used by the email action and does not need to be
    injected.
    
    Original commit: elastic/x-pack@823dc34c214ab3c43ccc845f293f26eaf6350880

--- elasticsearch/x-pack/watcher/src/test/java/org/elasticsearch/xpack/watcher/watch/WatchTests.java
+++ elasticsearch/x-pack/watcher/src/test/java/org/elasticsearch/xpack/watcher/watch/WatchTests.java
@@ -481,7 +481,7 @@ public class WatchTests extends ESTestCase {
         for (ActionWrapper action : actions) {
             switch (action.action().type()) {
                 case EmailAction.TYPE:
-                    parsers.put(EmailAction.TYPE, new EmailActionFactory(settings, emailService, templateEngine, htmlSanitizer,
+                    parsers.put(EmailAction.TYPE, new EmailActionFactory(settings, emailService, templateEngine,
                             new EmailAttachmentsParser(Collections.emptyMap())));
                     break;
                 case IndexAction.TYPE:
