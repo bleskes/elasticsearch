9e7f6532fb0c272a1a6d49f3910bcfc30a8ecdf2
Simon Willnauer <simonw@apache.org>
2016-07-12 17:56:06 +0200

    Add MockNettyPlugin to sidestep permissions assertion in pseudo integ tests
    Original commit: elastic/x-pack@3d2c651d90a2b1a037b2279c55c0667ec31915a0

--- elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/plugin/LicensesServiceClusterTests.java
+++ elasticsearch/x-pack/license-plugin/src/test/java/org/elasticsearch/license/plugin/LicensesServiceClusterTests.java
@@ -36,12 +36,11 @@ import org.elasticsearch.license.plugin.core.LicensesService;
 import org.elasticsearch.license.plugin.core.LicensesStatus;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
-import org.elasticsearch.transport.NettyPlugin;
+import org.elasticsearch.xpack.MockNettyPlugin;
 import org.elasticsearch.xpack.XPackPlugin;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 import static org.elasticsearch.license.plugin.TestUtils.generateSignedLicense;
 import static org.elasticsearch.test.ESIntegTestCase.Scope.TEST;
@@ -72,7 +71,7 @@ public class LicensesServiceClusterTests extends AbstractLicensesIntegrationTest
 
     @Override
     protected Collection<Class<? extends Plugin>> nodePlugins() {
-        return Arrays.asList(XPackPlugin.class, NettyPlugin.class);
+        return Arrays.asList(XPackPlugin.class, MockNettyPlugin.class);
     }
 
     @Override
---
+++ elasticsearch/x-pack/src/test/java/org/elasticsearch/xpack/MockNettyPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+package org.elasticsearch.xpack;
+
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.transport.NettyPlugin;
+
+public final class MockNettyPlugin extends NettyPlugin {
+    // se NettyPlugin.... this runs without the permission from the netty module so it will fail since reindex can't set the property
+    // to make it still work we disable that check for pseudo integ tests
+    public MockNettyPlugin(Settings settings) {
+        super(Settings.builder().put(settings).put("netty.assert.buglevel", false).build());
+    }
+}
