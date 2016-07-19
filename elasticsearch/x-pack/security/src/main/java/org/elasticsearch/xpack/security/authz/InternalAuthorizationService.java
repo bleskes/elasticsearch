9559dbd6c1973526affb8dc69303ababdeb8f7d4
Ryan Ernst <ryan@iernst.net>
2016-07-19 13:41:19 -0700

    Internal: Remove use of AuditTrail interface in place of AuditTrailService
    We currently have a number of actions and components which try to write
    to the audit trail. But they do so by expecting a bound AuditTrail
    object. In reality, this should always be AuditTrailService, except when
    security is disabled. This change removes the use of the AuditTrail
    interface for that purpose, and instead makes the AuditTrailService
    allow an empty list of trails, so that it is always bound when running
    on a node.
    
    Original commit: elastic/x-pack@c8e0b6c67516b04d33b00f9dd085d26e1ad4fe7e

--- elasticsearch/x-pack/security/src/main/java/org/elasticsearch/xpack/security/authz/InternalAuthorizationService.java
+++ elasticsearch/x-pack/security/src/main/java/org/elasticsearch/xpack/security/authz/InternalAuthorizationService.java
@@ -39,6 +39,7 @@ import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.common.util.set.Sets;
 import org.elasticsearch.search.action.SearchTransportService;
 import org.elasticsearch.xpack.security.SecurityTemplateService;
+import org.elasticsearch.xpack.security.audit.AuditTrailService;
 import org.elasticsearch.xpack.security.authc.Authentication;
 import org.elasticsearch.xpack.security.user.AnonymousUser;
 import org.elasticsearch.xpack.security.user.SystemUser;
@@ -92,7 +93,7 @@ public class InternalAuthorizationService extends AbstractComponent implements A
 
     @Inject
     public InternalAuthorizationService(Settings settings, RolesStore rolesStore, ClusterService clusterService,
-                                        AuditTrail auditTrail, AuthenticationFailureHandler authcFailureHandler,
+                                        AuditTrailService auditTrail, AuthenticationFailureHandler authcFailureHandler,
                                         ThreadPool threadPool, IndexNameExpressionResolver nameExpressionResolver) {
         super(settings);
         this.rolesStore = rolesStore;
