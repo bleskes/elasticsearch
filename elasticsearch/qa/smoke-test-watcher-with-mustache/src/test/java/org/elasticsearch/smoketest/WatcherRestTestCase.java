c47a7f839d94e02726856b49467853996390e01f
Nik Everett <nik9000@gmail.com>
2016-07-20 15:55:30 -0400

    Handle core renaming ESRestTestCase
    Original commit: elastic/x-pack@5743d7568cd80cfe0b9fc399c13ceb707e594a98

--- elasticsearch/qa/smoke-test-watcher-with-mustache/src/test/java/org/elasticsearch/smoketest/WatcherRestTestCase.java
+++ elasticsearch/qa/smoke-test-watcher-with-mustache/src/test/java/org/elasticsearch/smoketest/WatcherRestTestCase.java
@@ -19,7 +19,8 @@ package org.elasticsearch.smoketest;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
-import org.elasticsearch.test.rest.ESRestTestCase;
+
+import org.elasticsearch.test.rest.ESClientYamlSuiteTestCase;
 import org.elasticsearch.test.rest.RestTestCandidate;
 import org.elasticsearch.test.rest.parser.RestTestParseException;
 import org.junit.After;
@@ -30,7 +31,7 @@ import java.io.IOException;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 
-public abstract class WatcherRestTestCase extends ESRestTestCase {
+public abstract class WatcherRestTestCase extends ESClientYamlSuiteTestCase {
 
     public WatcherRestTestCase(@Name("yaml") RestTestCandidate testCandidate) {
         super(testCandidate);
@@ -38,7 +39,7 @@ public abstract class WatcherRestTestCase extends ESRestTestCase {
 
     @ParametersFactory
     public static Iterable<Object[]> parameters() throws IOException, RestTestParseException {
-        return ESRestTestCase.createParameters(0, 1);
+        return ESClientYamlSuiteTestCase.createParameters(0, 1);
     }
 
     @Before
