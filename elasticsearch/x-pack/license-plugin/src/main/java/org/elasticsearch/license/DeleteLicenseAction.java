2413c4f288ea0bfef6542ec5831db75c1ed7761a
Ryan Ernst <ryan@iernst.net>
2016-07-26 13:26:29 -0700

    Cleanup: Move all license related classes into a single package
    This change moves all license related code in xpack under
    org.elasticsearch.license. Some classes become package private (more can
    become package private later with further deguicing). It also adds
    package level javadocs. Note, I did not move anything from the actual
    licensing jar.  This should be cleaned up in a followup.
    
    Original commit: elastic/x-pack@cfdcc3e9153992400b64089f9ba94bcd85d93212

---
+++ elasticsearch/x-pack/license-plugin/src/main/java/org/elasticsearch/license/DeleteLicenseAction.java
@@ -0,0 +1,40 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ *  __________________
+ *
+ * [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+package org.elasticsearch.license;
+
+import org.elasticsearch.action.Action;
+import org.elasticsearch.client.ElasticsearchClient;
+
+public class DeleteLicenseAction extends Action<DeleteLicenseRequest, DeleteLicenseResponse, DeleteLicenseRequestBuilder> {
+
+    public static final DeleteLicenseAction INSTANCE = new DeleteLicenseAction();
+    public static final String NAME = "cluster:admin/xpack/license/delete";
+
+    private DeleteLicenseAction() {
+        super(NAME);
+    }
+
+    @Override
+    public DeleteLicenseResponse newResponse() {
+        return new DeleteLicenseResponse();
+    }
+
+    @Override
+    public DeleteLicenseRequestBuilder newRequestBuilder(ElasticsearchClient client) {
+        return new DeleteLicenseRequestBuilder(client, this);
+    }
+}
---
+++ elasticsearch/x-pack/license-plugin/src/main/java/org/elasticsearch/license/LicenseUtils.java
@@ -0,0 +1,48 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ *  __________________
+ *
+ * [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.license;
+
+import org.elasticsearch.ElasticsearchSecurityException;
+import org.elasticsearch.rest.RestStatus;
+
+class LicenseUtils {
+
+    public static final String EXPIRED_FEATURE_HEADER = "es.license.expired.feature";
+
+    /**
+     * Exception to be thrown when a feature action requires a valid license, but license
+     * has expired
+     *
+     * <code>feature</code> accessible through {@link #EXPIRED_FEATURE_HEADER} in the
+     * exception's rest header
+     */
+    public static ElasticsearchSecurityException newComplianceException(String feature) {
+        ElasticsearchSecurityException e = new ElasticsearchSecurityException("current license is non-compliant for [{}]",
+                RestStatus.FORBIDDEN, feature);
+        e.addHeader(EXPIRED_FEATURE_HEADER, feature);
+        return e;
+    }
+
+    /**
+     * Checks if a given {@link ElasticsearchSecurityException} refers to a feature that
+     * requires a valid license, but the license has expired.
+     */
+    public static boolean isLicenseExpiredException(ElasticsearchSecurityException exception) {
+        return (exception != null) && (exception.getHeader(EXPIRED_FEATURE_HEADER) != null);
+    }
+}
