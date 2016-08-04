f3e38ee0b9cf14d87d6ac9869141f490c8043b39
Henry Coggill <hcoggill@prelert.com>
2016-08-04 14:20:38 +0100

    Elastic unittests
    Former-commit-id: 05592caafa0461e25bcab8eebf24996f5b2baa2d
    
    Original commit: elastic/x-pack@98f9e7a508f5da2d920e6e98d4efcf12be060fb5

--- java/apps/engineApi/elasticsearch-persistence/src/test/java/com/prelert/job/persistence/elasticsearch/ElasticsearchJobProviderTest.java
+++ java/apps/engineApi/elasticsearch-persistence/src/test/java/com/prelert/job/persistence/elasticsearch/ElasticsearchJobProviderTest.java
@@ -42,6 +42,7 @@ import java.util.concurrent.ExecutionException;
 
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.node.Node;
 import org.junit.Before;
 import org.junit.Rule;
@@ -50,11 +51,15 @@ import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.prelert.job.JobConfiguration;
+import com.prelert.job.JobDetails;
+import com.prelert.job.ModelSizeStats;
 import com.prelert.job.SchedulerState;
 import com.prelert.job.UnknownJobException;
 import com.prelert.job.errorcodes.ErrorCodeMatcher;
 import com.prelert.job.errorcodes.ErrorCodes;
 import com.prelert.job.quantiles.Quantiles;
+import com.prelert.job.results.BucketProcessingTime;
 
 public class ElasticsearchJobProviderTest
 {
@@ -212,6 +217,184 @@ public class ElasticsearchJobProviderTest
         assertTrue(schedulerState.isPresent());
         assertEquals(18L, schedulerState.get().getStartTimeMillis().longValue());
         assertEquals(42L, schedulerState.get().getEndTimeMillis().longValue());
+    }
+
+    @Test
+    public void testConstructor_GivenRedClusterState() throws InterruptedException,
+            ExecutionException
+    {
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusRedResponse();
+
+        try
+        {
+            ElasticsearchJobProvider provider = createProvider(clientBuilder.build());
+            assertTrue(false);
+            provider.shutdown();
+        }
+        catch (IllegalStateException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testIsConnected() throws InterruptedException, ExecutionException
+    {
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true)
+                .addClusterStatusYellowResponse("prelertresults-id", TimeValue.timeValueSeconds(2L));
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        assertTrue(provider.isConnected("id"));
+    }
+
+    @Test
+    public void testCreateUsageMetering() throws InterruptedException, ExecutionException
+    {
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, false)
+                .prepareCreate(ElasticsearchJobProvider.PRELERT_USAGE_INDEX)
+                .addClusterStatusYellowResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX);
+        Client client = clientBuilder.build();
+        createProvider(client);
+        clientBuilder.verifyIndexCreated(ElasticsearchJobProvider.PRELERT_USAGE_INDEX);
+    }
+
+    @Test
+    public void testSavePrelertInfo() throws InterruptedException, ExecutionException
+    {
+        String index = "prelert-int";
+        String info = "info";
+
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true)
+                .prepareCreate(index)
+                .addClusterStatusYellowResponse(index);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+
+        clientBuilder.resetIndices();
+        clientBuilder.prepareCreate(index);
+        clientBuilder.addIndicesExistsResponse(index, false);
+        clientBuilder.prepareIndex(index,  info);
+        client = clientBuilder.build();
+
+        provider.savePrelertInfo(info);
+        clientBuilder.verifyIndexCreated(index);
+    }
+
+    @Test
+    public void testCheckJobExists() throws InterruptedException, ExecutionException
+    {
+        GetResponse getResponse = createGetResponse(true, null);
+        String jobId = "jobThing";
+
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true)
+                .prepareGet("prelertresults-" + jobId, JobDetails.TYPE, jobId, getResponse);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        try
+        {
+            provider.checkJobExists(jobId);
+        }
+        catch (UnknownJobException e)
+        {
+            assertTrue(false);
+        }
+    }
+
+
+    @Test
+    public void testCheckJobExists_GivenInvalidId() throws InterruptedException, ExecutionException
+    {
+        GetResponse getResponse = createGetResponse(false, null);
+        String jobId = "jobThing";
+
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true)
+                .prepareGet("prelertresults-" + jobId, JobDetails.TYPE, jobId, getResponse);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        try
+        {
+            provider.checkJobExists(jobId);
+            assertTrue(false);
+        }
+        catch (UnknownJobException e)
+        {
+        }
+    }
+
+    @Test
+    public void testJobIdIsUnique_GivenSingleJob() throws InterruptedException, ExecutionException
+    {
+        String jobId = "jobThing";
+
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        clientBuilder.resetIndices();
+        clientBuilder.addIndicesExistsResponse("prelertresults-" + jobId, false);
+        client = clientBuilder.build();
+        assertTrue(provider.jobIdIsUnique(jobId));
+    }
+
+    @Test
+    public void testJobIdIsUnique_GivenJobExists() throws InterruptedException, ExecutionException
+    {
+        String jobId = "jobThing";
+
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        clientBuilder.resetIndices();
+        clientBuilder.addIndicesExistsResponse("prelertresults-" + jobId, true);
+        client = clientBuilder.build();
+        assertFalse(provider.jobIdIsUnique(jobId));
+    }
+
+    @Test
+    public void testGetJobDetails() throws InterruptedException, ExecutionException
+    {
+        String jobId = "myJob";
+        Map<String, Object> source = new HashMap<>();
+        source.put("jobId",  jobId);
+        source.put("description", "This is a job description");
+        GetResponse response = createGetResponse(true, source);
+
+        GetResponse response2 = createGetResponse(false, null);
+        MockClientBuilder clientBuilder = new MockClientBuilder(CLUSTER_NAME)
+                .addClusterStatusYellowResponse()
+                .addIndicesExistsResponse(ElasticsearchJobProvider.PRELERT_USAGE_INDEX, true)
+                .prepareGet("prelertresults-" + jobId, JobDetails.TYPE, jobId, response)
+                .prepareGet("prelertresults-" + jobId, ModelSizeStats.TYPE, "modelSizeStats", response2)
+                .prepareGet("prelertresults-" + jobId, BucketProcessingTime.TYPE, BucketProcessingTime.AVERAGE_PROCESSING_TIME_MS, response2);
+
+        Client client = clientBuilder.build();
+        ElasticsearchJobProvider provider = createProvider(client);
+        Optional<JobDetails> details = provider.getJobDetails(jobId);
+        assertTrue(details.isPresent());
+        JobConfiguration config = new JobConfiguration();
+        config.setDescription("This is a job description");
+        JobDetails expected = new JobDetails(jobId, config);
+        assertEquals(details.get().allFields(), expected.allFields());
     }
 
     private ElasticsearchJobProvider createProvider(Client client)
--- java/apps/engineApi/elasticsearch-persistence/src/test/java/com/prelert/job/persistence/elasticsearch/MockClientBuilder.java
+++ java/apps/engineApi/elasticsearch-persistence/src/test/java/com/prelert/job/persistence/elasticsearch/MockClientBuilder.java
@@ -28,9 +28,12 @@
 package com.prelert.job.persistence.elasticsearch;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.concurrent.ExecutionException;
@@ -39,15 +42,22 @@ import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.ListenableActionFuture;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequestBuilder;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
 import org.elasticsearch.action.get.GetRequestBuilder;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.AdminClient;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.ClusterAdminClient;
 import org.elasticsearch.client.IndicesAdminClient;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.IndexNotFoundException;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
@@ -75,6 +85,14 @@ public class MockClientBuilder
         when(m_Client.settings()).thenReturn(settings);
     }
 
+    public MockClientBuilder addClusterStatusYellowResponse(String index, TimeValue timeout) throws InterruptedException, ExecutionException
+    {
+        ClusterHealthRequestBuilder clusterHealthRequestBuilder = mock(ClusterHealthRequestBuilder.class);
+        when(m_ClusterAdminClient.prepareHealth(index)).thenReturn(clusterHealthRequestBuilder);
+        when(clusterHealthRequestBuilder.get(timeout)).thenReturn(mock(ClusterHealthResponse.class));
+        return this;
+    }
+
     @SuppressWarnings({ "unchecked" })
     public MockClientBuilder addClusterStatusYellowResponse() throws InterruptedException, ExecutionException
     {
@@ -85,6 +103,34 @@ public class MockClientBuilder
         when(clusterHealthRequestBuilder.setWaitForYellowStatus()).thenReturn(clusterHealthRequestBuilder);
         when(clusterHealthRequestBuilder.execute()).thenReturn(actionFuture);
         when(actionFuture.actionGet()).thenReturn(mock(ClusterHealthResponse.class));
+        return this;
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public MockClientBuilder addClusterStatusYellowResponse(String index) throws InterruptedException, ExecutionException
+    {
+        ListenableActionFuture<ClusterHealthResponse> actionFuture = mock(ListenableActionFuture.class);
+        ClusterHealthRequestBuilder clusterHealthRequestBuilder = mock(ClusterHealthRequestBuilder.class);
+
+        when(m_ClusterAdminClient.prepareHealth(index)).thenReturn(clusterHealthRequestBuilder);
+        when(clusterHealthRequestBuilder.setWaitForYellowStatus()).thenReturn(clusterHealthRequestBuilder);
+        when(clusterHealthRequestBuilder.execute()).thenReturn(actionFuture);
+        when(actionFuture.actionGet()).thenReturn(mock(ClusterHealthResponse.class));
+        return this;
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public MockClientBuilder addClusterStatusRedResponse() throws InterruptedException, ExecutionException
+    {
+        ListenableActionFuture<ClusterHealthResponse> actionFuture = mock(ListenableActionFuture.class);
+        ClusterHealthRequestBuilder clusterHealthRequestBuilder = mock(ClusterHealthRequestBuilder.class);
+
+        when(m_ClusterAdminClient.prepareHealth()).thenReturn(clusterHealthRequestBuilder);
+        when(clusterHealthRequestBuilder.setWaitForYellowStatus()).thenReturn(clusterHealthRequestBuilder);
+        when(clusterHealthRequestBuilder.execute()).thenReturn(actionFuture);
+        ClusterHealthResponse response = mock(ClusterHealthResponse.class);
+        when(response.getStatus()).thenReturn(ClusterHealthStatus.RED);
+        when(actionFuture.actionGet()).thenReturn(response);
         return this;
     }
 
@@ -101,6 +147,24 @@ public class MockClientBuilder
             return request.indices()[0].equals(index) ? actionFuture : null;
         }).when(m_IndicesAdminClient).exists(any(IndicesExistsRequest.class));
         when(actionFuture.get()).thenReturn(new IndicesExistsResponse(exists));
+        when(actionFuture.actionGet()).thenReturn(new IndicesExistsResponse(exists));
+        return this;
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public MockClientBuilder addIndicesExistsResponse2(String index, boolean exists) throws InterruptedException, ExecutionException
+    {
+        ActionFuture actionFuture = mock(ActionFuture.class);
+        ArgumentCaptor<IndicesExistsRequest> requestCaptor = ArgumentCaptor.forClass(IndicesExistsRequest.class);
+
+        when(m_IndicesAdminClient.exists(requestCaptor.capture())).thenReturn(actionFuture);
+        doAnswer(invocation ->
+        {
+            IndicesExistsRequest request = (IndicesExistsRequest) invocation.getArguments()[0];
+            return request.indices()[0].equals(index) ? actionFuture : null;
+        }).when(m_IndicesAdminClient).exists(any(IndicesExistsRequest.class));
+        when(actionFuture.get()).thenReturn(new IndicesExistsResponse(exists));
+
         return this;
     }
 
@@ -108,7 +172,33 @@ public class MockClientBuilder
     {
         GetRequestBuilder getRequestBuilder = mock(GetRequestBuilder.class);
         when(getRequestBuilder.get()).thenReturn(response);
+        when(getRequestBuilder.setFetchSource(false)).thenReturn(getRequestBuilder);
+        when(getRequestBuilder.setFields()).thenReturn(getRequestBuilder);
         when(m_Client.prepareGet(index, type, id)).thenReturn(getRequestBuilder);
+        return this;
+    }
+
+    public MockClientBuilder prepareCreate(String index)
+    {
+        CreateIndexRequestBuilder createIndexRequestBuilder = mock(CreateIndexRequestBuilder.class);
+        CreateIndexResponse response = mock(CreateIndexResponse.class);
+        when(createIndexRequestBuilder.setSettings(any(Settings.Builder.class))).thenReturn(createIndexRequestBuilder);
+        when(createIndexRequestBuilder.addMapping(any(String.class), any(XContentBuilder.class))).thenReturn(createIndexRequestBuilder);
+        when(createIndexRequestBuilder.get()).thenReturn(response);
+        when(m_IndicesAdminClient.prepareCreate(eq(index))).thenReturn(createIndexRequestBuilder);
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MockClientBuilder prepareIndex(String index, String source)
+    {
+        IndexRequestBuilder builder = mock(IndexRequestBuilder.class);
+        ListenableActionFuture<IndexResponse> actionFuture = mock(ListenableActionFuture.class);
+
+        when(m_Client.prepareIndex(eq(index), any(), any())).thenReturn(builder);
+        when(builder.setSource(eq(source))).thenReturn(builder);
+        when(builder.execute()).thenReturn(actionFuture);
+        when(actionFuture.actionGet()).thenReturn(mock(IndexResponse.class));
         return this;
     }
 
@@ -122,4 +212,15 @@ public class MockClientBuilder
     {
         return m_Client;
     }
+
+    public void verifyIndexCreated(String index)
+    {
+        verify(m_IndicesAdminClient).prepareCreate(eq(index));
+    }
+
+    public void resetIndices()
+    {
+        reset(m_IndicesAdminClient);
+    }
+
 }
