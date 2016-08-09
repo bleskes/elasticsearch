eccab616ca8d933c8962623e3ae7dacec3d6abb2
jaymode <jay.modi@elasticsearch.com>
2016-08-09 12:19:08 -0400

    fix serialization of XPackFeatureSet.Usage
    The XPackFeatureSet.Usage writeTo method failed to write the name to the
    StreamOutput whereas the deserialization code expected it to be the first value
    present in the StreamInput. This causes xpack usage requests made to nodes
    other than the master to fail.
    
    This change fixes the serialization and adds tests to all of the feature sets to ensure
    that the usage is serialized properly.
    
    Closes elastic/x-pack#3072
    
    Original commit: elastic/x-pack@c506e321633a056d9228146f89c3de143e529a26

--- elasticsearch/x-pack/src/main/java/org/elasticsearch/xpack/XPackFeatureSet.java
+++ elasticsearch/x-pack/src/main/java/org/elasticsearch/xpack/XPackFeatureSet.java
@@ -78,6 +78,7 @@ public interface XPackFeatureSet {
 
         @Override
         public void writeTo(StreamOutput out) throws IOException {
+            out.writeString(name);
             out.writeBoolean(available);
             out.writeBoolean(enabled);
         }
