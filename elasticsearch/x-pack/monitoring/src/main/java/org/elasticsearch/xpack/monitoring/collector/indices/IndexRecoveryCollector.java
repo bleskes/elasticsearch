14025cb17c114dfdc5df76394d8106e678eeb700
Chris Earle <chris.earle@elastic.co>
2016-08-17 20:50:47 -0400

    [Monitoring] Remove "agent" package
    This removes the "agent" package from org.elasticsearch.xpack.monitoring.agent.*, so that now everything is simply org.elasticsearch.xpack.monitoring.*.
    
    Follow-on work will be refactoring some of the other code, but this is a first step now that it's always the agent (in effect).
    
    Original commit: elastic/x-pack@de1e6eae2993f8c08849c2565e270c30596a84f4

---
+++ elasticsearch/x-pack/monitoring/src/main/java/org/elasticsearch/xpack/monitoring/collector/indices/IndexRecoveryCollector.java
@@ -0,0 +1,91 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.xpack.monitoring.collector.indices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.elasticsearch.action.admin.indices.recovery.RecoveryResponse;
+import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.license.XPackLicenseState;
+import org.elasticsearch.xpack.XPackSettings;
+import org.elasticsearch.xpack.monitoring.MonitoringSettings;
+import org.elasticsearch.xpack.monitoring.collector.AbstractCollector;
+import org.elasticsearch.xpack.monitoring.exporter.MonitoringDoc;
+import org.elasticsearch.xpack.security.InternalClient;
+
+/**
+ * Collector for the Recovery API.
+ * <p>
+ * This collector runs on the master node only and collects a {@link IndexRecoveryMonitoringDoc} document
+ * for every index that has on-going shard recoveries.
+ */
+public class IndexRecoveryCollector extends AbstractCollector {
+
+    public static final String NAME = "index-recovery-collector";
+
+    private final Client client;
+
+    public IndexRecoveryCollector(Settings settings, ClusterService clusterService,
+                                  MonitoringSettings monitoringSettings, XPackLicenseState licenseState, InternalClient client) {
+        super(settings, NAME, clusterService, monitoringSettings, licenseState);
+        this.client = client;
+    }
+
+    @Override
+    protected boolean shouldCollect() {
+        return super.shouldCollect() && isLocalNodeMaster();
+    }
+
+    @Override
+    protected Collection<MonitoringDoc> doCollect() throws Exception {
+        List<MonitoringDoc> results = new ArrayList<>(1);
+        try {
+            RecoveryResponse recoveryResponse = client.admin().indices().prepareRecoveries()
+                    .setIndices(monitoringSettings.indices())
+                    .setIndicesOptions(IndicesOptions.lenientExpandOpen())
+                    .setActiveOnly(monitoringSettings.recoveryActiveOnly())
+                    .get(monitoringSettings.recoveryTimeout());
+
+            if (recoveryResponse.hasRecoveries()) {
+                IndexRecoveryMonitoringDoc indexRecoveryDoc = new IndexRecoveryMonitoringDoc(monitoringId(), monitoringVersion());
+                indexRecoveryDoc.setClusterUUID(clusterUUID());
+                indexRecoveryDoc.setTimestamp(System.currentTimeMillis());
+                indexRecoveryDoc.setSourceNode(localNode());
+                indexRecoveryDoc.setRecoveryResponse(recoveryResponse);
+                results.add(indexRecoveryDoc);
+            }
+        } catch (IndexNotFoundException e) {
+            if (XPackSettings.SECURITY_ENABLED.get(settings)
+                && IndexNameExpressionResolver.isAllIndices(Arrays.asList(monitoringSettings.indices()))) {
+                logger.debug("collector [{}] - unable to collect data for missing index [{}]", name(), e.getIndex());
+            } else {
+                throw e;
+            }
+        }
+        return Collections.unmodifiableCollection(results);
+    }
+}
