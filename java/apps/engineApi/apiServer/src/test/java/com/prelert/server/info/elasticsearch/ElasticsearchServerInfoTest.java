894b85c593cd905d00e030511cb18be9ec0102d1
Henry Coggill <hcoggill@prelert.com>
2016-08-24 09:46:43 +0100

    Flesh out ServerInfo test
    Former-commit-id: e4505d9d338e5eb5054041459e29a18defc89166
    
    Original commit: elastic/x-pack@540ea7ffe84f57f82c2ebabe2b477f568f3d0d18

--- java/apps/engineApi/apiServer/src/test/java/com/prelert/server/info/elasticsearch/ElasticsearchServerInfoTest.java
+++ java/apps/engineApi/apiServer/src/test/java/com/prelert/server/info/elasticsearch/ElasticsearchServerInfoTest.java
@@ -30,9 +30,11 @@ package com.prelert.server.info.elasticsearch;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -44,15 +46,28 @@ import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
 import org.elasticsearch.action.admin.cluster.node.stats.NodeStats;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.indices.NodeIndicesStats;
+import org.elasticsearch.monitor.fs.FsInfo;
+import org.elasticsearch.monitor.fs.FsInfo.Path;
+import org.elasticsearch.monitor.jvm.JvmInfo;
+import org.elasticsearch.monitor.os.OsInfo;
+import org.elasticsearch.monitor.os.OsStats;
+import org.elasticsearch.monitor.os.OsStats.Mem;
+import org.elasticsearch.monitor.process.ProcessInfo;
+import org.elasticsearch.monitor.process.ProcessStats;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedMap;
 import com.prelert.server.info.ServerInfo;
+import com.prelert.utils.HostnameFinder;
 
 
 
@@ -105,6 +120,86 @@ public class ElasticsearchServerInfoTest
         assertNull(info.getTotalDiskMb());
         assertNull(info.getTotalMemoryMb());
         assertNull(info.getAvailableDiskMb());
+    }
+
+    @Test
+    public void testNodesStats() throws IOException
+    {
+        ElasticsearchServerInfo esi = Mockito.spy(new ElasticsearchServerInfo(m_Client));
+        NodeInfo[] nodes = new NodeInfo[1];
+        NodeInfo node = mock(NodeInfo.class);
+        nodes[0] = node;
+        DiscoveryNode discoveryNode = mock(DiscoveryNode.class);
+        when(node.getNode()).thenReturn(discoveryNode);
+        when(discoveryNode.isClientNode()).thenReturn(false);
+        when(discoveryNode.isDataNode()).thenReturn(false);
+
+        JvmInfo jvmInfo = JvmInfo.jvmInfo();
+        StreamInput si = mock(StreamInput.class);
+        when(si.readByte()).thenReturn((byte) 0x01);
+
+        when(si.getVersion()).thenReturn(Version.V_0_90_0_Beta1);
+        OsInfo osInfo = OsInfo.readOsInfo(si);
+        when(node.getOs()).thenReturn(osInfo);
+        when(node.getJvm()).thenReturn(jvmInfo);
+        when(node.getProcess()).thenReturn(new ProcessInfo(12345678, false));
+
+        NodeStats[] stats = new NodeStats[1];
+        NodeStats stat = mock(NodeStats.class);
+        stats[0] = stat;
+        when(stat.getNode()).thenReturn(discoveryNode);
+        when(stat.getHostname()).thenReturn("test_host");
+        when(stat.getIndices()).thenReturn(mock(NodeIndicesStats.class));
+
+        when(stat.getOs()).thenReturn(mock(OsStats.class));
+        when(stat.getProcess()).thenReturn(mock(ProcessStats.class));
+        when(stat.getJvm()).thenReturn(mock(org.elasticsearch.monitor.jvm.JvmStats.class));
+        when(stat.getFs()).thenReturn(mock(FsInfo.class));
+
+        Mockito.doReturn(nodes).when(esi).nodesInfo();
+        Mockito.doReturn(stats).when(esi).nodesStats();
+        String serverStats = esi.serverStats();
+        assertTrue(serverStats.matches("(?s).*\"jvm\".*"));
+        assertTrue(serverStats.matches("(?s).*12345678.*"));
+        assertTrue(serverStats.matches("(?s).*test_host.*"));
+    }
+
+
+    @Test
+    public void testServerInfo() throws IOException
+    {
+        ElasticsearchServerInfo esi = Mockito.spy(new ElasticsearchServerInfo(m_Client));
+        DiscoveryNode discoveryNode = mock(DiscoveryNode.class);
+        when(discoveryNode.isClientNode()).thenReturn(false);
+        when(discoveryNode.isDataNode()).thenReturn(true);
+
+        NodeStats[] stats = new NodeStats[1];
+        NodeStats stat = mock(NodeStats.class);
+        stats[0] = stat;
+        when(stat.getNode()).thenReturn(discoveryNode);
+
+        OsStats os = mock(OsStats.class);
+        Mem mem = mock(Mem.class);
+        when(os.getMem()).thenReturn(mem);
+        when(mem.getTotal()).thenReturn(new ByteSizeValue(1357924680));
+        Path p = mock(Path.class);
+        when(p.getAvailable()).thenReturn(new ByteSizeValue(543210000));
+        when(p.getTotal()).thenReturn(new ByteSizeValue(987650000));
+
+        FsInfo fs = mock(FsInfo.class);
+        when(fs.getTotal()).thenReturn(p);
+
+        when(stat.getOs()).thenReturn(os);
+        when(stat.getFs()).thenReturn(fs);
+
+        Mockito.doReturn(stats).when(esi).nodesStats();
+
+        ServerInfo si = esi.serverInfo();
+        assertEquals(1295L, (long)si.getTotalMemoryMb());
+        assertEquals(518l, (long)si.getAvailableDiskMb());
+        assertEquals(941l, (long)si.getTotalDiskMb());
+
+        assertEquals(HostnameFinder.findHostname(), si.getHostname());
     }
 
     private class TestServerInfo extends ElasticsearchServerInfo
