4d027b2fab1370db4dacaa751c5db852390ad0bc
Jay Modi <jaymode@users.noreply.github.com>
2016-10-17 11:00:04 -0400

    security: system user needs put mapping permissions to shrink indices
    The system user gets used to put mappings for an index during recovery from local shards, which
    is how the shrink index process works. The system user previously had this privilege in 2.x as
    we did not have the ThreadContext and dynamic mapping updates would be done by the system user;
    with the ThreadContext, these mapping updates are done by the actual user so this privilege
    was removed from the SystemUser.
    
    Closes elastic/x-pack#3766
    
    Original commit: elastic/x-pack@243e8a9a8229d6605a7b8bacab8ed4ad456ba949

---
+++ elasticsearch/src/test/java/org/elasticsearch/integration/ShrinkIndexWithSecurityIT.java
@@ -0,0 +1,71 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ * __________________
+ *
+ *  [2014] Elasticsearch Incorporated. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+package org.elasticsearch.integration;
+
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
+import org.elasticsearch.test.SecurityIntegTestCase;
+
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
+
+/**
+ * Integration test that uses multiple data nodes to test that the shrink index api works with security.
+ */
+@ClusterScope(minNumDataNodes = 2)
+public class ShrinkIndexWithSecurityIT extends SecurityIntegTestCase {
+
+    @Override
+    protected final boolean ignoreExternalCluster() {
+        return true;
+    }
+
+    public void testShrinkIndex() throws Exception {
+        final int randomNumberOfDocs = scaledRandomIntBetween(2, 12);
+        for (int i = 0; i < randomNumberOfDocs; i++) {
+            client().prepareIndex("bigindex", "type").setSource("foo", "bar").get();
+        }
+
+        ImmutableOpenMap<String, DiscoveryNode> dataNodes = client().admin().cluster().prepareState().get().getState().nodes()
+                .getDataNodes();
+        DiscoveryNode[] discoveryNodes = dataNodes.values().toArray(DiscoveryNode.class);
+        final String mergeNode = discoveryNodes[0].getName();
+        ensureGreen();
+        // relocate all shards to one node such that we can merge it.
+        client().admin().indices().prepareUpdateSettings("bigindex")
+                .setSettings(Settings.builder()
+                        .put("index.routing.allocation.require._name", mergeNode)
+                        .put("index.blocks.write", true)).get();
+
+        // wait for green and then shrink
+        ensureGreen();
+        assertAcked(client().admin().indices().prepareShrinkIndex("bigindex", "shrunk_bigindex")
+                .setSettings(Settings.builder()
+                        .put("index.number_of_replicas", 0)
+                        .put("index.number_of_shards", 1)
+                        .build()));
+
+        // verify all docs
+        ensureGreen();
+        assertHitCount(client().prepareSearch("shrunk_bigindex").setSize(100).setQuery(new TermsQueryBuilder("foo", "bar")).get(),
+                randomNumberOfDocs);
+    }
+}
