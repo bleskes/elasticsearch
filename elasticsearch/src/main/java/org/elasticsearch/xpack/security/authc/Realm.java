7f18c2dbc88675b60c8b4da59a942cb1846fbd00
Simon Willnauer <simon.willnauer@elasticsearch.com>
2016-10-25 22:11:19 +0200

    Remove all blocking calls from TransportGetUsersAction (elastic/x-pack#3876)
    `TransportGetUsersAction` does some funky blocking calls even though
    it's specifying `SAME` as the thread-pool indicating that it's fast or
    forking off quickly. Both might not be true today. This change adds
    async support to the methods it calls without breaking the existing
    Realm interface. Yet, we might need to do this down the road.
    
    Original commit: elastic/x-pack@fda63ff8ab3c08708215989d1a8b2fd669bf778b

--- elasticsearch/src/main/java/org/elasticsearch/xpack/security/authc/Realm.java
+++ elasticsearch/src/main/java/org/elasticsearch/xpack/security/authc/Realm.java
@@ -18,6 +18,7 @@
 package org.elasticsearch.xpack.security.authc;
 
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.xpack.security.user.User;
 
@@ -98,6 +99,10 @@ public abstract class Realm implements Comparable<Realm> {
      * @return         the {@link User} or {@code null} if lookup failed
      */
     public abstract User lookupUser(String username);
+
+    public void lookupUser(String username, ActionListener<User> listener) {
+        listener.onResponse(lookupUser(username));
+    }
 
     public Map<String, Object> usageStats() {
         Map<String, Object> stats = new HashMap<>();
