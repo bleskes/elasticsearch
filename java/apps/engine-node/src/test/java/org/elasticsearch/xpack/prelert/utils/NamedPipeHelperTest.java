11cda359e65354a4f82e078b501781b47e5d5e6b
David Roberts <dave.roberts@elastic.co>
2016-10-27 15:37:03 +0100

    Add a Java helper class for opening named pipes (elastic/x-pack#165)
    There are two lots of tests for this:
    
    1. Some standard unit tests
    2. Some tests that simulate the creation of named pipes that will be done in C++ 
       in the production code, and that need to run without Elasticsearch's security
       manager or seccomp enabled
    
    Original commit: elastic/x-pack@a5c3eccd6557b48433900900071e5d2a5951a185

---
+++ java/apps/engine-node/src/test/java/org/elasticsearch/xpack/prelert/utils/NamedPipeHelperTest.java
@@ -0,0 +1,69 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ *
+ * Copyright (C) 2016 Elasticsearch BV. All Rights Reserved.
+ *
+ * Notice: this software, and all information contained therein, is the
+ * exclusive property of Elasticsearch BV and its licensors, if any, and is
+ * protected under applicable domestic and foreign law, and international
+ * treaties. Reproduction, republication or distribution without the express
+ * written consent of Elasticsearch BV is strictly prohibited.
+ */
+
+package org.elasticsearch.xpack.prelert.utils;
+
+import org.elasticsearch.test.ESTestCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+
+
+/**
+ * Only negative test cases are covered, as positive tests would need to create named pipes,
+ * and this is not possible in Java with the Elasticsearch security manager configuration.
+ */
+public class NamedPipeHelperTest extends ESTestCase {
+
+    public void testOpenForInputGivenPipeDoesNotExist() {
+        IOException ioe = ESTestCase.expectThrows(FileNotFoundException.class, () ->
+                NamedPipeHelper.openNamedPipeInputStream(NamedPipeHelper.getDefaultPipeDirectoryPrefix() + "this pipe does not exist",
+                        Duration.ofSeconds(1)));
+
+        assertTrue(ioe.getMessage(), ioe.getMessage().contains("No such file or directory") ||
+                ioe.getMessage().contains("The system cannot find the file specified"));
+    }
+
+    public void testOpenForOutputGivenPipeDoesNotExist() {
+        IOException ioe = ESTestCase.expectThrows(FileNotFoundException.class, () ->
+                NamedPipeHelper.openNamedPipeOutputStream(NamedPipeHelper.getDefaultPipeDirectoryPrefix() + "this pipe does not exist",
+                        Duration.ofSeconds(1)));
+
+        assertTrue(ioe.getMessage(), ioe.getMessage().contains("No such file or directory") ||
+                ioe.getMessage().contains("The system cannot find the file specified"));
+    }
+
+    public void testOpenForInputGivenPipeIsRegularFile() throws IOException {
+        File tempFile = File.createTempFile("not a named pipe", null);
+
+        IOException ioe = ESTestCase.expectThrows(IOException.class, () ->
+            NamedPipeHelper.openNamedPipeInputStream(tempFile, Duration.ofSeconds(1)));
+
+        assertTrue(ioe.getMessage(), ioe.getMessage().contains("is not a named pipe"));
+
+        assertTrue(tempFile.delete());
+    }
+
+    public void testOpenForOutputGivenPipeIsRegularFile() throws IOException {
+        File tempFile = File.createTempFile("not a named pipe", null);
+
+        IOException ioe = ESTestCase.expectThrows(IOException.class, () ->
+                NamedPipeHelper.openNamedPipeOutputStream(tempFile, Duration.ofSeconds(1)));
+
+        assertTrue(ioe.getMessage(), ioe.getMessage().contains("is not a named pipe") ||
+                ioe.getMessage().contains("The system cannot find the file specified"));
+
+        assertTrue(tempFile.delete());
+    }
+}
