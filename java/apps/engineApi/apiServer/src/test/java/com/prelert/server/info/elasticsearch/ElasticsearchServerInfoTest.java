e07820f228c1022710b039da5371a08654f4b97c
David Roberts <dave.roberts@elastic.co>
2016-10-28 11:16:35 +0100

    Upgrade the analytics-dev branch to use GA Elasticsearch and Kibana 5.0 (elastic/x-pack#188)
    Disabled the build of the incomplete engine-node project as it no longer
    builds with the GA versions
    
    Original commit: elastic/x-pack@c3987ef58ca792ed7106b77e9a18839827d9768c

--- java/apps/engineApi/apiServer/src/test/java/com/prelert/server/info/elasticsearch/ElasticsearchServerInfoTest.java
+++ java/apps/engineApi/apiServer/src/test/java/com/prelert/server/info/elasticsearch/ElasticsearchServerInfoTest.java
@@ -137,7 +137,7 @@ public class ElasticsearchServerInfoTest
         StreamInput si = mock(StreamInput.class);
         when(si.readByte()).thenReturn((byte) 0x01);
 
-        when(si.getVersion()).thenReturn(Version.V_5_0_0_beta1);
+        when(si.getVersion()).thenReturn(Version.V_5_0_0);
         OsInfo osInfo = new OsInfo(si);
         when(node.getOs()).thenReturn(osInfo);
         when(node.getJvm()).thenReturn(jvmInfo);
