88731f4bebf0457d6bde7acc5c7f2d8ef12ec4f5
Colin Goodheart-Smithe <colings86@users.noreply.github.com>
2016-10-30 08:44:37 +0000

    removed tabs
    Original commit: elastic/x-pack@e6f33a8a952c8ee94b315b8edfedc32f0f11feb2

--- java/apps/engine-node/src/test/java/org/elasticsearch/xpack/prelert/job/errorcodes/ErrorCodesTest.java
+++ java/apps/engine-node/src/test/java/org/elasticsearch/xpack/prelert/job/errorcodes/ErrorCodesTest.java
@@ -7,24 +7,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * This test ensures that all the error values in {@linkplain ErrorCodes}
- * are unique so no 2 conditions can return the same error code.
- * This tests is designed to catch copy/paste errors.
+ * This test ensures that all the error values in {@linkplain ErrorCodes} are
+ * unique so no 2 conditions can return the same error code. This tests is
+ * designed to catch copy/paste errors.
  */
-public class ErrorCodesTest extends ESTestCase
-{
-	public void testErrorCodesAreUnique()
-	throws IllegalArgumentException, IllegalAccessException
-	{
-		ErrorCodes[] values = ErrorCodes.class.getEnumConstants();
+public class ErrorCodesTest extends ESTestCase {
+    public void testErrorCodesAreUnique() throws IllegalArgumentException, IllegalAccessException {
+        ErrorCodes[] values = ErrorCodes.class.getEnumConstants();
 
-		Set<Long> errorValueSet = new HashSet<>();
+        Set<Long> errorValueSet = new HashSet<>();
 
-		for (ErrorCodes value : values)
-		{
-			errorValueSet.add(value.getValue());
-		}
+        for (ErrorCodes value : values) {
+            errorValueSet.add(value.getValue());
+        }
 
-		assertEquals(values.length, errorValueSet.size());
-	}
+        assertEquals(values.length, errorValueSet.size());
+    }
 }
