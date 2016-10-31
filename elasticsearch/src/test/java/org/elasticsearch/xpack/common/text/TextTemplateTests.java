d2404c832cda243641fa11a5a4126f3c57ef8287
Jack Conradson <osjdconrad@gmail.com>
2016-10-31 14:05:31 -0700

    Backport clean up of ScriptType.
    Relates elastic/x-pack#3922
    
    Original commit: elastic/x-pack@bf5997ad7f2ff2d9b48b5e3a51ceb5f03643bc03

--- elasticsearch/src/test/java/org/elasticsearch/xpack/common/text/TextTemplateTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/common/text/TextTemplateTests.java
@@ -121,7 +121,7 @@ public class TextTemplateTests extends ESTestCase {
                 builder.field("file", template.getTemplate());
                 break;
             case STORED:
-                builder.field("id", template.getTemplate());
+                builder.field("stored", template.getTemplate());
         }
         builder.field("params", template.getParams());
         builder.endObject();
