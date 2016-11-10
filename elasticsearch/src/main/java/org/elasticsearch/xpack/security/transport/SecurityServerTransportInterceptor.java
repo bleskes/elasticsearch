024b4308ded3f0893e5395904e5ae5893b3700c2
Jason Tedor <jason@tedor.me>
2016-11-10 16:13:23 -0500

    Fix handler name on message not fully read
    Today when a message is not fully read on a response, we log (among
    other details) the handler name. Unfortunately, if the handler is a
    wrapper, all that we see is
    
    o.e.x.s.t.TransportService$ContextRestoreResponseHandler@7446ba18
    
    completely losing the offending handler. This commit adds an override
    for TransportService$ContextRestoreResponseHandler#toString so that the
    underlying offender can be discovered.
    
    Relates elastic/x-pack#4043
    
    Original commit: elastic/x-pack@f08b1451db6350648477281034fcc1ff6b19dcc6

--- elasticsearch/src/main/java/org/elasticsearch/xpack/security/transport/SecurityServerTransportInterceptor.java
+++ elasticsearch/src/main/java/org/elasticsearch/xpack/security/transport/SecurityServerTransportInterceptor.java
@@ -271,6 +271,7 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
      * are invoked we restore the context.
      */
     static final class ContextRestoreResponseHandler<T extends TransportResponse> implements TransportResponseHandler<T> {
+
         private final TransportResponseHandler<T> delegate;
         private final ThreadContext.StoredContext context;
         private final ThreadContext threadContext;
@@ -308,5 +309,12 @@ public class SecurityServerTransportInterceptor implements TransportInterceptor 
         public String executor() {
             return delegate.executor();
         }
+
+        @Override
+        public String toString() {
+            return getClass().getName() + "/" + delegate.toString();
+        }
+
     }
+
 }
