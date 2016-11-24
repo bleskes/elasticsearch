1546c77fca0b40fc2988fd992f2600c657bbb185
Dimitris Athanasiou <dimitris@elastic.co>
2016-11-24 14:59:25 +0000

    Make close job a master node action (elastic/x-pack#362)
    * Make close job a master node action
    
    The close job action now:
    
     - Is a master node action
     - Sets the job status to CLOSING
     - Waits for the job status to change to CLOSED before it responds
    
    JobLifeCycleService picks up on a job status change to CLOSING and
    closes the job. At the end, it sets the job status to CLOSED.
    
    * Assert job status is closed after close in integration test
    
    This also correctly passes an ActionListener to JobManager.setJobStatus
    in order to ensure the job status request has completed and to properly
    propagate failures.
    
    Original commit: elastic/x-pack@82151823dccda593df2ea9a413b04fc607dfd338

--- elasticsearch/src/main/java/org/elasticsearch/xpack/prelert/job/metadata/JobLifeCycleService.java
+++ elasticsearch/src/main/java/org/elasticsearch/xpack/prelert/job/metadata/JobLifeCycleService.java
@@ -102,6 +102,7 @@ public class JobLifeCycleService extends AbstractComponent implements ClusterSta
             case RUNNING:
                 break;
             case CLOSING:
+                executor.execute(() -> closeJob(job));
                 break;
             case CLOSED:
                 break;
@@ -150,6 +151,18 @@ public class JobLifeCycleService extends AbstractComponent implements ClusterSta
         Set<String> newSet = new HashSet<>(localAllocatedJobs);
         newSet.remove(jobId);
         localAllocatedJobs = newSet;
+    }
+
+    private void closeJob(Job job) {
+        try {
+            // NORELEASE Ensure this also removes the job auto-close timeout task
+            dataProcessor.closeJob(job.getId());
+        } catch (ElasticsearchException e) {
+            logger.error("Failed to close job [" + job.getId() + "]", e);
+            updateJobStatus(job.getId(), JobStatus.FAILED);
+            return;
+        }
+        updateJobStatus(job.getId(), JobStatus.CLOSED);
     }
 
     private void pauseJob(Job job) {
