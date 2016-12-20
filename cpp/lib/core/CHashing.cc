7b18ea429f7d84c81deb4691960666c93126fede
David Roberts <dave.roberts@elastic.co>
2016-12-20 15:59:27 +0000

    Silence clang's new fallthrough warnings (elastic/x-pack#592)
    Original commit: elastic/x-pack@9934cb78c3a7b00fa4faefa622148b1078d1724e

--- cpp/lib/core/CHashing.cc
+++ cpp/lib/core/CHashing.cc
@@ -19,6 +19,7 @@
 #include <core/CScopedFastLock.h>
 #include <core/CStringUtils.h>
 
+#include <boost/config.hpp>
 #include <boost/bind.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/ref.hpp>
@@ -393,11 +394,18 @@ uint32_t CHashing::murmurHash32(const void *key,
 
     switch (remainder)
     {
-    case 3: h ^= remainingData[2] << 16;
-    case 2: h ^= remainingData[1] << 8;
-    case 1: h ^= remainingData[0];
-            h *= m;
-    default: break;
+    case 3:
+        h ^= remainingData[2] << 16;
+        BOOST_FALLTHROUGH;
+    case 2:
+        h ^= remainingData[1] << 8;
+        BOOST_FALLTHROUGH;
+    case 1:
+        h ^= remainingData[0];
+        h *= m;
+        BOOST_FALLTHROUGH;
+    default:
+        break;
     };
 
     h ^= h >> 13;
@@ -441,11 +449,18 @@ uint32_t CHashing::safeMurmurHash32(const void *key,
 
     switch (length)
     {
-    case 3: h ^= data[2] << 16;
-    case 2: h ^= data[1] << 8;
-    case 1: h ^= data[0];
-            h *= m;
-    default: break;
+    case 3:
+        h ^= data[2] << 16;
+        BOOST_FALLTHROUGH;
+    case 2:
+        h ^= data[1] << 8;
+        BOOST_FALLTHROUGH;
+    case 1:
+        h ^= data[0];
+        h *= m;
+        BOOST_FALLTHROUGH;
+    default:
+        break;
     };
 
     h ^= h >> 13;
@@ -488,15 +503,30 @@ uint64_t CHashing::murmurHash64(const void *key,
 
     switch (remainder)
     {
-    case 7: h ^= uint64_t(remainingData[6]) << 48;
-    case 6: h ^= uint64_t(remainingData[5]) << 40;
-    case 5: h ^= uint64_t(remainingData[4]) << 32;
-    case 4: h ^= uint64_t(remainingData[3]) << 24;
-    case 3: h ^= uint64_t(remainingData[2]) << 16;
-    case 2: h ^= uint64_t(remainingData[1]) << 8;
-    case 1: h ^= uint64_t(remainingData[0]);
-            h *= m;
-    default: break;
+    case 7:
+        h ^= uint64_t(remainingData[6]) << 48;
+        BOOST_FALLTHROUGH;
+    case 6:
+        h ^= uint64_t(remainingData[5]) << 40;
+        BOOST_FALLTHROUGH;
+    case 5:
+        h ^= uint64_t(remainingData[4]) << 32;
+        BOOST_FALLTHROUGH;
+    case 4:
+        h ^= uint64_t(remainingData[3]) << 24;
+        BOOST_FALLTHROUGH;
+    case 3:
+        h ^= uint64_t(remainingData[2]) << 16;
+        BOOST_FALLTHROUGH;
+    case 2:
+        h ^= uint64_t(remainingData[1]) << 8;
+        BOOST_FALLTHROUGH;
+    case 1:
+        h ^= uint64_t(remainingData[0]);
+        h *= m;
+        BOOST_FALLTHROUGH;
+    default:
+        break;
     };
 
     h ^= h >> r;
@@ -544,15 +574,30 @@ uint64_t CHashing::safeMurmurHash64(const void *key,
 
     switch (length)
     {
-    case 7: h ^= uint64_t(data[6]) << 48;
-    case 6: h ^= uint64_t(data[5]) << 40;
-    case 5: h ^= uint64_t(data[4]) << 32;
-    case 4: h ^= uint64_t(data[3]) << 24;
-    case 3: h ^= uint64_t(data[2]) << 16;
-    case 2: h ^= uint64_t(data[1]) << 8;
-    case 1: h ^= uint64_t(data[0]);
-            h *= m;
-    default: break;
+    case 7:
+        h ^= uint64_t(data[6]) << 48;
+        BOOST_FALLTHROUGH;
+    case 6:
+        h ^= uint64_t(data[5]) << 40;
+        BOOST_FALLTHROUGH;
+    case 5:
+        h ^= uint64_t(data[4]) << 32;
+        BOOST_FALLTHROUGH;
+    case 4:
+        h ^= uint64_t(data[3]) << 24;
+        BOOST_FALLTHROUGH;
+    case 3:
+        h ^= uint64_t(data[2]) << 16;
+        BOOST_FALLTHROUGH;
+    case 2:
+        h ^= uint64_t(data[1]) << 8;
+        BOOST_FALLTHROUGH;
+    case 1:
+        h ^= uint64_t(data[0]);
+        h *= m;
+        BOOST_FALLTHROUGH;
+    default:
+        break;
     };
 
     h ^= h >> r;
