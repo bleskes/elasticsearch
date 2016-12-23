b1cb074651dec52a1e4beaa2788b3461d2172c53
David Kyle <david.kyle@elastic.co>
2016-12-23 12:34:25 +0000

    Fix compilation after upstream changes
    Original commit: elastic/x-pack@29065bda05c3c8a8d662bfc5e72750ccb95651ee

--- elasticsearch/src/test/java/org/elasticsearch/xpack/prelert/integration/PrelertYamlTestSuiteIT.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/prelert/integration/PrelertYamlTestSuiteIT.java
@@ -17,7 +17,6 @@ package org.elasticsearch.xpack.prelert.integration;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
-import org.elasticsearch.test.rest.yaml.parser.ClientYamlTestParseException;
 import org.junit.After;
 
 import java.io.IOException;
@@ -30,7 +29,7 @@ public class PrelertYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
     }
 
     @ParametersFactory
-    public static Iterable<Object[]> parameters() throws IOException, ClientYamlTestParseException {
+    public static Iterable<Object[]> parameters() throws IOException {
         return createParameters();
     }
 
