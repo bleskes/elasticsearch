779e313abc99855e66d62a9543759eb44f5aa3a9
Chris Earle <pickypg@users.noreply.github.com>
2016-12-30 18:55:51 -0500

    [Monitoring] Use Cgroup Data from _nodes/stats (elastic/x-pack#4484)
    * [Monitoring] Use Cgroup Data from _nodes/stats
    
    This adds Cgroup data from _nodes/stats.
    
    - This also adds 5m and 15m from system load, which are sent from ES, but
      were not recorded. Kibana does record/report those values though.
    
    Original commit: elastic/x-pack@1bb009d717dd2289749f22e3ec0aeb034fa359f7

--- elasticsearch/src/test/java/org/elasticsearch/xpack/monitoring/resolver/node/NodeStatsResolverTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/monitoring/resolver/node/NodeStatsResolverTests.java
@@ -95,7 +95,7 @@ public class NodeStatsResolverTests extends MonitoringIndexNameResolverTestCase<
             return;
         }
 
-        // we only report IoStats and spins on Linux
+        // we only report IoStats on Linux
         if (Constants.LINUX == false) {
             if (field.startsWith("node_stats.fs.io_stats")) {
                 return;
@@ -104,6 +104,11 @@ public class NodeStatsResolverTests extends MonitoringIndexNameResolverTestCase<
 
         // node_stats.fs.data.spins can be null and it's only reported on Linux
         if (field.startsWith("node_stats.fs.data.spins")) {
+            return;
+        }
+
+        // cgroups can be null, and it's only reported on Linux
+        if (field.startsWith("node_stats.os.cgroup")) {
             return;
         }
 
