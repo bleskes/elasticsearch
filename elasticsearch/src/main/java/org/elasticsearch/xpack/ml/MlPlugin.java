1cc7d5651e6255e4deaf25a8b93c489045aab052
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-01-12 16:30:58 +0100

    fixed compile error due upstream changed
    Original commit: elastic/x-pack@8183c8ba7649ec2541b2d6a722422e078a6d1fbf

--- elasticsearch/src/main/java/org/elasticsearch/xpack/ml/MlPlugin.java
+++ elasticsearch/src/main/java/org/elasticsearch/xpack/ml/MlPlugin.java
@@ -34,6 +34,7 @@ import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestHandler;
 import org.elasticsearch.script.ScriptService;
+import org.elasticsearch.search.SearchRequestParsers;
 import org.elasticsearch.tasks.TaskManager;
 import org.elasticsearch.threadpool.ExecutorBuilder;
 import org.elasticsearch.threadpool.FixedExecutorBuilder;
@@ -183,7 +184,8 @@ public class MlPlugin extends Plugin implements ActionPlugin {
 
     @Override
     public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
-                                               ResourceWatcherService resourceWatcherService, ScriptService scriptService,
+                                               ResourceWatcherService resourceWatcherService,
+                                               ScriptService scriptService, SearchRequestParsers searchRequestParsers,
                                                NamedXContentRegistry xContentRegistry) {
         JobResultsPersister jobResultsPersister = new JobResultsPersister(settings, client);
         JobProvider jobProvider = new JobProvider(client, 0, parseFieldMatcherSupplier.getParseFieldMatcher());
