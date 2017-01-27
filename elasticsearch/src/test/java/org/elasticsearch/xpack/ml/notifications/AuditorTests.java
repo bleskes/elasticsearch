38eb1d6ce021c34d921ff2888b3da17d7ff38664
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-01-27 17:20:18 +0100

    [TEST] fixed mocking logic to include id
    Original commit: elastic/x-pack@6a697cd6ac493dc9eae01686eb2f61de099aaf0c

--- elasticsearch/src/test/java/org/elasticsearch/xpack/ml/notifications/AuditorTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/ml/notifications/AuditorTests.java
@@ -29,6 +29,7 @@ import org.mockito.Mockito;
 
 import java.io.IOException;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 public class AuditorTests extends ESTestCase {
@@ -101,9 +102,9 @@ public class AuditorTests extends ESTestCase {
         IndexRequestBuilder indexRequestBuilder = Mockito.mock(IndexRequestBuilder.class);
         when(indexRequestBuilder.setSource(jsonCaptor.capture())).thenReturn(indexRequestBuilder);
         when(indexRequestBuilder.execute()).thenReturn(indexResponse);
-        when(client.prepareIndex(indexCaptor.capture(), typeCaptor.capture()))
+        when(client.prepareIndex(indexCaptor.capture(), typeCaptor.capture(), any()))
         .thenReturn(indexRequestBuilder);
-        when(client.prepareIndex(indexCaptor.capture(), typeCaptor.capture()))
+        when(client.prepareIndex(indexCaptor.capture(), typeCaptor.capture(), any()))
         .thenReturn(indexRequestBuilder);
     }
 
