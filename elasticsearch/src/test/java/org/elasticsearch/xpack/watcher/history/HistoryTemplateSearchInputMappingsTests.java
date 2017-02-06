8d1b9c26d81e2481de9d5e3c5be49f3ac5ead353
Simon Willnauer <simonw@apache.org>
2017-02-06 18:26:01 +0100

    Remove obsolet search type QUERY_AND_FETCH
    Original commit: elastic/x-pack@f044591675029f27e419ce7c3e9568d311e8544a

--- elasticsearch/src/test/java/org/elasticsearch/xpack/watcher/history/HistoryTemplateSearchInputMappingsTests.java
+++ elasticsearch/src/test/java/org/elasticsearch/xpack/watcher/history/HistoryTemplateSearchInputMappingsTests.java
@@ -63,7 +63,7 @@ public class HistoryTemplateSearchInputMappingsTests extends AbstractWatcherInte
         refresh();
 
         WatcherSearchTemplateRequest request = new WatcherSearchTemplateRequest(
-                new String[]{index}, new String[]{type}, SearchType.QUERY_AND_FETCH,
+                new String[]{index}, new String[]{type}, SearchType.QUERY_THEN_FETCH,
                 WatcherSearchTemplateRequest.DEFAULT_INDICES_OPTIONS, new BytesArray("{}")
         );
         PutWatchResponse putWatchResponse = watcherClient().preparePutWatch("_id").setSource(watchBuilder()
