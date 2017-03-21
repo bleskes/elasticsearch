19da3fcbd18ee7d81d7ca4117dcdf123decb4d94
Dimitris Athanasiou <dimitris@elastic.co>
2017-03-21 17:05:36 +0000

    [ML] Accept 'now' in start/end params of start datafeed API (#784)
    The params start/end of the start datafeed API now accept
    'now' as a value.
    
    Also adds a validation that start must be earlier than end.
    
    Closes #781

--- plugin/src/test/java/org/elasticsearch/xpack/ml/rest/datafeeds/RestStartDatafeedActionTests.java
+++ plugin/src/test/java/org/elasticsearch/xpack/ml/rest/datafeeds/RestStartDatafeedActionTests.java
@@ -28,37 +28,32 @@ import java.util.Map;
 
 import static org.mockito.Mockito.mock;
 
-public class RestStartJobDatafeedActionTests extends ESTestCase {
+public class RestStartDatafeedActionTests extends ESTestCase {
 
     public void testPrepareRequest() throws Exception {
-        RestStartDatafeedAction action = new RestStartDatafeedAction(Settings.EMPTY, mock(RestController.class));
+        RestStartDatafeedAction action = new RestStartDatafeedAction(Settings.EMPTY,
+                mock(RestController.class));
 
         Map<String, String> params = new HashMap<>();
         params.put("start", "not-a-date");
         params.put("datafeed_id", "foo-datafeed");
-        RestRequest restRequest1 = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY).withParams(params).build();
+        RestRequest restRequest1 = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY)
+                .withParams(params).build();
         ElasticsearchParseException e =  expectThrows(ElasticsearchParseException.class,
                 () -> action.prepareRequest(restRequest1, mock(NodeClient.class)));
-        assertEquals("Query param 'start' with value 'not-a-date' cannot be parsed as a date or converted to a number (epoch).",
+        assertEquals("Query param 'start' with value 'not-a-date' cannot be parsed as a date or " +
+                        "converted to a number (epoch).",
                 e.getMessage());
 
         params = new HashMap<>();
+        params.put("start", "now");
         params.put("end", "not-a-date");
         params.put("datafeed_id", "foo-datafeed");
-        RestRequest restRequest2 = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY).withParams(params).build();
-        e =  expectThrows(ElasticsearchParseException.class, () -> action.prepareRequest(restRequest2, mock(NodeClient.class)));
-        assertEquals("Query param 'end' with value 'not-a-date' cannot be parsed as a date or converted to a number (epoch).",
-                e.getMessage());
+        RestRequest restRequest2 = new FakeRestRequest.Builder(NamedXContentRegistry.EMPTY)
+                .withParams(params).build();
+        e =  expectThrows(ElasticsearchParseException.class,
+                () -> action.prepareRequest(restRequest2, mock(NodeClient.class)));
+        assertEquals("Query param 'end' with value 'not-a-date' cannot be parsed as a date or " +
+                        "converted to a number (epoch).", e.getMessage());
     }
-
-    public void testParseDateOrThrow() {
-        assertEquals(0L, RestStartDatafeedAction.parseDateOrThrow("0", "start"));
-        assertEquals(0L, RestStartDatafeedAction.parseDateOrThrow("1970-01-01T00:00:00Z", "start"));
-
-        Exception e = expectThrows(ElasticsearchParseException.class,
-                () -> RestStartDatafeedAction.parseDateOrThrow("not-a-date", "start"));
-        assertEquals("Query param 'start' with value 'not-a-date' cannot be parsed as a date or converted to a number (epoch).",
-                e.getMessage());
-    }
-
 }
