28f40f94d11fc46017a39801c7833e73c845a6bf
David Roberts <dave.roberts@elastic.co>
2017-03-30 10:33:56 +0100

    [ML] Adjust error to make sense for FAILED as well as CLOSED jobs (#888)
    Previously trying to perform an operation on a FAILED job would tell
    you that it hadn't been opened.  This was very confusing if you knew
    very well you'd just opened the job.
    
    Relates #879

--- plugin/src/main/java/org/elasticsearch/xpack/ml/action/TransportJobTaskAction.java
+++ plugin/src/main/java/org/elasticsearch/xpack/ml/action/TransportJobTaskAction.java
@@ -72,7 +72,7 @@ public abstract class TransportJobTaskAction<OperationTask extends Task, Request
         PersistentTasksCustomMetaData tasks = clusterService.state().getMetaData().custom(PersistentTasksCustomMetaData.TYPE);
         PersistentTasksCustomMetaData.PersistentTask<?> jobTask = MlMetadata.getJobTask(jobId, tasks);
         if (jobTask == null || jobTask.isAssigned() == false) {
-            String message = "Cannot perform requested action because job [" + jobId + "] hasn't been opened";
+            String message = "Cannot perform requested action because job [" + jobId + "] is not open";
             listener.onFailure(ExceptionsHelper.conflictStatusException(message));
         } else {
             request.setNodes(jobTask.getExecutorNode());
@@ -90,7 +90,7 @@ public abstract class TransportJobTaskAction<OperationTask extends Task, Request
         } else {
             logger.warn("Unexpected job state based on cluster state version [{}]", state.getVersion());
             listener.onFailure(ExceptionsHelper.conflictStatusException("Cannot perform requested action because job [" +
-                    request.getJobId() + "] hasn't been opened"));
+                    request.getJobId() + "] is not open"));
         }
     }
 
