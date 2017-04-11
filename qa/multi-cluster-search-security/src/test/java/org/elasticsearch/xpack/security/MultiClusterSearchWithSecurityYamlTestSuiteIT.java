995984c535fb6b1ec6aae75178f638340913bc9f
Simon Willnauer <simonw@apache.org>
2017-04-11 17:55:21 +0200

    [TEST] Use SecurityClusterClientYamlTestCase to ensure mappings and templates are present

--- qa/multi-cluster-search-security/src/test/java/org/elasticsearch/xpack/security/MultiClusterSearchWithSecurityYamlTestSuiteIT.java
+++ qa/multi-cluster-search-security/src/test/java/org/elasticsearch/xpack/security/MultiClusterSearchWithSecurityYamlTestSuiteIT.java
@@ -28,7 +28,7 @@ import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
 
 import static org.elasticsearch.xpack.security.authc.support.UsernamePasswordToken.basicAuthHeaderValue;
 
-public class MultiClusterSearchWithSecurityYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+public class MultiClusterSearchWithSecurityYamlTestSuiteIT extends SecurityClusterClientYamlTestCase {
 
     private static final String USER = "test_user";
     private static final String PASS = "changeme";
