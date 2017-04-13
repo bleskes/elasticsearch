e44f42032607a7dc12e5f73b9d6afd284ad17e8a
Martijn van Groningen <martijn.v.groningen@gmail.com>
2017-04-13 20:52:48 +0200

    [ML] Split persistent job params from action request and introduced separate task names to register the persistent tasks executors and params.
    Also renamed start and stop datafeed action names to be singular in order to be consistent with open and close action names.

--- plugin/src/main/java/org/elasticsearch/xpack/ml/action/StopDatafeedAction.java
+++ plugin/src/main/java/org/elasticsearch/xpack/ml/action/StopDatafeedAction.java
@@ -71,7 +71,7 @@ public class StopDatafeedAction
         extends Action<StopDatafeedAction.Request, StopDatafeedAction.Response, StopDatafeedAction.RequestBuilder> {
 
     public static final StopDatafeedAction INSTANCE = new StopDatafeedAction();
-    public static final String NAME = "cluster:admin/xpack/ml/datafeeds/stop";
+    public static final String NAME = "cluster:admin/xpack/ml/datafeed/stop";
     public static final ParseField TIMEOUT = new ParseField("timeout");
     public static final ParseField FORCE = new ParseField("force");
     public static final TimeValue DEFAULT_TIMEOUT = TimeValue.timeValueMinutes(5);
@@ -121,7 +121,6 @@ public class StopDatafeedAction
         public Request(String datafeedId) {
             this.datafeedId = ExceptionsHelper.requireNonNull(datafeedId, DatafeedConfig.ID.getPreferredName());
             this.resolvedDatafeedIds = new String[] { datafeedId };
-            setActions(StartDatafeedAction.NAME);
         }
 
         Request() {
