dd1673c63a42a6e03c29f6867f0577db1f3f2340
Dimitrios Athanasiou <dimitris@elastic.co>
2017-07-05 13:30:25 +0100

    [TEST] Fix MlBasicMultiNodeIT after changing flush response
    Relates #1914

--- qa/ml-basic-multi-node/src/test/java/org/elasticsearch/xpack/ml/integration/MlBasicMultiNodeIT.java
+++ qa/ml-basic-multi-node/src/test/java/org/elasticsearch/xpack/ml/integration/MlBasicMultiNodeIT.java
@@ -31,6 +31,8 @@ import java.util.Map;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.elasticsearch.common.xcontent.XContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class MlBasicMultiNodeIT extends ESRestTestCase {
 
@@ -85,7 +87,7 @@ public class MlBasicMultiNodeIT extends ESRestTestCase {
 
         response = client().performRequest("post", MachineLearning.BASE_PATH + "anomaly_detectors/" + jobId + "/_flush");
         assertEquals(200, response.getStatusLine().getStatusCode());
-        assertEquals(Collections.singletonMap("flushed", true), responseEntityToMap(response));
+        assertFlushResponse(response, true, 1403481600000L);
 
         response = client().performRequest("post", MachineLearning.BASE_PATH + "anomaly_detectors/" + jobId + "/_close",
                 Collections.singletonMap("timeout", "20s"));
@@ -210,7 +212,7 @@ public class MlBasicMultiNodeIT extends ESRestTestCase {
 
         response = client().performRequest("post", MachineLearning.BASE_PATH + "anomaly_detectors/" + jobId + "/_flush");
         assertEquals(200, response.getStatusLine().getStatusCode());
-        assertEquals(Collections.singletonMap("flushed", true), responseEntityToMap(response));
+        assertFlushResponse(response, true, 1403481600000L);
 
         response = client().performRequest("post", MachineLearning.BASE_PATH + "anomaly_detectors/" + jobId + "/_close",
                 Collections.singletonMap("timeout", "20s"));
@@ -321,4 +323,11 @@ public class MlBasicMultiNodeIT extends ESRestTestCase {
         return XContentHelper.convertToMap(JSON.xContent(), response.getEntity().getContent(), false);
     }
 
+    private static void assertFlushResponse(Response response, boolean expectedFlushed, long expectedLastFinalizedBucketEnd)
+            throws IOException {
+        Map<String, Object> asMap = responseEntityToMap(response);
+        assertThat(asMap.size(), equalTo(2));
+        assertThat(asMap.get("flushed"), is(true));
+        assertThat(asMap.get("last_finalized_bucket_end"), equalTo(expectedLastFinalizedBucketEnd));
+    }
 }
